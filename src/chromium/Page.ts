bd6c6607a6008e755ff1b91ae91abaf7fb5063f4
Pavel <pavel.feldman@gmail.com>
2019-11-19 13:34:34 -0800

    chore: remove page.pdf API

--- src/chromium/Page.ts
+++ src/chromium/Page.ts
@@ -28,7 +28,7 @@ import { Events } from '../Events';
 import { Frame } from './Frame';
 import { FrameManager, FrameManagerEvents } from './FrameManager';
 import { assert, debugError, helper } from '../helper';
-import { readProtocolStream, releaseObject, getExceptionMessage, valueFromRemoteObject } from './protocolHelper';
+import { releaseObject, getExceptionMessage, valueFromRemoteObject } from './protocolHelper';
 import { Keyboard, Mouse, Touchscreen } from './Input';
 import { createJSHandle, ElementHandle, JSHandle, ClickOptions, PointerActionOptions, MultiClickOptions } from './JSHandle';
 import { Response, NetworkManagerEvents } from './NetworkManager';
@@ -736,57 +736,6 @@ export class Page extends EventEmitter {
     }
   }
 
-  async pdf(options: PDFOptions = {}): Promise<Buffer> {
-    const {
-      scale = 1,
-      displayHeaderFooter = false,
-      headerTemplate = '',
-      footerTemplate = '',
-      printBackground = false,
-      landscape = false,
-      pageRanges = '',
-      preferCSSPageSize = false,
-      margin = {},
-      path = null
-    } = options;
-
-    let paperWidth = 8.5;
-    let paperHeight = 11;
-    if (options.format) {
-      const format = PagePaperFormats[options.format.toLowerCase()];
-      assert(format, 'Unknown paper format: ' + options.format);
-      paperWidth = format.width;
-      paperHeight = format.height;
-    } else {
-      paperWidth = convertPrintParameterToInches(options.width) || paperWidth;
-      paperHeight = convertPrintParameterToInches(options.height) || paperHeight;
-    }
-
-    const marginTop = convertPrintParameterToInches(margin.top) || 0;
-    const marginLeft = convertPrintParameterToInches(margin.left) || 0;
-    const marginBottom = convertPrintParameterToInches(margin.bottom) || 0;
-    const marginRight = convertPrintParameterToInches(margin.right) || 0;
-
-    const result = await this._client.send('Page.printToPDF', {
-      transferMode: 'ReturnAsStream',
-      landscape,
-      displayHeaderFooter,
-      headerTemplate,
-      footerTemplate,
-      printBackground,
-      scale,
-      paperWidth,
-      paperHeight,
-      marginTop,
-      marginBottom,
-      marginLeft,
-      marginRight,
-      pageRanges,
-      preferCSSPageSize
-    });
-    return await readProtocolStream(this._client, result.stream, path);
-  }
-
   async title(): Promise<string> {
     return this.mainFrame().title();
   }
@@ -866,22 +815,6 @@ export class Page extends EventEmitter {
   }
 }
 
-type PDFOptions = {
-  scale?: number,
-  displayHeaderFooter?: boolean,
-  headerTemplate?: string,
-  footerTemplate?: string,
-  printBackground?: boolean,
-  landscape?: boolean,
-  pageRanges?: string,
-  format?: string,
-  width?: string|number,
-  height?: string|number,
-  preferCSSPageSize?: boolean,
-  margin?: {top?: string|number, bottom?: string|number, left?: string|number, right?: string|number},
-  path?: string,
-}
-
 type Metrics = {
   Timestamp?: number,
   Documents?: number,
@@ -928,55 +861,6 @@ const supportedMetrics: Set<string> = new Set([
   'JSHeapUsedSize',
   'JSHeapTotalSize',
 ]);
-
-const PagePaperFormats = {
-  letter: {width: 8.5, height: 11},
-  legal: {width: 8.5, height: 14},
-  tabloid: {width: 11, height: 17},
-  ledger: {width: 17, height: 11},
-  a0: {width: 33.1, height: 46.8 },
-  a1: {width: 23.4, height: 33.1 },
-  a2: {width: 16.54, height: 23.4 },
-  a3: {width: 11.7, height: 16.54 },
-  a4: {width: 8.27, height: 11.7 },
-  a5: {width: 5.83, height: 8.27 },
-  a6: {width: 4.13, height: 5.83 },
-};
-
-const unitToPixels = {
-  'px': 1,
-  'in': 96,
-  'cm': 37.8,
-  'mm': 3.78
-};
-
-function convertPrintParameterToInches(parameter: (string | number | undefined)): (number | undefined) {
-  if (typeof parameter === 'undefined')
-    return undefined;
-  let pixels: number;
-  if (helper.isNumber(parameter)) {
-    // Treat numbers as pixel values to be aligned with phantom's paperSize.
-    pixels = parameter as number;
-  } else if (helper.isString(parameter)) {
-    const text: string = parameter as string;
-    let unit = text.substring(text.length - 2).toLowerCase();
-    let valueText = '';
-    if (unitToPixels.hasOwnProperty(unit)) {
-      valueText = text.substring(0, text.length - 2);
-    } else {
-      // In case of unknown unit try to parse the whole parameter as number of pixels.
-      // This is consistent with phantom's paperSize behavior.
-      unit = 'px';
-      valueText = text;
-    }
-    const value = Number(valueText);
-    assert(!isNaN(value), 'Failed to parse parameter value: ' + text);
-    pixels = value * unitToPixels[unit];
-  } else {
-    throw new Error('page.pdf() Cannot handle parameter type: ' + (typeof parameter));
-  }
-  return pixels / 96;
-}
 
 type NetworkCookie = {
   name: string,
