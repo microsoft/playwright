d105f182fcab5a1631706d575281b06cc85e4355
Yury Semikhatsky <yurys@chromium.org>
2019-12-19 17:15:02 -0700

    browser(webkit): rebase WebKit on r253791 (#315)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/webkit/webkit"
 BASE_BRANCH="master"
-BASE_REVISION="8aa7db4d6eb617223c73c8bb0865653dc87f1fdd"
+BASE_REVISION="d203717920e7f6b6397be871ba86df396d0559db"
