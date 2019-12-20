455412a92923ae76236abb63ad5febfc44c4fac8
Dmitry Gozman <dgozman@gmail.com>
2019-12-19 18:03:25 -0800

    chore: update index.d.ts and index.js

--- src/firefox/ffApi.ts
+++ src/firefox/ffApi.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export { FFInterception } from './features/ffInterception';
-export { FFPermissions } from './features/ffPermissions';
-export { FFBrowser } from './ffBrowser';
-export { FFPlaywright } from './ffPlaywright';
-
+export { FFInterception as FirefoxInterception } from './features/ffInterception';
+export { FFPermissions as FirefoxPermissions } from './features/ffPermissions';
+export { FFBrowser as FirefoxBrowser } from './ffBrowser';
+export { FFPlaywright as Firefox } from './ffPlaywright';
