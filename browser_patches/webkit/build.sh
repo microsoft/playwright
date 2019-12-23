659a30e6276c325f394a89d01555d05282af4a35
Yury Semikhatsky <yurys@chromium.org>
2019-12-23 13:20:04 -0800

    feat(scripts): support Debug builds in pw_run.sh (#330)

--- browser_patches/webkit/build.sh
+++ browser_patches/webkit/build.sh
@@ -6,14 +6,19 @@ trap "cd $(pwd -P)" EXIT
 cd "$(dirname $0)"
 cd "checkout"
 
+BUILD_TYPE=--release
+if [[ -v WKDEBUG ]]; then
+  BUILD_TYPE=--debug
+fi
+
 if [[ "$(uname)" == "Darwin" ]]; then
-  ./Tools/Scripts/build-webkit --release --touch-events
+  ./Tools/Scripts/build-webkit $BUILD_TYPE --touch-events
 elif [[ "$(uname)" == "Linux" ]]; then
   # Check that WebKitBuild exists and is not empty.
   if ! [[ (-d ./WebKitBuild) && (-n $(ls -1 ./WebKitBuild/)) ]]; then
     yes | DEBIAN_FRONTEND=noninteractive ./Tools/Scripts/update-webkitgtk-libs
   fi
-  ./Tools/Scripts/build-webkit --gtk --release --touch-events MiniBrowser
+  ./Tools/Scripts/build-webkit --gtk $BUILD_TYPE --touch-events MiniBrowser
 else
   echo "ERROR: cannot upload on this platform!" 1>&2
   exit 1;
