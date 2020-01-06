806fbd1d05fe88130b6ccf28913d6e2336316f15
Yury Semikhatsky <yurys@chromium.org>
2020-01-06 09:26:34 -0800

    Revert "test: uncomment / fix webkit tests that time out (#379)"
    This reverts commit 2f3593bd9a3729152d085d0a89d634a546aa1b16.

--- test/page.spec.js
+++ test/page.spec.js
@@ -269,13 +269,11 @@ module.exports.describe = function({testRunner, expect, headless, playwright, FF
       await page.evaluate(async() => {
         // 1. Create a popup that Playwright is not connected to.
         const win = window.open(window.location.href, 'Title', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top=0,left=0');
-        while (window.document.readyState !== 'complete')
-          await new Promise(f => setTimeout(f, 100));
+        await new Promise(x => win.onload = x);
         // 2. In this popup, create an iframe that console.logs a message.
         win.document.body.innerHTML = `<iframe src='/consolelog.html'></iframe>`;
         const frame = win.document.querySelector('iframe');
-        while (frame.contentDocument.readyState !== 'complete')
-          await new Promise(f => setTimeout(f, 100));
+        await new Promise(x => frame.onload = x);
         // 3. After that, remove the iframe.
         frame.remove();
       });
