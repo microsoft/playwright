efc22979946a164384623de71d91c5d2b1357497
Yury Semikhatsky <yurys@chromium.org>
2020-01-28 13:36:19 -0800

    fix(webkit): do not poll readyState if target is paused before first navigation

--- test/page.spec.js
+++ test/page.spec.js
@@ -122,7 +122,7 @@ module.exports.describe = function({testRunner, expect, headless, playwright, FF
     });
   });
 
-  describe('Page.Events.Popup', function() {
+  fdescribe('Page.Events.Popup', function() {
     it('should work', async({page}) => {
       const [popup] = await Promise.all([
         new Promise(x => page.once('popup', x)),
