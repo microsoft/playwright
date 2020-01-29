598e72bf954c422de75a21f0ab3e81f42a02cd78
Yury Semikhatsky <yurys@chromium.org>
2020-01-28 16:27:38 -0800

    chore(webkit): rebase on r255314

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/webkit/webkit"
 BASE_BRANCH="master"
-BASE_REVISION="bf547c919b89d82d840e3f2182d0f380683f4816"
+BASE_REVISION="8aff3e156bbf71ab772ab9b75baa2e2e1035eb30"
