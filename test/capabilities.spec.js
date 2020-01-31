9b87298d5ecbc07b44c2ea8c2a972550dca7553b
Pavel Feldman <pavel.feldman@gmail.com>
2020-01-31 08:31:31 -0800

    test: start adding capability smoke tests

---
+++ test/capabilities.spec.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const utils = require('./utils');
+const { waitEvent } = utils;
+
+module.exports.describe = function({testRunner, expect}) {
+  const {describe, xdescribe, fdescribe} = testRunner;
+  const {it, fit, xit, dit} = testRunner;
+  const {beforeAll, beforeEach, afterAll, afterEach} = testRunner;
+
+  describe('Capabilities', function() {
+    it('Web Assembly should work', async function({page, server}) {
+      await page.goto(server.PREFIX + '/wasm/table2.html');
+      expect(await page.evaluate(() => loadTable())).toBe('42, 83');
+    });
+  });
+};
