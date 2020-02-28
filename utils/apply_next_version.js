9b51febf503ef46d8c0d7f34cff03ac138f9aff9
Andrey Lushnikov <aslushnikov@gmail.com>
2020-02-28 11:02:42 -0800

    feat: setup continuous deployment (#1159)
    This patch sets continuous deployment of playwright packages
    from tip-of-tree. These packages are released under the "@next",
    so it should be possible to install tip-of-tree version of Playwright
    from NPM using
    
    ```
    npm i playwright@next
    ```
    
    A few technical details:
    - This uses travis to drive deployments
    - This deploys from master and only if all browsers pass all tests on linux
    - This is an attempt to deploy just two packages: `playwright-core` and `playwright-firefox`. If this works out, I'll follow-up with other packages as well

--- utils/apply_next_version.js
+++ utils/apply_next_version.js
@@ -21,5 +21,6 @@ if (dashIndex !== -1)
   version = version.substring(0, dashIndex);
 version += '-next.' + Date.now();
 console.log('Setting version to ' + version);
-package.version = version;
-fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(package, undefined, 2) + '\n');
+
+execSync(`npm --no-git-tag-version version ${version}`);
+
