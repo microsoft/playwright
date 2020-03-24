31ca8608e1c7dcec208bb76b8d8962c10e05b2c5
Dmitry Gozman <dgozman@gmail.com>
2020-03-24 13:23:10 -0700

    chore: update release guide and helper script

--- utils/update_version.js
+++ utils/update_version.js
@@ -13,19 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 const fs = require('fs');
 const path = require('path');
-const {version} = require('../package.json');
+let version = process.argv[2];
+
+if (!version || !version.match(/^v\d+\.\d+\.\d+(-post)?$/)) {
+  console.error(`Malformed version "${version}"`);
+  console.error(`Correct examples:`);
+  console.error(`  update_version.js v1.0.0`);
+  console.error(`  update_version.js v1.0.0-post`);
+  process.exit(1);
+}
+
+version = version.substring(1);
+updatePackage(path.join(__dirname, '..', 'package.json'), packageJSON => {
+  packageJSON.version = version;
+});
 
 for (const packageName of ['playwright-chromium', 'playwright-firefox', 'playwright-webkit', 'playwright']) {
-  updatePackage(packageName, packageJSON => {
+  updatePackage(path.join(__dirname, '..', 'packages', packageName, 'package.json'), packageJSON => {
     packageJSON.version = version;
     packageJSON.dependencies['playwright-core'] = `=${version}`;
   });
 }
 
-function updatePackage(packageName, transform) {
-  const packageJSONPath = path.join(__dirname, '..', 'packages', packageName, 'package.json');
+function updatePackage(packageJSONPath, transform) {
   console.log(`Updating ${packageJSONPath} to ${version}.`);
   const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath));
   transform(packageJSON);
