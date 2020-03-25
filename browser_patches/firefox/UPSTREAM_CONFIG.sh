45053560c693a70643a6c33b41796e9342deac15
Andrey Lushnikov <lushnikov@chromium.org>
2020-03-24 22:44:11 -0700

    browser(firefox): roll Firefox
    Review URL: https://github.com/aslushnikov/juggler/commit/2fda0f5fc8120f19fcf2500aa1680fb169658411

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="45e34fdb8ef05b9fedc2af1ee6cd27334af4e6a5"
+BASE_REVISION="d140f8cc3e2aaaa8af73f92785041783a9a6b791"
