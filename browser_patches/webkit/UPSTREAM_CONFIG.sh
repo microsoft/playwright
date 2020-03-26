5d03be7ab1c70a71dfc89b9a2bcd1c47cb9508aa
Yury Semikhatsky <yurys@chromium.org>
2020-03-26 14:34:02 -0700

    feat(webkit): roll WebKit to r258828 (#1517) (#1556)
    Resolved conflicts: https://github.com/yury-s/webkit/commit/94ea16136d63d63dbab68ef0a0766aff1dcf9771

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/webkit/webkit"
 BASE_BRANCH="master"
-BASE_REVISION="15c61492b5630ee502084c75c6bbf5877dfa480d"
+BASE_REVISION="e64f6fa135bb9bbeeab2080019f538377c91b8f2"
