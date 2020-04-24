3cab73ace2955786cc80d0d9f81a463a4f6550b2
Pavel Feldman <pavel.feldman@gmail.com>
2020-04-23 17:21:24 -0800

    browser(webkit): roll to ToT 4/22/2020

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/webkit/webkit"
 BASE_BRANCH="master"
-BASE_REVISION="1cbe0700df266f49b1dcef8c215b4c398ba94420"
+BASE_REVISION="46190f2de4ab3b12e0a807e9af6813e8b793bab8"
