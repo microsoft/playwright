e2475061699751876481a35c57429d77be808a18
Pavel Feldman <pavel.feldman@gmail.com>
2020-05-07 18:37:15 -0700

    browser(firefox): roll ff to ToT 5/7/2020 (#2148)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="123c1a431fe7e2a72e9eb6e5077330a8365ab907"
+BASE_REVISION="3d00efe258323fe3c4f01280a348b435ec95c7ed"
