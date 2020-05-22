d0231a34e682f61bee2b35231eed4b3051e99380
Dmitry Gozman <dgozman@gmail.com>
2020-05-22 16:01:48 -0700

    test: unflake more cookie tests

--- test/headful.spec.js
+++ test/headful.spec.js
@@ -92,7 +92,11 @@ describe('Headful', function() {
       iframe.src = src;
       return promise;
     }, server.CROSS_PROCESS_PREFIX + '/grid.html');
-    await page.frames()[1].evaluate(`document.cookie = 'username=John Doe'`);
+    const documentCookie = await page.frames()[1].evaluate(() => {
+      document.cookie = 'username=John Doe';
+      return document.cookie;
+    });
+    expect(documentCookie).toBe('username=John Doe');
     await page.waitForTimeout(2000);
     const allowsThirdParty = CHROMIUM || FFOX;
     const cookies = await page.context().cookies(server.CROSS_PROCESS_PREFIX + '/grid.html');
