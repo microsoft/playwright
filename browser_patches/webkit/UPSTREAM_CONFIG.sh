fcc5f75baa959ca9266c6c2f9f1a38f3b909eb94
Yury Semikhatsky <yurys@chromium.org>
2020-06-03 12:21:27 -0700

    Revert "browser(webkit): roll to 06/03/20 (#2455)" (#2456)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/webkit/webkit"
 BASE_BRANCH="master"
-BASE_REVISION="66728e3e2ab6f33cb71d0bc1bd01e4b7886a5810"
+BASE_REVISION="b47ab419bc62ab0450737d49bfd23e9584b0c0db"
