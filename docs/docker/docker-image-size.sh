f71da3ba765545313065a0fa92cd589e4100b204
Andrey Lushnikov <lushnikov@chromium.org>
2020-07-10 17:17:40 -0700

    devops: add utility to count compressed docker image size
    This adds a new script to calculate docker image size with
    all parent layers.
    
    Note: take this metrics with a grain of salt, since in reality
    docker compresses and reuses layers.

---
+++ docs/docker/docker-image-size.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set +x
+
+# This script computes **compressed image size with all its layers**.
+# This solution is based on https://stackoverflow.com/a/55156181/314883
+
+DOCKER_IMAGE_NAME="docker-image-to-count-compressed-size"
+FILE_NAME="docker-image-to-count-compressed-size"
+RUN_AS=""
+if [[ "$(uname)" == "Darwin" ]]; then
+  RUN_AS=""
+elif [[ "$(uname)" == "Linux" ]]; then
+  RUN_AS="sudo"
+else
+  echo "ERROR: cannot run script on platform $(uname)"
+  exit 1;
+fi
+
+function cleanup() {
+  echo "-- Removing .tar if any"
+  rm -f "${FILE_NAME}.tar"
+  echo "-- Removing .tar.gz if any"
+  rm -f "${FILE_NAME}.tar.gz"
+  echo "-- Removing docker image if any"
+  $RUN_AS docker rmi "${DOCKER_IMAGE_NAME}:bionic" >/dev/null
+}
+
+trap "cleanup; cd $(pwd -P)" EXIT
+cd "$(dirname "$0")"
+
+echo "-- Building image..."
+$RUN_AS docker build -t "${DOCKER_IMAGE_NAME}:bionic" -f Dockerfile.bionic . >/dev/null
+echo "-- Saving .tar of the image..."
+$RUN_AS docker save "${DOCKER_IMAGE_NAME}:bionic" > "${FILE_NAME}.tar"
+echo "-- Compressing image..."
+gzip "${FILE_NAME}.tar" >/dev/null
+du -sh ${FILE_NAME}.tar.gz
