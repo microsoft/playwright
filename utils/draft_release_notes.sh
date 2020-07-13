0346c3a1dcc3f1df3cbaba0e1c58cf4006e05214
Andrey Lushnikov <aslushnikov@gmail.com>
2020-07-13 14:48:29 -0700

    chore: update release notes draft gen (#2932)

--- utils/draft_release_notes.sh
+++ utils/draft_release_notes.sh
@@ -8,27 +8,32 @@ cd "$(dirname $0)"
 git fetch --tags git@github.com:microsoft/playwright.git >/dev/null 2>/dev/null
 LAST_RELEASE=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-echo "## Browser Versions"
-echo
-node ./print_versions.js
-echo
 echo "## Highlights"
 echo
 echo "TODO: \`git diff ${LAST_RELEASE}:docs/api.md docs/api.md\`"
 echo
-echo "## Breaking API Changes"
+echo "## Browser Versions"
 echo
-echo "TODO: \`git diff ${LAST_RELEASE}:docs/api.md docs/api.md\`"
+node ./print_versions.js
 echo
 echo "## New APIs"
 echo
 echo "TODO: \`git diff ${LAST_RELEASE}:docs/api.md docs/api.md\`"
 echo
-echo "## Bug Fixes"
+CLOSED_ISSUES=$(./list_closed_issues.sh "${LAST_RELEASE}")
+ISSUES_COUNT=$(echo "${CLOSED_ISSUES}" | wc -l | xargs)
+echo "<details>"
+echo "  <summary><b>Issues Closed (${ISSUES_COUNT})</b></summary>"
 echo
-./list_closed_issues.sh "${LAST_RELEASE}"
+echo "${CLOSED_ISSUES}"
 echo
-echo "## Raw Notes"
-echo
-git log --pretty="%h - %s" "${LAST_RELEASE}"..HEAD
+echo "</details>"
 
+COMMITS=$(git log --pretty="%h - %s" "${LAST_RELEASE}"..HEAD)
+COMMITS_COUNT=$(echo "${COMMITS}" | wc -l | xargs)
+echo "<details>"
+echo "  <summary><b>Commits (${COMMITS_COUNT})</b></summary>"
+echo
+echo "${COMMITS}"
+echo
+echo "</details>"
