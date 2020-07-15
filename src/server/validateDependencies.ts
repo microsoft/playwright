fca0cd3b95b52b6b3a037a59d2e318fc501341f6
Andrey Lushnikov <lushnikov@chromium.org>
2020-07-15 14:18:57 -0700

    fix lint

--- src/server/validateDependencies.ts
+++ src/server/validateDependencies.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import * as fs from 'fs';
 import * as util from 'util';
 import * as path from 'path';
@@ -63,13 +78,11 @@ function lddAsync(filePath: string): Promise<{stdout: string, stderr: string, co
     },
   });
 
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     let stdout = '';
     let stderr = '';
     ldd.stdout.on('data', data => stdout += data);
     ldd.stderr.on('data', data => stderr += data);
-    ldd.on('close', (code) => {
-      resolve({stdout, stderr, code});
-    });
+    ldd.on('close', code => resolve({stdout, stderr, code}));
   });
 }
