a59220b06d368880520449946f0773123c00d30f
Dmitry Gozman <dgozman@gmail.com>
2020-07-27 18:40:21 -0700

    test: prepare fixtures test to run with rpc (#3190)

--- test/fixtures/closeme.js
+++ test/fixtures/closeme.js
@@ -1,11 +1,17 @@
 (async() => {
-  const { playwrightFile, browserTypeName, launchOptions, stallOnClose } = JSON.parse(process.argv[2]);
+  const { playwrightPath, browserTypeName, launchOptions, stallOnClose } = JSON.parse(process.argv[2]);
   if (stallOnClose) {
     launchOptions.__testHookGracefullyClose = () => {
       console.log(`(stalled=>true)`);
       return new Promise(() => {});
     };
   }
+
+  const path = require('path');
+  const { setUnderTest } = require(path.join(playwrightPath, 'lib', 'helper'));
+  setUnderTest();
+  const playwrightFile = path.join(playwrightPath, 'index');
+
   const browserServer = await require(playwrightFile)[browserTypeName].launchServer(launchOptions);
   browserServer.on('close', (exitCode, signal) => {
     console.log(`(exitCode=>${exitCode})`);
