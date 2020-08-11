812d7eefe9333cf286fff55e4015879cf77e340e
Yury Semikhatsky <yurys@chromium.org>
2020-08-11 13:59:23 -0700

    feat(webkit): bump to 1325 (#3388)

--- test/screencast.spec.js
+++ test/screencast.spec.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+require('./base.fixture');
 
 const fs = require('fs');
 const os = require('os');
