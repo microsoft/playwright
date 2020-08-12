a4a07c46c365cf68f676df9fac7dca94502c6ce9
Pavel Feldman <pavel.feldman@gmail.com>
2020-08-12 14:57:37 -0700

    test: support --grep, --forbid-only (#3417)

--- test/runner/index.js
+++ test/runner/index.js
@@ -27,40 +27,53 @@ class NullReporter {}
 
 program
   .version('Version ' + require('../../package.json').version)
-  .option('--timeout <timeout>', 'timeout', 10000)
-  .option('--reporter <reporter>', 'reporter to use', '')
-  .option('--max-workers <maxWorkers>', 'max workers to use', Math.ceil(require('os').cpus().length / 2))
-  .option('--retries <retries>', 'number of times to retry a failing test', 1)
+  .option('--forbid-only', 'Fail if exclusive test(s) encountered', false)
+  .option('-g, --grep <grep>', 'Only run tests matching this string or regexp', '.*')
+  .option('-j, --jobs <jobs>', 'Number of concurrent jobs for --parallel; use 1 to run in serial, default: (number of CPU cores / 2)', Math.ceil(require('os').cpus().length / 2))
+  .option('--reporter <reporter>', 'Specify reporter to use', '')
+  .option('--timeout <timeout>', 'Specify test timeout threshold (in milliseconds), default: 10000', 10000)
   .action(async (command) => {
     // Collect files
     const files = [];
     collectFiles(path.join(process.cwd(), 'test'), command.args, files);
     const rootSuite = new Mocha.Suite('', new Mocha.Context(), true);
 
-    console.log(`Transpiling ${files.length} test files`);
+    console.log(`Parsing ${files.length} test files`);
+    let total = 0;
     // Build the test model, suite per file.
     for (const file of files) {
       const mocha = new Mocha({
-        ui: fixturesUI.bind(null, true),
+        forbidOnly: command.forbidOnly || undefined,
+        reporter: NullReporter,
         retries: command.retries,
         timeout: command.timeout,
-        reporter: NullReporter
+        ui: fixturesUI.bind(null, true),
       });
+      if (command.grep)
+        mocha.grep(command.grep);
       mocha.addFile(file);
-      mocha.suite.title = path.basename(file);
-      mocha.suite.root = false;
-      rootSuite.suites.push(mocha.suite);
+      let runner;
       await new Promise(f => {
-        const runner = mocha.run(f);
+        runner = mocha.run(f);
         runner.on(constants.EVENT_RUN_BEGIN, () => {
           process.stdout.write(colors.yellow('\u00B7'));
         });
       });
+      total += runner.grepTotal(mocha.suite);
+
+      rootSuite.addSuite(mocha.suite);
+      mocha.suite.title = path.basename(file);
     }
+
+    if (rootSuite.hasOnly())
+      rootSuite.filterOnly();
     console.log();
+    total = Math.min(total, rootSuite.total()); // First accounts for grep, second for only.
+    console.log(`Running ${total} tests using ${Math.min(command.jobs, total)} workers`);
 
     const runner = new Runner(rootSuite, {
-      maxWorkers: command.maxWorkers,
+      grep: command.grep,
+      jobs: command.jobs,
       reporter: command.reporter,
       retries: command.retries,
       timeout: command.timeout,
