359209115518f743de95bb5dd9fb5e96f5cab290
Joel Einbinder <joel.einbinder@gmail.com>
2020-08-13 14:15:13 -0700

    pavel comments

--- test/runner/transform.js
+++ test/runner/transform.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const crypto = require('crypto');
 const os = require('os');
 const path = require('path');
@@ -13,7 +28,7 @@ const cacheDir = path.join(os.tmpdir(), 'playwright-transform-cache');
  * @return {string}
  */
 function calculateCachePath(content, filePath) {
-  const hash = crypto.createHash('md5').update(content).update(filePath).update(String(version)).digest('hex');
+  const hash = crypto.createHash('sha1').update(content).update(filePath).update(String(version)).digest('hex');
   const fileName = path.basename(filePath, path.extname(filePath)).replace(/\W/g, '') + '_' + hash;
 
   return path.join(cacheDir, hash[0] + hash[1], fileName);
