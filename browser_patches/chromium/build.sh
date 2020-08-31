3aa0bc1f7434a2e278b9daa2f0982fa1698db112
Andrey Lushnikov <lushnikov@chromium.org>
2020-08-31 10:47:07 -0700

    devops: bundle ffmpeg with chromium
    This starts bundling FFMPEG v4.3.1 binary and license agreement with Chromium
    archives.
    
    The FFMPEG static binaries are taken from the [official website](https://ffmpeg.org/)
    and are copied to our Azure CDN, since download link for linux static
    release builds is not versioned (see https://johnvansickle.com/ffmpeg/).
    
    References #3680

--- browser_patches/chromium/build.sh
+++ browser_patches/chromium/build.sh
@@ -12,34 +12,53 @@ cd output
 
 FOLDER_NAME=""
 ZIP_NAME=""
-FILES_TO_REMOVE=()
+FFMPEG_URL=""
+FFMPEG_BIN_PATH=""
+FFMPEG_LICENSE_PATH=""
+CHROMIUM_FILES_TO_REMOVE=()
+
 
 if [[ $1 == "--win32" ]]; then
   FOLDER_NAME="Win"
   ZIP_NAME="chrome-win.zip"
-  FILES_TO_REMOVE+=("chrome-win/interactive_ui_tests.exe")
+  CHROMIUM_FILES_TO_REMOVE+=("chrome-win/interactive_ui_tests.exe")
+  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-win32.zip"
+  FFMPEG_BIN_PATH="ffmpeg-${FFMPEG_VERSION}-win32-static/bin/ffmpeg.exe"
+  FFMPEG_LICENSE_PATH="ffmpeg-${FFMPEG_VERSION}-win32-static/LICENSE.txt"
 elif [[ $1 == "--win64" ]]; then
   FOLDER_NAME="Win_x64"
   ZIP_NAME="chrome-win.zip"
-  FILES_TO_REMOVE+=("chrome-win/interactive_ui_tests.exe")
+  CHROMIUM_FILES_TO_REMOVE+=("chrome-win/interactive_ui_tests.exe")
+  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-win64.zip"
+  FFMPEG_BIN_PATH="ffmpeg-${FFMPEG_VERSION}-win64-static/bin/ffmpeg.exe"
+  FFMPEG_LICENSE_PATH="ffmpeg-${FFMPEG_VERSION}-win64-static/LICENSE.txt"
 elif [[ $1 == "--mac" ]]; then
   FOLDER_NAME="Mac"
   ZIP_NAME="chrome-mac.zip"
+  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-mac.zip"
+  FFMPEG_BIN_PATH="ffmpeg-${FFMPEG_VERSION}-macos64-static/bin/ffmpeg"
+  FFMPEG_LICENSE_PATH="ffmpeg-${FFMPEG_VERSION}-macos64-static/LICENSE.txt"
 elif [[ $1 == "--linux" ]]; then
   FOLDER_NAME="Linux_x64"
   ZIP_NAME="chrome-linux.zip"
+  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-linux.zip"
+  FFMPEG_BIN_PATH="ffmpeg-${FFMPEG_VERSION}-amd64-static/ffmpeg"
+  FFMPEG_LICENSE_PATH="ffmpeg-${FFMPEG_VERSION}-amd64-static/GPLv3.txt"
 else
   echo "ERROR: unknown platform to build: $1"
   exit 1
 fi
 
-URL="https://storage.googleapis.com/chromium-browser-snapshots/${FOLDER_NAME}/${UPSTREAM_CHROMIUM_REVISION}/${ZIP_NAME}"
-curl --output upstream.zip "${URL}"
-
-unzip upstream.zip
-
-for file in ${FILES_TO_REMOVE[@]}; do
+CHROMIUM_URL="https://storage.googleapis.com/chromium-browser-snapshots/${FOLDER_NAME}/${UPSTREAM_CHROMIUM_REVISION}/${ZIP_NAME}"
+curl --output chromium-upstream.zip "${CHROMIUM_URL}"
+unzip chromium-upstream.zip
+for file in ${CHROMIUM_FILES_TO_REMOVE[@]}; do
   rm -f "${file}"
 done
+
+curl --output ffmpeg-upstream.zip "${FFMPEG_URL}"
+unzip ffmpeg-upstream.zip
+cp "$FFMPEG_BIN_PATH" ${ZIP_NAME%.zip}
+cp "$FFMPEG_LICENSE_PATH" ${ZIP_NAME%.zip}/ffmpeg-license.txt
 
 zip --symlinks -r build.zip ${ZIP_NAME%.zip}
