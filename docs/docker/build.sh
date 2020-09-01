dfd9e258f78bdd9b0c1eb24586c6d5672acfb1f1
Andrey Lushnikov <lushnikov@chromium.org>
2020-09-01 00:03:16 -0700

    devops: switch docker publishing to a bash script
    It turns out Github Actions have docker pre-installed.
    
    This patch unifies the codepaths that we use to build docker image
    locally and to publish it from CI.
    
    As a result, there's no extra `--prepare-context` argument in
    `//docs/docker/build.sh` that was used solely on CI.
    
    References #2926

--- docs/docker/build.sh
+++ docs/docker/build.sh
@@ -3,7 +3,7 @@ set -e
 set +x
 
 if [[ ($1 == '--help') || ($1 == '-h') ]]; then
-  echo "usage: $(basename $0) [--prepare-only]"
+  echo "usage: $(basename $0)"
   echo
   echo "Build Playwright docker image and tag it as 'playwright:localbuild'."
   echo "Once image is built, you can run it with"
@@ -12,22 +12,12 @@ if [[ ($1 == '--help') || ($1 == '-h') ]]; then
   echo ""
   echo "NOTE: this requires on Playwright dependencies to be installed with 'npm install'"
   echo "      and Playwright itself being built with 'npm run build'"
-  echo
-  echo "  --prepare-context    prepare docker context and skip building."
-  echo "                       This is to defer building & publishing to Docker Github Action."
   echo ""
   exit 0
 fi
 
-PREPARE_CONTEXT_ONLY=""
-if [[ $1 == "--prepare-context" ]]; then
-  PREPARE_CONTEXT_ONLY="1"
-fi
-
 function cleanup() {
-  if [[ -z "${PREPARE_CONTEXT_ONLY}" ]]; then
-    rm -f "playwright.tar.gz"
-  fi
+  rm -f "playwright.tar.gz"
 }
 
 trap "cleanup; cd $(pwd -P)" EXIT
@@ -36,9 +26,5 @@ cd "$(dirname "$0")"
 # We rely on `./playwright.tar.gz` to download browsers into the docker
 # image.
 node ../../packages/build_package.js playwright ./playwright.tar.gz
-
-if [[ -n "${PREPARE_CONTEXT_ONLY}" ]]; then
-  exit 0
-fi
 
 docker build -t "playwright:localbuild" -f Dockerfile.bionic .
