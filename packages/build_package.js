5364e3283ff11e211d9eb07c25fc0bf56c016e14
Andrey Lushnikov <aslushnikov@gmail.com>
2020-09-03 10:29:43 -0700

    devops: bake commit SHA inside npm package (#3754)
    This bakes in `//commitinfo` file with the SHA of the commit that
    produced given NPM package.
    
    Fixes #3743

--- packages/build_package.js
+++ packages/build_package.js
@@ -157,7 +157,12 @@ if (!args.some(arg => arg === '--no-cleanup')) {
     browser.download = package.browsers.includes(browser.name);
   await writeToPackage('browsers.json', JSON.stringify(browsersJSON, null, 2));
 
-  // 6. Run npm pack
+  // 6. Bake commit SHA into the package
+  const commitSHA = spawnSync('git', ['rev-parse', 'HEAD'], {cwd: __dirname, encoding: 'utf8'});
+  console.log(commitSHA.stdout.trim());
+  await writeToPackage('commitinfo', commitSHA.stdout.trim());
+
+  // 7. Run npm pack
   const shell = os.platform() === 'win32';
   const {stdout, stderr, status} = spawnSync('npm', ['pack'], {cwd: packagePath, encoding: 'utf8', shell});
   if (status !== 0) {
