f4d7d1ceb05d1ff6bd0fbd19ce07d6f863a70d40
Andrey Lushnikov <lushnikov@chromium.org>
2020-09-08 16:47:04 -0600

    feat: actually bundle FFMPEG binaries with NPM packages
    This patch:
    - adds FFMPEG binaries to the NPM packages
    - adds a screencast test to make sure that screencast works
    
    Note: this currently relies on private screencast APIs.

---
+++ packages/installation-tests/screencast.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const requireName = process.argv[2];
+let success = {
+  'playwright': ['chromium', 'firefox', 'webkit'],
+  'playwright-chromium': ['chromium'],
+  'playwright-firefox': ['firefox'],
+  'playwright-webkit': ['webkit'],
+}[requireName];
+if (process.argv[3] === 'none')
+  success = [];
+if (process.argv[3] === 'all')
+  success = ['chromium', 'firefox', 'webkit'];
+
+const playwright = require(requireName);
+const path = require('path');
+const fs = require('fs');
+
+(async () => {
+  for (const browserType of success) {
+    try {
+      const browser = await playwright[browserType].launch({
+        _videosPath: __dirname,
+      });
+      const context = await browser.newContext({
+        _recordVideos: {width: 320, height: 240},
+      });
+      const page = await context.newPage();
+      const video = await page.waitForEvent('_videostarted');
+      const [videoFile] = await Promise.all([
+        video.path(),
+        context.close(),
+      ]);
+      await browser.close();
+      if (!fs.existsSync(videoFile)) {
+        console.error(`Package "${requireName}", browser "${browserType}" should have created screencast!`);
+        process.exit(1);
+      }
+    } catch (err) {
+      console.error(`Should be able to launch ${browserType} from ${requireName}`);
+      console.error(err);
+      process.exit(1);
+    }
+  }
+})();
