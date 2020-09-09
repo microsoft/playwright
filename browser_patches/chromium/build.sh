638c77c8e2518c565dd9a83dd3842f02a6f5db80
Andrey Lushnikov <aslushnikov@gmail.com>
2020-09-08 17:05:21 -0700

    devops: stop bundling FFMPEG with Chromium (#3806)
    - This stops bundling FFMPEG with Chromium
    - Stop supporting build numbers as fractional parts for Chromium
    revisions

--- browser_patches/chromium/build.sh
+++ browser_patches/chromium/build.sh
@@ -9,18 +9,11 @@ rm -rf output
 mkdir -p output
 cd output
 
-BUILD_NUMBER=$(head -1 ../BUILD_NUMBER)
-# Support BUILD_NUMBER in the form of <CRREV>.<GENERATION>
-# This will allow us to bump generation to produce new builds.
-CRREV="${BUILD_NUMBER%.*}"
+CRREV=$(head -1 ../BUILD_NUMBER)
 
 CHROMIUM_URL=""
 CHROMIUM_FOLDER_NAME=""
 CHROMIUM_FILES_TO_REMOVE=()
-
-FFMPEG_VERSION="4.3.1"
-FFMPEG_URL=""
-FFMPEG_BIN_PATH=""
 
 PLATFORM="$1"
 if [[ -z "${PLATFORM}" ]]; then
@@ -41,24 +34,16 @@ if [[ "${PLATFORM}" == "--win32" ]]; then
   CHROMIUM_URL="https://storage.googleapis.com/chromium-browser-snapshots/Win/${CRREV}/chrome-win.zip"
   CHROMIUM_FOLDER_NAME="chrome-win"
   CHROMIUM_FILES_TO_REMOVE+=("chrome-win/interactive_ui_tests.exe")
-  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-win32.zip"
-  FFMPEG_BIN_PATH="ffmpeg.exe"
 elif [[ "${PLATFORM}" == "--win64" ]]; then
   CHROMIUM_URL="https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/${CRREV}/chrome-win.zip"
   CHROMIUM_FOLDER_NAME="chrome-win"
   CHROMIUM_FILES_TO_REMOVE+=("chrome-win/interactive_ui_tests.exe")
-  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-win64.zip"
-  FFMPEG_BIN_PATH="ffmpeg.exe"
 elif [[ "${PLATFORM}" == "--mac" ]]; then
   CHROMIUM_URL="https://storage.googleapis.com/chromium-browser-snapshots/Mac/${CRREV}/chrome-mac.zip"
   CHROMIUM_FOLDER_NAME="chrome-mac"
-  FFMPEG_URL="https://playwright2.blob.core.windows.net/builds/ffmpeg/${FFMPEG_VERSION}/ffmpeg-mac.zip"
-  FFMPEG_BIN_PATH="ffmpeg"
 elif [[ "${PLATFORM}" == "--linux" ]]; then
   CHROMIUM_URL="https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CRREV}/chrome-linux.zip"
   CHROMIUM_FOLDER_NAME="chrome-linux"
-  # Even though we could bundle ffmpeg on Linux (2.5MB zipped), we
-  # prefer rely on system-installed ffmpeg instead.
 else
   echo "ERROR: unknown platform to build: $1"
   exit 1
@@ -71,11 +56,5 @@ unzip chromium-upstream.zip
 for file in ${CHROMIUM_FILES_TO_REMOVE[@]}; do
   rm -f "${file}"
 done
-
-if [[ -n "${FFMPEG_URL}" ]]; then
-  curl --output ffmpeg-upstream.zip "${FFMPEG_URL}"
-  unzip ffmpeg-upstream.zip
-  cp "$FFMPEG_BIN_PATH" "${CHROMIUM_FOLDER_NAME}"
-fi
 
 zip --symlinks -r build.zip "${CHROMIUM_FOLDER_NAME}"
