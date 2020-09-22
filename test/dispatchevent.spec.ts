84bd67cec9ad703fe9bbbc6ab11b63a15399eab3
Yury Semikhatsky <yurys@chromium.org>
2020-09-22 11:27:28 -0700

    Enable one more passing "dispatch drag drop events" tests

--- test/dispatchevent.spec.ts
+++ test/dispatchevent.spec.ts
@@ -137,9 +137,7 @@ it('should dispatch drag drop events', async ({page, server}) => {
   }, {source, target})).toBeTruthy();
 });
 
-it('should dispatch drag drop events', (test, parameters) => {
-  test.fail(options.WEBKIT(parameters));
-}, async ({page, server}) => {
+it('should dispatch drag drop events', async ({page, server}) => {
   await page.goto(server.PREFIX + '/drag-n-drop.html');
   const dataTransfer = await page.evaluateHandle(() => new DataTransfer());
   const source = await page.$('#source');
