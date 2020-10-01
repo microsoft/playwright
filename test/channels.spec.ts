b9dcfb9909e099c1b1eefbcbd4b76a7719eecf5a
Andrey Lushnikov <aslushnikov@gmail.com>
2020-09-30 17:23:13 -0700

    test: minor fixes (#4024)
    - missing await
    - missing `defaultBrowserOptions` that's important to respect `FFPATH` (and other custom browsers)

--- test/channels.spec.ts
+++ test/channels.spec.ts
@@ -162,8 +162,8 @@ it('should scope browser handles', async ({browserType, defaultBrowserOptions}) 
   await expectScopeState(browserType, GOLDEN_PRECONDITION);
 });
 
-it('should work with the domain module', async ({ domain, browserType }) => {
-  const browser = await browserType.launch();
+it('should work with the domain module', async ({ domain, browserType, defaultBrowserOptions }) => {
+  const browser = await browserType.launch(defaultBrowserOptions);
   const page = await browser.newPage();
   const result = await page.evaluate(() => 1 + 1);
   expect(result).toBe(2);
