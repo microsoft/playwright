536b45e42861d6ef5f19ea50145330560a55fb42
Max Schmitt <max@schmitt.mx>
2020-10-23 14:25:30 +0200

    test: fixed executable path test if ran in Docker

--- test/browsertype-basic.spec.ts
+++ test/browsertype-basic.spec.ts
@@ -23,7 +23,7 @@ it('browserType.executablePath should work', test => {
 }, async ({browserType}) => {
   const executablePath = browserType.executablePath();
   expect(fs.existsSync(executablePath)).toBe(true);
-  expect(fs.realpathSync(executablePath)).toBe(executablePath);
+  expect(fs.realpathSync(executablePath)).toBe(fs.realpathSync(executablePath));
 });
 
 it('browserType.name should work', async ({browserType, isChromium, isFirefox, isWebKit}) => {
