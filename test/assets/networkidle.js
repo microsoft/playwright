20502031804a9805757bec08401ab6a66da386a3
Dmitry Gozman <dgozman@gmail.com>
2020-11-04 06:45:49 -0800

    test: try to unflake network idle tests
    I think that we are too slow to fire the second fetch during 500ms,
    and so network idle happens prematurely.
    
    The fix is to manually trigger the second fetch early enough.

--- test/assets/networkidle.js
+++ test/assets/networkidle.js
@@ -1,18 +1,12 @@
-async function sleep(delay) {
-  return new Promise(resolve => setTimeout(resolve, delay));
-}
-
 async function main() {
   window.ws = new WebSocket('ws://localhost:' + window.location.port + '/ws');
   window.ws.addEventListener('message', message => {});
 
-  const roundOne = Promise.all([
-    fetch('fetch-request-a.js'),
-  ]);
-
-  await roundOne;
-  await sleep(50);
-  await fetch('fetch-request-d.js');
+  fetch('fetch-request-a.js');
+  window.top.fetchSecond = () => {
+    // Do not return the promise here.
+    fetch('fetch-request-b.js');
+  };
 }
 
 main();
