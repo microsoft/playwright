bd75fb1c55658b6734b0b16a170c0aae88b289b3
Yury Semikhatsky <yurys@chromium.org>
2020-11-09 11:30:12 -0800

    browser(webkit): roll to 11-09 (#4384)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://git.webkit.org/git/WebKit.git"
 BASE_BRANCH="master"
-BASE_REVISION="d02f1dd23a7449c5401f3c6382462eadd21f6e97"
+BASE_REVISION="d1313e22e40ba04ebb1fdb478ff0fdc0da700a88"
