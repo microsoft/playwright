77907a77033039d29d3fce7d73434ff10053c111
Pavel Feldman <pavel.feldman@gmail.com>
2020-12-04 17:48:51 -0800

    test(click): add a failing test for click w/ scroll

---
+++ test/click-scroll.spec.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { it } from './fixtures';
+
+it('should not hit scroll bar', (test, { browserName, platform }) => {
+  test.fixme(browserName === 'webkit' && platform === 'darwin');
+}, async ({page, server}) => {
+  await page.setContent(`
+    <style>
+      .categories { width: 180px; display: flex; overflow-x: scroll; }
+      button { flex: none; height: 28px; }
+    </style>
+    <div class="categories">
+      <button>One</button>
+      <button>Two</button>
+      <button>Three</button>
+      <button>Story</button>
+      <button>More</button>
+      <button>Items</button>
+      <button>Here</button>
+    </div>
+    `);
+  await page.click('text=Story', { timeout: 2000 });
+});
