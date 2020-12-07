e75ebc17521db343770036c8025fa9d414fbd0b9
Andrey Lushnikov <aslushnikov@gmail.com>
2020-12-07 10:27:50 -0800

    browser(firefox): roll Firefox to Dec, 7 2020 (#4622)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="72d163a63c6101d59f606787f2d0ad4245c9ed79"
+BASE_REVISION="e2139f2032ae3a4450371f5e8aca8d2a10d45a6c"
