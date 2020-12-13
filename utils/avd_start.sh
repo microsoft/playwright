978c0ffa84500a61462d2b5b4c6548b5ec26f723
Max Schmitt <max@schmitt.mx>
2020-12-13 22:35:33 +0100

    Killing previous emulators

--- utils/avd_start.sh
+++ utils/avd_start.sh
@@ -8,6 +8,9 @@ if [[ -z "${ANDROID_HOME}" ]]; then
     export ANDROID_SDK_ROOT=${SDKDIR}
 fi
 
+echo "Killing previous emulators"
+adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
+
 echo "Starting emulator"
 nohup ${ANDROID_HOME}/emulator/emulator -avd android30 -no-audio -no-snapshot -no-window -gpu swiftshader_indirect &
 ${ANDROID_HOME}/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
