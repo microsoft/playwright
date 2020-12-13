ca389e1b193106b1711f499f34794090d802f5d3
Max Schmitt <max@schmitt.mx>
2020-12-13 20:37:34 +0100

    delete existing avd

--- utils/avd_recreate.sh
+++ utils/avd_recreate.sh
@@ -8,6 +8,7 @@ if [[ -z "${ANDROID_HOME}" ]]; then
     export ANDROID_SDK_ROOT=${SDKDIR}
 fi
 
+${ANDROID_HOME}/tools/bin/avdmanager delete avd --name android30
 echo "y" | ${ANDROID_HOME}/tools/bin/sdkmanager --install "system-images;android-30;google_apis;x86"
 echo "no" | ${ANDROID_HOME}/tools/bin/avdmanager create avd --force --name android30 --device "Nexus 5X" -k 'system-images;android-30;google_apis;x86'
 ${ANDROID_HOME}/emulator/emulator -list-avds
