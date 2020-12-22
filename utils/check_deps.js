9d6a8527679df29c1575b6de8b3f7dd2c270ac3d
Dmitry Gozman <dgozman@gmail.com>
2020-12-22 09:55:25 -0800

    chore: fix check-deps

--- utils/check_deps.js
+++ utils/check_deps.js
@@ -34,7 +34,12 @@ async function checkDeps() {
   });
   const sourceFiles = program.getSourceFiles();
   const errors = [];
+  const usedDeps = new Set();
   sourceFiles.filter(x => !x.fileName.includes('node_modules')).map(x => visit(x, x.fileName));
+  for (const key of Object.keys(DEPS)) {
+    if (!usedDeps.has(key) && DEPS[key].length)
+      errors.push(`Stale DEPS entry "${key}"`);
+  }
   for (const error of errors)
     console.log(error);
   if (errors.length) {
@@ -56,10 +61,10 @@ async function checkDeps() {
   }
 
   function allowImport(from, to) {
-    if (!to.startsWith('src' + path.sep))
+    if (!to.startsWith(src + path.sep))
       return true;
-    from = from.substring(from.indexOf('src' + path.sep)).replace(/\\/g, '/');
-    to = to.substring(to.indexOf('src' + path.sep)).replace(/\\/g, '/');
+    from = path.relative(root, from).replace(/\\/g, '/');
+    to = path.relative(root, to).replace(/\\/g, '/');
     const fromDirectory = from.substring(0, from.lastIndexOf('/') + 1);
     const toDirectory = to.substring(0, to.lastIndexOf('/') + 1);
     if (fromDirectory === toDirectory)
@@ -73,6 +78,7 @@ async function checkDeps() {
       from = from.substring(0, from.lastIndexOf('/') + 1);
     }
 
+    usedDeps.add(from);
     for (const dep of DEPS[from]) {
       if (to === dep || toDirectory === dep)
         return true;
@@ -113,9 +119,8 @@ DEPS['src/server/common/'] = [];
 DEPS['src/server/injected/'] = ['src/server/common/'];
 
 // Electron and Clank use chromium internally.
-DEPS['src/server/android/'] = [...DEPS['src/server/'], 'src/server/chromium/', 'src/protocol/transport.ts'];
+DEPS['src/server/android/'] = [...DEPS['src/server/'], 'src/server/chromium/', 'src/protocol/'];
 DEPS['src/server/electron/'] = [...DEPS['src/server/'], 'src/server/chromium/'];
-DEPS['src/server/clank/'] = [...DEPS['src/server/'], 'src/server/chromium/'];
 
 DEPS['src/server/playwright.ts'] = [...DEPS['src/server/'], 'src/server/chromium/', 'src/server/webkit/', 'src/server/firefox/', 'src/server/android/', 'src/server/electron/'];
 DEPS['src/driver.ts'] = DEPS['src/inprocess.ts'] = DEPS['src/browserServerImpl.ts'] = ['src/**'];
