277d255fc32b68a633203cef582a3b76d286f786
Pavel Feldman <pavel.feldman@gmail.com>
2020-12-23 19:35:43 -0800

    chore: brush up md processing (#4819)

--- utils/doclint/cli.js
+++ utils/doclint/cli.js
@@ -21,7 +21,7 @@ const path = require('path');
 const os = require('os');
 const Source = require('./Source');
 const Message = require('./Message');
-const { parseMd, renderMd, parseArgument } = require('./../parse_md');
+const { parseMd, renderMd, parseArgument, applyTemplates } = require('./../parse_md');
 const { spawnSync } = require('child_process');
 const preprocessor = require('./preprocessor');
 
@@ -57,39 +57,25 @@ async function run() {
     const header = fs.readFileSync(path.join(PROJECT_DIR, 'docs-src', 'api-header.md')).toString();
     const body = fs.readFileSync(path.join(PROJECT_DIR, 'docs-src', 'api-body.md')).toString();
     const footer = fs.readFileSync(path.join(PROJECT_DIR, 'docs-src', 'api-footer.md')).toString();
-    let params = fs.readFileSync(path.join(PROJECT_DIR, 'docs-src', 'api-params.md')).toString();
+    const params = fs.readFileSync(path.join(PROJECT_DIR, 'docs-src', 'api-params.md')).toString();
 
-    const paramsMap = new Map();
-    for (const node of parseMd(params)) {
-      if (node.h2.endsWith('-list')) {
-        node.children = node.children.map(child => paramsMap.get(child.li));
-        paramsMap.set('%%-' + node.h2 + '-%%', node);
-        continue;
-      }
-      if (node.children[1])
-        node.children[0].li += ' ' + node.children[1].text;
-      paramsMap.set('%%-' + node.h2 + '-%%', node.children[0]);
-    }
-  
     // Generate signatures
     {
-      const nodes = parseMd(body);
+      const nodes = applyTemplates(parseMd(body), parseMd(params));
       const signatures = new Map();
       for (const clazz of nodes) {
-        clazz.h3 = clazz.h1;
-        clazz.h1 = undefined;
+        clazz.type = 'h3';
         for (const member of clazz.children) {
-          if (!member.h2)
+          if (member.type !== 'h2')
             continue;
-          member.h4 = member.h2;
-          member.h2 = undefined;
+          member.type = 'h4';
 
-          let match = member.h4.match(/(event|method|namespace|async method|): (JS|CDP|[A-Z])([^.]+)\.(.*)/);
+          let match = member.text.match(/(event|method|namespace|async method|): (JS|CDP|[A-Z])([^.]+)\.(.*)/);
           if (!match)
             continue;
 
           if (match[1] === 'event') {
-            member.h4 = `${match[2].toLowerCase() + match[3]}.on('${match[4]}')`;
+            member.text = `${match[2].toLowerCase() + match[3]}.on('${match[4]}')`;
             continue;
           }
 
@@ -100,72 +86,53 @@ async function run() {
             const nonArgChildren = [];
             const optionsContainer = [];
             for (const item of member.children) {
-              if (item.li && item.liType === 'default') {
-                const { type } = parseArgument(item.li);
+              if (item.type === 'li' && item.liType === 'default') {
+                const { type } = parseArgument(item.text);
                 if (match[1] === 'method')
-                  item.li = `returns: ${type}`;
+                  item.text = `returns: ${type}`;
                 else
-                  item.li = `returns: <[Promise]${type}>`;
+                  item.text = `returns: <[Promise]${type}>`;
                 returnContainer.push(item);
                 continue;
               }
-              if (!item.h3) {
+              if (item.type !== 'h3') {
                 nonArgChildren.push(item);
                 continue;
               }
-              if (item.h3.startsWith('param:')) {
-                if (item.h3.includes('=')) {
-                  const [name, key] = item.h3.split(' = ');
-                  item.h3 = name;
-                  const template = paramsMap.get(key);
-                  if (!template)
-                    throw new Error('Bad template: ' + kkey);
-                  args.push(parseArgument(template.li));
-                  argChildren.push(template);
-                } else {
-                  const param = item.children[0];
-                  if (item.children[1])
-                    param.li += ' ' + item.children[1].text;
-                  args.push(parseArgument(param.li));
-                  argChildren.push(param);
-                }
+              if (item.text.startsWith('param:')) {
+                const param = item.children[0];
+                if (item.children[1])
+                  param.text += ' ' + item.children[1].text;
+                args.push(parseArgument(param.text));
+                argChildren.push(param);
               }
-              if (item.h3.startsWith('option:')) {
+              if (item.text.startsWith('option:')) {
                 let optionsNode = optionsContainer[0];
                 if (!optionsNode) {
                   optionsNode = {
-                    li: '`options` <[Object]>',
+                    type: 'li',
+                    text: '`options` <[Object]>',
                     liType: 'default',
                     children: [],
                   };
                   optionsContainer.push(optionsNode);
-                  args.push(parseArgument(optionsNode.li));
+                  args.push(parseArgument(optionsNode.text));
                 }
-                if (item.h3.includes('=')) {
-                  const [name, key] = item.h3.split(' = ');
-                  const template = paramsMap.get(key);
-                  if (!template)
-                    throw new Error('Bad template: ' + key);
-                  if (item.h3.includes('-inline-')) {
-                    optionsNode.children.push(...template.children);
-                  } else {
-                    item.h3 = name;
-                    optionsNode.children.push(template);
-                  }
-                } else {
-                  const param = item.children[0];
-                  if (item.children[1])
-                    param.li += ' ' + item.children[1].text;
-                  optionsNode.children.push(param);
-                }
+                const param = item.children[0];
+                if (item.children[1])
+                  param.text += ' ' + item.children[1].text;
+                optionsNode.children.push(param);
               }
             }
             if (match[1] === 'async method' && !returnContainer[0]) {
               returnContainer.push({
-                li: 'returns: <[Promise]>',
+                type: 'li',
+                text: 'returns: <[Promise]>',
                 liType: 'default'
               });
             }
+            if (optionsContainer[0])
+              optionsContainer[0].children.sort((o1, o2) => o1.text.localeCompare(o2.text));
             member.children = [...argChildren, ...optionsContainer, ...returnContainer, ...nonArgChildren];
 
             const tokens = [];
@@ -190,11 +157,11 @@ async function run() {
             const signature = tokens.join('');
             const methodName = `${match[2].toLowerCase() + match[3]}.${match[4]}`;
             signatures.set(methodName, signature);
-            member.h4 = `${methodName}(${signature})`;
+            member.text = `${methodName}(${signature})`;
           }
 
           if (match[1] === 'namespace') {
-            member.h4 = `${match[2].toLowerCase() + match[3]}.${match[4]}`;
+            member.text = `${match[2].toLowerCase() + match[3]}.${match[4]}`;
             continue;
           }
         }
