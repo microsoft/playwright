3ff81fe172e294c5c2fea13decf60d835f2f1a31
Yury Semikhatsky <yurys@chromium.org>
2021-01-04 13:40:56 -0800

    browser(webkit): do run win build again if first attempt failed (#4881)

--- browser_patches/webkit/build.sh
+++ browser_patches/webkit/build.sh
@@ -70,7 +70,7 @@ elif [[ "$(uname)" == "Linux" ]]; then
     build_wpe
   fi
 elif [[ "$(uname)" == MINGW* ]]; then
-  /c/Windows/System32/cmd.exe "/c $(cygpath -w ${SCRIPT_FOLDER}/buildwin.bat)"
+  /c/Windows/System32/cmd.exe "/c $(cygpath -w ${SCRIPT_FOLDER}/buildwin.bat)" ||
   # Run build step twice on Windows to work around adjust-angle-include-paths.py not
   # being called at right time. See
   # https://github.com/WebKit/WebKit/commit/e9a19fe7e3c245252be6ed70c86639d09227a6dc
