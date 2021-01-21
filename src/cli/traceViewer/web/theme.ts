5549e55d0bdf68e968053cc6b4da741df4162033
Pavel Feldman <pavel.feldman@gmail.com>
2021-01-20 19:15:48 -0800

    chore: add storybook dep

---
+++ src/cli/traceViewer/web/theme.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function platformName(): string {
+  if (window.navigator.userAgent.includes('Linux'))
+    return 'platform-linux';
+  if (window.navigator.userAgent.includes('Windows'))
+    return 'platform-windows';
+  if (window.navigator.userAgent.includes('Mac'))
+    return 'platform-mac';
+  return 'platform-generic';
+}
+
+export function applyTheme() {
+  if ((document as any).playwrightThemeInitialized)
+    return;
+  (document as any).playwrightThemeInitialized = true;
+  document!.defaultView!.addEventListener('focus', (event: any) => {
+    if (event.target.document.nodeType === Node.DOCUMENT_NODE)
+      document.body.classList.remove('inactive');
+  }, false);
+  document!.defaultView!.addEventListener('blur', event => {
+    document.body.classList.add('inactive');
+  }, false);
+  document.documentElement.classList.add(platformName());
+}
