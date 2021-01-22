7a4b94e66ceb16b576255cd2545f2ebfb4961b14
Dmitry Gozman <dgozman@gmail.com>
2021-01-21 16:39:49 -0800

    feat(selectors): nth-match selector (#5081)
    Introduces :nth-match(ul > li, 3) css extension, with one-based index.

--- src/server/common/selectorParser.ts
+++ src/server/common/selectorParser.ts
@@ -26,7 +26,7 @@ export type ParsedSelector = {
   capture?: number,
 };
 
-const customCSSNames = new Set(['not', 'is', 'where', 'has', 'scope', 'light', 'visible', 'text', 'text-matches', 'text-is', 'above', 'below', 'right-of', 'left-of', 'near']);
+export const customCSSNames = new Set(['not', 'is', 'where', 'has', 'scope', 'light', 'visible', 'text', 'text-matches', 'text-is', 'above', 'below', 'right-of', 'left-of', 'near', 'nth-match']);
 
 export function parseSelector(selector: string): ParsedSelector {
   const result = parseSelectorV1(selector);
