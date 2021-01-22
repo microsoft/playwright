2f29c6b06696886510386afdc5c4b0517368ac8a
Andrey Lushnikov <aslushnikov@gmail.com>
2021-01-22 01:41:12 -0800

    browser(firefox): roll Firefox to beta Jan, 18 2021 (#5106)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="107fed8283ee8cfeb485a121e0ef7efaf7ee0ecb"
+BASE_REVISION="4cfd64739b9c29ff183b326b65cf70abc571257c"
