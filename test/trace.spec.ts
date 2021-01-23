b7fd0cd14c7fa4598bf5fb9f162ba6dc754dd865
Andrey Lushnikov <aslushnikov@gmail.com>
2021-01-22 17:27:16 -0800

    test: disable trace test that always fails (#5124)

--- test/trace.spec.ts
+++ test/trace.spec.ts
@@ -19,7 +19,7 @@ import type * as trace from '../src/trace/traceTypes';
 import * as path from 'path';
 import * as fs from 'fs';
 
-it('should record trace', async ({browser, testInfo, server}) => {
+it('should record trace', test => test.fixme(), async ({browser, testInfo, server}) => {
   const traceDir = testInfo.outputPath('trace');
   const context = await browser.newContext({ _traceDir: traceDir } as any);
   const page = await context.newPage();
