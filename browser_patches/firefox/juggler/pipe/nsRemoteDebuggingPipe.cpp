986e89d1249d89f72ebc54e022a018dd91114d34
Andrey Lushnikov <lushnikov@chromium.org>
2021-02-03 08:52:46 -0700

    browser(firefox): fix build on Windows

--- browser_patches/firefox/juggler/pipe/nsRemoteDebuggingPipe.cpp
+++ browser_patches/firefox/juggler/pipe/nsRemoteDebuggingPipe.cpp
@@ -110,8 +110,8 @@ nsresult nsRemoteDebuggingPipe::Init(nsIRemoteDebuggingPipeClient* aClient) {
 #if defined(_WIN32)
   CHAR pipeReadStr[20];
   CHAR pipeWriteStr[20];
-  GetEnvironmentVariable("PW_PIPE_READ", pipeReadStr, 20);
-  GetEnvironmentVariable("PW_PIPE_WRITE", pipeWriteStr, 20);
+  GetEnvironmentVariableA("PW_PIPE_READ", pipeReadStr, 20);
+  GetEnvironmentVariableA("PW_PIPE_WRITE", pipeWriteStr, 20);
   readHandle = reinterpret_cast<HANDLE>(atoi(pipeReadStr));
   writeHandle = reinterpret_cast<HANDLE>(atoi(pipeWriteStr));
 #endif
