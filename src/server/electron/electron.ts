5feda0925991cc86f06e2ee72a175824a5d8ddfe
Pavel Feldman <pavel.feldman@gmail.com>
2021-02-08 14:49:25 -0800

    chore: kill electron process on ctrl+c

--- src/server/electron/electron.ts
+++ src/server/electron/electron.ts
@@ -163,6 +163,9 @@ export class Electron  {
         cwd: options.cwd,
         tempDirectories: [],
         attemptToGracefullyClose: () => app!.close(),
+        handleSIGINT: true,
+        handleSIGTERM: true,
+        handleSIGHUP: true,
         onExit: () => {},
       });
 
