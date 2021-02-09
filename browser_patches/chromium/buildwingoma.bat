ad557dc6da3ddd3cb02869c2378fba4322a73d3d
Andrey Lushnikov <aslushnikov@gmail.com>
2021-02-09 08:33:39 -0800

    devops: introduce goma infrastructure for Chromium builds (#5377)
    This patch adds `//browser_patches/chromium/goma.sh` script that
    manages goma to build chromium.

---
+++ browser_patches/chromium/buildwingoma.bat
@@ -0,0 +1,2 @@
+CALL gn gen out/Default
+CALL ninja -j 200 -C out/Default chrome
---
+++ browser_patches/chromium/goma.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+set +x
+
+trap "cd $(pwd -P)" EXIT
+cd "$(dirname $0)"
+
+if [[ ! -d ./electron-build-tools ]]; then
+  git clone --single-branch --branch msft-goma  https://github.com/electron/build-tools/ electron-build-tools
+  cd electron-build-tools
+  npm install
+  mkdir -p third_party
+  ./src/e update-goma msftGoma
+  cd ..
+fi
+
+cd electron-build-tools
+
+if [[ $1 == "--help" ]]; then
+  echo "$(basename $0) [login|start|stop|--help]"
+  exit 0
+elif [[ $1 == "login" ]]; then
+  python ./third_party/goma/goma_auth.py login
+elif [[ $1 == "start" ]]; then
+  if [[ ! -z "$GOMA_LOGIN_COOKIE" ]]; then
+    echo "$GOMA_LOGIN_COOKIE" > "$HOME/.goma_oauth2_config"
+  fi
+  if [[ ! -f "$HOME/.goma_oauth2_config" ]]; then
+    echo "ERROR: goma is not logged in!"
+    echo "run '$(basename $0) login'"
+    exit 1
+  fi
+  python ./third_party/goma/goma_ctl.py ensure_start
+elif [[ $1 == "stop" ]]; then
+  python ./third_party/goma/goma_ctl.py stop
+else
+  echo "ERROR: unknown command - $1"
+  echo "Use --help to list all available commands"
+  exit 1
+fi
+
