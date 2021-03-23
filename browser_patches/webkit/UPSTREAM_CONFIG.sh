485c5b61e433aa7eb377ce988a68dfed4682d9b5
Yury Semikhatsky <yurys@chromium.org>
2021-03-23 11:20:28 -0700

    browser(webkit): roll back to safari-612.1.7-branch first commit

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://git.webkit.org/git/WebKit.git"
 BASE_BRANCH="master"
-BASE_REVISION="3c70b8c33ea20632f65de3831f855152f1f7c05b"
+BASE_REVISION="f09ebd5f77c9f9f38cbbfb8468664e7b5d5174d4"
