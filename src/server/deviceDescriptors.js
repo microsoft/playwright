52eb9d6c817220fde958c492bad0b202e84e5c14
Fili <fili@fili.com>
2021-03-25 09:32:49 +0100

    Adding Moto G4 to deviceDescriptors, UA based on Emulated Moto G4 in Lighthouse API

--- src/server/deviceDescriptors.js
+++ src/server/deviceDescriptors.js
@@ -854,7 +854,7 @@ module.exports = {
     'isMobile': true,
     'hasTouch': true,
     'defaultBrowserType': 'chromium'
-  }
+  },
   'Moto G4': {
     'userAgent': 'Mozilla/5.0 (Linux; Android 7.0; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4143.7 Mobile Safari/537.36',
     'viewport': {
