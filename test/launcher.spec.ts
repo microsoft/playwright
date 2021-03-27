0943af28069774b09dd2481059568072c88cd3dd
Yury Semikhatsky <yurys@chromium.org>
2021-03-27 09:59:04 -0700

    fix: kill browser if process doesnt exit for 30s after close (#5968)

--- test/launcher.spec.ts
+++ test/launcher.spec.ts
@@ -27,3 +27,18 @@ it('should require top-level DeviceDescriptors', async ({playwright}) => {
   expect(Devices['iPhone 6']).toBeTruthy();
   expect(Devices['iPhone 6']).toEqual(playwright.devices['iPhone 6']);
 });
+
+it('should kill browser process on timeout after close', (test, { mode }) => {
+  test.skip(mode !== 'default', 'Test passes server hooks via options');
+}, async ({browserType, browserOptions}) => {
+  const launchOptions = { ...browserOptions };
+  let stalled = false;
+  (launchOptions as any).__testHookGracefullyClose = () => {
+    stalled = true;
+    return new Promise(() => {});
+  };
+  (launchOptions as any).__testHookBrowserCloseTimeout = 1_000;
+  const browser = await browserType.launch(launchOptions);
+  await browser.close();
+  expect(stalled).toBeTruthy();
+});
