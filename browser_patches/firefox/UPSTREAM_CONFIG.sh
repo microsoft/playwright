fe90642a673a0c6f88e00ed20492b8b263ab469b
Andrey Lushnikov <lushnikov@chromium.org>
2021-04-01 15:41:50 -0700

    browser(firefox): roll Firefox to beta @ Apr 1, 2021
    Merging conflicts:
    https://github.com/aslushnikov/juggler/commit/038f367c30a7696407b7eed1440f9c5a57e2cc86
    
    After this roll, we no longer require our custom `onFrameLocationChanged` instrumentation
    since gecko now fixed the `NOTIFY_LOCATION` behavior so that it works
    for same-document navigations inside iframes.

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="bb8d64aa47546c08613bc10e29c16af37766fba7"
+BASE_REVISION="7d54fe2fbe98b24686b90d9e67e1192226484b6f"
