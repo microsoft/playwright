4b4de24ae8fb53524660b99d080dfb1d129aecdd
Andrey Lushnikov <lushnikov@chromium.org>
2021-04-06 14:42:23 -0500

    disable feature

--- src/server/chromium/chromium.ts
+++ src/server/chromium/chromium.ts
@@ -177,7 +177,7 @@ const DEFAULT_ARGS = [
   '--disable-dev-shm-usage',
   '--disable-extensions',
   // BlinkGenPropertyTrees disabled due to crbug.com/937609
-  '--disable-features=TranslateUI,BlinkGenPropertyTrees,ImprovedCookieControls,SameSiteByDefaultCookies,LazyFrameLoading',
+  '--disable-features=PaintHoldingCrossOrigin,TranslateUI,BlinkGenPropertyTrees,ImprovedCookieControls,SameSiteByDefaultCookies,LazyFrameLoading',
   '--disable-hang-monitor',
   '--disable-ipc-flooding-protection',
   '--disable-popup-blocking',
