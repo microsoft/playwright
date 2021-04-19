9b51fd444724f494270be59e1cf40154c3ee03ee
Andrey Lushnikov <lushnikov@chromium.org>
2021-04-19 13:29:25 -0500

    add helpful comment

--- src/server/chromium/chromium.ts
+++ src/server/chromium/chromium.ts
@@ -192,6 +192,7 @@ const DEFAULT_ARGS = [
   '--enable-automation',
   '--password-store=basic',
   '--use-mock-keychain',
+  // See https://chromium-review.googlesource.com/c/chromium/src/+/2436773
   '--no-service-autorun',
 ];
 
