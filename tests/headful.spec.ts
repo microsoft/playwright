3e8efa2ba0adf8d73a590f8a20ddca7e90d7b18f
Andrey Lushnikov <lushnikov@chromium.org>
2021-04-29 17:00:46 -0500

    disable test on win

--- tests/headful.spec.ts
+++ tests/headful.spec.ts
@@ -166,6 +166,7 @@ it('Page.bringToFront should work', async ({browserType, browserOptions}) => {
 it('focused input should produce the same screenshot', async ({browserType, browserOptions, browserName, platform, browserChannel}, testInfo) => {
   it.fail(browserName === 'firefox' && platform === 'darwin', 'headless has thinner outline');
   it.fail(browserName === 'firefox' && platform === 'linux', 'headless has no outline');
+  it.fail(browserName === 'firefox' && platform === 'win32', 'headless has outline since new version');
   it.skip(browserName === 'webkit' && platform === 'linux', 'gtk vs wpe');
   it.skip(!!process.env.CRPATH);
   it.skip(!!browserChannel, 'Uncomment on roll');
