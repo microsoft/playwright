298b7aef2ab09c019f1bacc83619cbbc70b60d42
Andrey Lushnikov <aslushnikov@gmail.com>
2021-05-06 10:07:00 -0700

    devops: install Google Chrome Beta testers (#6389)

---
+++ utils/install-chrome-beta/reinstall_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+
+rm -rf "/Applications/Google Chrome Beta.app"
+cd /tmp
+curl -o ./googlechromebeta.dmg -k https://dl.google.com/chrome/mac/beta/googlechromebeta.dmg
+hdiutil attach -nobrowse -quiet -noautofsck -noautoopen -mountpoint /Volumes/googlechromebeta.dmg ./googlechromebeta.dmg
+cp -rf "/Volumes/googlechromebeta.dmg/Google Chrome Beta.app" /Applications
+hdiutil detach /Volumes/googlechromebeta.dmg
