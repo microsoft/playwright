301999ed5a1c0250c03bc7bae51190981b258df4
Andrey Lushnikov <lushnikov@chromium.org>
2021-05-06 11:02:08 -0700

    browser(firefox-beta): roll Firefox to v89.0b9 (May 6, 2021)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="5c667fafaaf7f15ed9df2b50e6bb26b570b3fee3"
+BASE_REVISION="8596b58aaea60b1bd772f9e4c5e57416eee9ff1d"
