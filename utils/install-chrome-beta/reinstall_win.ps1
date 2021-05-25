5eefee418e752796cb4963ceab265f581b5430bc
Andrey Lushnikov <lushnikov@chromium.org>
2021-05-24 22:51:43 -0700

    feat: do not rely on chocolatey to install Google Chrome Beta

---
+++ utils/install-chrome-beta/reinstall_win.ps1
@@ -0,0 +1,22 @@
+$url = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi';
+
+if ([Environment]::Is64BitProcess) {
+    $url = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
+} else {
+    $url = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
+}
+
+$app = Get-WmiObject -Class Win32_Product | Where-Object { 
+    $_.Name -match "Google Chrome Beta" 
+}
+if ($app) {
+    $app.Uninstall()
+}
+
+$wc = New-Object net.webclient
+$msiInstaller = "$env:temp\google-chrome-beta.msi"
+Remove-Item $msiInstaller
+$wc.Downloadfile($url, $msiInstaller)
+
+$arguments = "/i `"$msiInstaller`" /quiet"
+Start-Process msiexec.exe -ArgumentList $arguments -Wait
