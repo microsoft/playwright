7d51557fce49daadf3dbf01ada66589b383dd5de
Andrey Lushnikov <lushnikov@chromium.org>
2021-05-26 10:04:20 -0700

    fix lint #2

--- src/server/processLauncher.ts
+++ src/server/processLauncher.ts
@@ -84,7 +84,7 @@ export async function launchProcess(options: LaunchProcessOptions): Promise<Laun
       if (errors[i])
         options.log(`[pid=${spawnedProcess.pid || 'N/A'}] exception while removing ${options.tempDirectories[i]}: ${errors[i]}`);
     }
-  }
+  };
 
   // Prevent Unhandled 'error' event.
   spawnedProcess.on('error', () => {});
