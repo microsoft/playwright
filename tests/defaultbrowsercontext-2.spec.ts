a1ae99b3eafdedd6b008c8c5347809e405486bcd
Yury Semikhatsky <yurys@chromium.org>
2021-06-01 22:38:37 -0700

    Update test expectations

--- tests/defaultbrowsercontext-2.spec.ts
+++ tests/defaultbrowsercontext-2.spec.ts
@@ -44,9 +44,11 @@ it('should support reducedMotion option', async ({launchPersistent}) => {
   expect(await page.evaluate(() => matchMedia('(prefers-reduced-motion: no-preference)').matches)).toBe(false);
 });
 
-it('should support timezoneId option', async ({launchPersistent}) => {
+it.only('should support timezoneId option', async ({launchPersistent, browserName}) => {
   const {page} = await launchPersistent({locale: 'en-US', timezoneId: 'America/Jamaica'});
-  expect(await page.evaluate(() => new Date(1479579154987).toString())).toBe('Sat Nov 19 2016 13:12:34 GMT-0500 (Eastern Standard Time)');
+  expect(await page.evaluate(() => new Date(1479579154987).toString())).toBe(browserName === 'webkit' ?
+    'Sat Nov 19 2016 13:12:34 GMT-0500' :
+    'Sat Nov 19 2016 13:12:34 GMT-0500 (Eastern Standard Time)');
 });
 
 it('should support locale option', async ({launchPersistent}) => {
