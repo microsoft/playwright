93d9df8c6c85aaa6df6bc14cf10a8178ad0ca626
Andrey Lushnikov <lushnikov@chromium.org>
2021-06-06 03:16:04 -0700

    fix: do not install media pack on non-server windows

--- bin/install_media_pack.ps1
+++ bin/install_media_pack.ps1
@@ -1,1 +1,5 @@
-Install-WindowsFeature Server-Media-Foundation
+$osInfo = Get-WmiObject -Class Win32_OperatingSystem
+# check if running on Windows Server
+if ($osInfo.ProductType -eq 3) {
+  Install-WindowsFeature Server-Media-Foundation
+}
