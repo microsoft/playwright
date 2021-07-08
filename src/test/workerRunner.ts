b2742976a85795ed0cadad4e85c8e7fd475dd7dc
Yury Semikhatsky <yurys@chromium.org>
2021-07-08 09:36:28 -0700

    fix(runner): do not restart worker after skipping tests (#7511)

--- src/test/workerRunner.ts
+++ src/test/workerRunner.ts
@@ -338,7 +338,7 @@ export class WorkerRunner extends EventEmitter {
     if (!preserveOutput)
       await removeFolderAsync(testInfo.outputDir).catch(e => {});
 
-    if (testInfo.status !== 'passed') {
+    if (testInfo.status !== 'passed' && testInfo.status !== 'skipped') {
       this._failedTestId = testId;
       this._reportDoneAndStop();
     }
