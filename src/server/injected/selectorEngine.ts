39c0a540c292078bb4da0fba2a4b888cab46c0cf
Pavel Feldman <pavel.feldman@gmail.com>
2021-07-26 07:51:54 -0700

    chore: unify v1 and v2 selector handling

--- src/server/injected/selectorEngine.ts
+++ src/server/injected/selectorEngine.ts
@@ -17,6 +17,7 @@
 export type SelectorRoot = Element | ShadowRoot | Document;
 
 export interface SelectorEngine {
-  query(root: SelectorRoot, selector: string): Element | undefined;
+  query?(root: SelectorRoot, selector: string): Element | undefined;
+
   queryAll(root: SelectorRoot, selector: string): Element[];
 }
