28ee6ff3bf4add01db1a53c0d323a5349bb45122
Max Schmitt <max@schmitt.mx>
2021-08-02 17:09:04 +0200

    feat(test-runner): re-enable web server

--- types/test.d.ts
+++ types/test.d.ts
@@ -479,24 +479,24 @@ export interface Project<TestArgs = {}, WorkerArgs = {}> extends TestProject {
 
 export type FullProject<TestArgs = {}, WorkerArgs = {}> = Required<Project<TestArgs, WorkerArgs>>;
 
-export type LaunchConfig = {
+export type WebServerConfig = {
   /**
    * Shell command to start. For example `npm run start`.
    */
   command: string,
   /**
-   * The port that your http server is expected to appear on. If specified it does wait until it accepts connections.
+   * The port that your http server is using. Gets used for `setBaseURL`.
    */
-  waitForPort?: number,
+  port: number,
   /**
    * How long to wait for the process to start up and be available in milliseconds. Defaults to 60000.
    */
-  waitForPortTimeout?: number,
+  timeout?: number,
   /**
-   * If true it will verify that the given port via `waitForPort` is available and throw otherwise.
+   * If true it will use an existing server if available. If false it will verify that the port is available before starting the command.
    * This should commonly set to !!process.env.CI to allow the local dev server when running tests locally.
    */
-  strict?: boolean
+  reuseExistingServer?: boolean
   /**
    * Environment variables, process.env by default
    */
@@ -690,7 +690,7 @@ interface TestConfig {
    * Learn more about [snapshots](https://playwright.dev/docs/test-snapshots).
    */
   updateSnapshots?: UpdateSnapshots;
-  _launch?: LaunchConfig | LaunchConfig[];
+  webServer?: WebServerConfig;
   /**
    * The maximum number of concurrent worker processes to use for parallelizing tests.
    *
@@ -1051,7 +1051,7 @@ export interface FullConfig {
    * Playwright Test.
    */
   workers: number;
-  _launch: LaunchConfig[];
+  webServer: WebServerConfig;
 }
 
 export type TestStatus = 'passed' | 'failed' | 'timedOut' | 'skipped';
