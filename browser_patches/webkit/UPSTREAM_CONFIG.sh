3b0856da18a95f127f38a738c2d21bdbe79d3504
Daniel Kolesa <dkolesa@igalia.com>
2021-08-04 20:08:42 +0200

    browser(webkit): roll to 04/08/21 (#7984)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/WebKit/WebKit.git"
 BASE_BRANCH="main"
-BASE_REVISION="2cf99c4a7d4fb944ebb1f2b7cb4ddd864be6d141"
+BASE_REVISION="29e16514d1f040bfd22e50723e0a10b643c625e4"
