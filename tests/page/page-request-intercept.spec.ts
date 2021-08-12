029cc20cc1e593488d9dc5dc3af7690c3820190e
Andrey Lushnikov <lushnikov@chromium.org>
2021-08-12 19:26:07 +0300

    test: fix electron tests

--- tests/page/page-request-intercept.spec.ts
+++ tests/page/page-request-intercept.spec.ts
@@ -40,7 +40,8 @@ it('should fulfill intercepted response', async ({page, server, browserName}) =>
   expect(await page.evaluate(() => document.body.textContent)).toBe('Yo, page!');
 });
 
-it('should fulfill response with empty body', async ({page, server, browserName}) => {
+it('should fulfill response with empty body', async ({page, server, browserName, browserMajorVersion}) => {
+  it.skip(browserName === 'chromium' && browserMajorVersion <= 91);
   it.fail(browserName === 'firefox');
   await page.route('**/*', async route => {
     // @ts-expect-error
@@ -85,7 +86,8 @@ it('should support fulfill after intercept', async ({page, server}) => {
   expect(await response.text()).toBe('<title>Woof-Woof</title>' + os.EOL);
 });
 
-it('should support request overrides', async ({page, server}) => {
+it('should support request overrides', async ({page, server, browserName, browserMajorVersion}) => {
+  it.skip(browserName === 'chromium' && browserMajorVersion <= 91);
   const requestPromise = server.waitForRequest('/empty.html');
   await page.route('**/foo', async route => {
     // @ts-expect-error
