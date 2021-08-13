f2c2bced49d35494f7d3a4cb6dc0b04f8cbe2a01
Anže Vodovnik <anvod@microsoft.com>
2021-08-13 17:52:30 +0200

    WIP: chore(dotnet): generate channels

--- utils/generate_dotnet_channels.js
+++ utils/generate_dotnet_channels.js
@@ -21,13 +21,32 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const yaml = require('yaml');
+const { execSync } = require('child_process');
 
 const channels = new Set();
 const inherits = new Map();
 const mixins = new Map();
 
+const protocolDir = path.join(process.argv[2], 'Transport', 'Protocol', 'Generated');
+const channelsDir = path.join(process.argv[2], 'Transport', 'Channels', 'Generated');
+const eventArgsDir = path.join(process.argv[2], 'Transport', 'Channels', 'EventArgs', 'Generated');
+fs.mkdirSync(protocolDir, { recursive: true });
+fs.mkdirSync(channelsDir, { recursive: true });
+fs.mkdirSync(eventArgsDir, { recursive: true });
+
+const namePrettyMap = new Map([
+  ["loadstate", "LoadState"]
+]);
+
+var enumTypes = null;
+
 function raise(item) {
   throw new Error('Invalid item: ' + JSON.stringify(item, null, 2));
+}
+
+function getName(name) {
+  name = namePrettyMap.get(name) || name;
+  return titleCase(name);
 }
 
 function titleCase(name) {
@@ -52,7 +71,7 @@ function nullableSuffix(inner) {
   return '';
 }
 
-function inlineType(type, indent = '', wrapEnums = false) {
+function inlineType(type, indent = '', parentName) {
   if (typeof type === 'string') {
     const optional = type.endsWith('?');
     if (optional)
@@ -65,24 +84,27 @@ function inlineType(type, indent = '', wrapEnums = false) {
       return { ts: mapType(type), scheme: `t${titleCase(type)}`, optional };
     }
     if (channels.has(type))
-      return { ts: `${type}`, scheme: `tChannel('${type}')` , optional };
+      return { ts: `${type}`, scheme: `tChannel('${type}')`, optional };
     if (type === 'Channel')
       return { ts: `Channel`, scheme: `tChannel('*')`, optional };
     return { ts: mapType(type), scheme: `tType('${type}')`, optional };
   }
   if (type.type.startsWith('array')) {
     const optional = type.type.endsWith('?');
-    const inner = inlineType(type.items, indent, true);
+    const inner = inlineType(type.items, indent);
     return { ts: `List<${inner.ts}>`, scheme: `tArray(${inner.scheme})`, optional };
   }
   if (type.type.startsWith('enum')) {
-    if (type.literals.includes('networkidle'))
-      return { ts: 'LoadState', scheme: `tString`, optional: false };
+    // check our existing ones, I know it's slow, but it'll do for now
+    for (let [ enumName, enumType ] of enumTypes) {
+      if (enumType.map(x => x.replace(/"/g, '')).sort().join(',') === type.literals.sort().join(','))
+        return { ts: enumName, scheme: `tEnum`, optional: false };
+    }
     return { ts: 'string', scheme: `tString`, optional: false };
   }
   if (type.type.startsWith('object')) {
     const optional = type.type.endsWith('?');
-    const custom = processCustomType(type, optional);
+    const custom = processCustomType(type, optional, parentName);
     if (custom)
       return custom;
     const inner = properties(type.properties, indent + '  ');
@@ -106,11 +128,12 @@ function properties(properties, indent, onlyOptional) {
         visitProperties(mixins.get(value).properties);
         continue;
       }
-      const inner = inlineType(value, indent);
+      const inner = inlineType(value, indent, name);
       if (onlyOptional && !inner.optional)
         continue;
+      var typeName = inner.ts;
       ts.push('');
-      ts.push(`${indent}public ${inner.ts}${nullableSuffix(inner)} ${toTitleCase(name)} { get; set; }`);
+      ts.push(`${indent}public ${typeName}${nullableSuffix(inner)} ${getName(name)} { get; set; }`);
       const wrapped = inner.optional ? `tOptional(${inner.scheme})` : inner.scheme;
       scheme.push(`${indent}${name}: ${wrapped},`);
     }
@@ -126,65 +149,95 @@ function objectType(props, indent, onlyOptional = false) {
   return { ts: `${indent}{${inner.ts}\n${indent}}`, scheme: `tObject({\n${inner.scheme}\n${indent}})` };
 }
 
-const yml = fs.readFileSync(path.join(__dirname, '..', 'src', 'protocol', 'protocol.yml'), 'utf-8');
-const protocol = yaml.parse(yml);
+const licence =
+  `
+/*
+* MIT License
+*
+* Copyright (c) Microsoft Corporation.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*
+* ------------------------------------------------------------------------------
+* <auto-generated>
+* This file is generated by ${path.basename(__filename).split(path.sep).join(path.posix.sep)}, do not edit manually.
+* </auto-generated>
+*/
+`;
 
-for (const [name, value] of Object.entries(protocol)) {
-  if (value.type === 'interface') {
-    channels.add(name);
-    if (value.extends)
-      inherits.set(name, value.extends);
-  }
-  if (value.type === 'mixin')
-    mixins.set(name, value);
+function writeEvent(eventName, event, parent) {
+  var argsName = `${parent}${eventName}EventArgs`;
+  const eventArgs = [licence];
+
+  if (!event.parameters)
+    return `internal event EventHandler ${eventName};`;
+
+  var args = objectType(event.parameters, ``);
+
+  eventArgs.push('using System;');
+  eventArgs.push('using Microsoft.Playwright.Core;');
+  eventArgs.push(``);
+  eventArgs.push(`namespace Microsoft.Playwright.Transport.Channels`);
+  eventArgs.push(`{`);
+  eventArgs.push(`    internal class ${argsName} : EventArgs`);
+  eventArgs.push(args.ts);
+  eventArgs.push(`}`);
+  eventArgs.push(``);
+
+  writeFile(`${argsName}.cs`, eventArgs.join(`\n`), eventArgsDir);
+
+  return `internal event EventHandler<${argsName}> ${eventName};`;
 }
 
-const dir = path.join(process.argv[2], 'Transport', 'Protocol', 'Generated')
-fs.mkdirSync(dir, { recursive: true });
-
-for (const [name, item] of Object.entries(protocol)) {
-  if (item.type === 'interface') {
-    const channelName = name;
-    const channels_ts = [];
-    const init = objectType(item.initializer || {}, '    ');
-    const initializerName = channelName + 'Initializer';
-    const superName = inherits.get(name);
-    channels_ts.push('using System.Collections.Generic;');
-    channels_ts.push('using Microsoft.Playwright.Core;');
-    channels_ts.push(``);
-    channels_ts.push(`namespace Microsoft.Playwright.Transport.Protocol`);
-    channels_ts.push(`{`);
-    channels_ts.push(`    internal class ${initializerName}${superName ? ' : ' + superName + 'Initializer' : ''}`);
-    channels_ts.push(init.ts);
-    channels_ts.push(`}`);
-    channels_ts.push(``);
-    writeFile(`${initializerName}.cs`, channels_ts.join('\n'));
-  }
+function writeInitializer(name, item) {
+  const init = objectType(item.initializer || {}, '    ');
+  const initializer = [licence];
+  const initializerName = name + 'Initializer';
+  const superName = inherits.get(name);
+  initializer.push('using System.Collections.Generic;');
+  initializer.push('using Microsoft.Playwright.Core;');
+  initializer.push(``);
+  initializer.push(`namespace Microsoft.Playwright.Transport.Protocol`);
+  initializer.push(`{`);
+  initializer.push(`    internal class ${initializerName}${superName ? ' : ' + superName + 'Initializer' : ''}`);
+  initializer.push(init.ts);
+  initializer.push(`}`);
+  initializer.push(``);
+  writeFile(`${initializerName}.cs`, initializer.join('\n'), protocolDir);
 }
 
-function writeFile(file, content) {
-  fs.writeFileSync(path.join(dir, file), content, 'utf8');
+function writeFile(file, content, folder) {
+  fs.writeFileSync(path.join(folder, file), content, 'utf8');
 }
 
-/**
- * @param {string} name
- * @returns {string}
- */
-function toTitleCase(name) {
-  return name.charAt(0).toUpperCase() + name.substring(1);
-}
-
-function processCustomType(type, optional) {
+function processCustomType(type, optional, parentName) {
   if (type.properties.name
-      && type.properties.value
-      && inlineType(type.properties.name).ts === 'string'
-      && inlineType(type.properties.value).ts === 'string') {
+    && type.properties.value
+    && inlineType(type.properties.name).ts === 'string'
+    && inlineType(type.properties.value).ts === 'string') {
     return { ts: 'HeaderEntry', scheme: 'tObject()', optional };
   }
   if (type.properties.width
-      && type.properties.height
-      && inlineType(type.properties.width).ts === 'int'
-      && inlineType(type.properties.height).ts === 'int') {
+    && type.properties.height
+    && inlineType(type.properties.width).ts === 'int'
+    && inlineType(type.properties.height).ts === 'int') {
     return { ts: 'ViewportSize', scheme: 'tObject()', optional };
   }
   if (type.properties.url
@@ -198,4 +251,96 @@ function processCustomType(type, optional) {
     && inlineType(type.properties.name).ts === 'string') {
     return { ts: 'DeviceDescriptorEntry', scheme: 'tObject()', optional };
   }
+  if (parentName === 'newDocument')
+    return { ts: 'NavigateDocument', scheme: 'tObject()', optional };
 }
+
+function generateChannels(mappedEnumTypes) {
+  enumTypes = mappedEnumTypes;
+
+  const yml = fs.readFileSync(path.join(__dirname, '..', 'src', 'protocol', 'protocol.yml'), 'utf-8');
+  const protocol = yaml.parse(yml);
+
+  for (const [name, value] of Object.entries(protocol)) {
+    if (value.type === 'interface') {
+      channels.add(name);
+      if (value.extends)
+        inherits.set(name, value.extends);
+    }
+    if (value.type === 'mixin')
+      mixins.set(name, value);
+  }
+
+  for (const [name, item] of Object.entries(protocol)) {
+    var channelName = `${name}Channel`;
+    if (item.type === 'interface') {
+      writeInitializer(name, item);
+
+      var channel = [
+        licence,
+        `using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Playwright.Core;
+
+namespace Microsoft.Playwright.Transport.Channels
+{
+internal class ${channelName} : Channel<${name}>
+{
+  public ${channelName}(string guid, Connection connection, ${name} owner) : base(guid, connection, owner)
+  {
+  }
+`];
+
+      for (let [eventName, event] of Object.entries(item.events || {})) {
+        eventName = getName(eventName);
+        if (event === null)
+          event = {};
+
+        channel.push(writeEvent(eventName, event, channelName));
+        channel.push(``);
+        // const parameters = objectType(event.parameters || {}, '');
+        // const paramsName = `${channelName}${titleCase(eventName)}Event`;
+        // ts_types.set(paramsName, parameters.ts);
+        // channels_ts.push(`  on(event: '${eventName}', callback: (params: ${paramsName}) => void): this;`);
+      }
+
+      for (let [methodName, method] of Object.entries(item.commands || {})) {
+        if (method === null)
+          method = {};
+        channel.push(`// method: ${methodName}`);
+        // if (method.tracing && method.tracing.snapshot) {
+        //   tracingSnapshots.push(name + '.' + methodName);
+        //   for (const derived of derivedClasses.get(name) || [])
+        //     tracingSnapshots.push(derived + '.' + methodName);
+        // }
+        // const parameters = objectType(method.parameters || {}, '');
+        // const paramsName = `${channelName}${titleCase(methodName)}Params`;
+        // const optionsName = `${channelName}${titleCase(methodName)}Options`;
+        // ts_types.set(paramsName, parameters.ts);
+        // ts_types.set(optionsName, objectType(method.parameters || {}, '', true).ts);
+        // addScheme(paramsName, method.parameters ? parameters.scheme : `tOptional(tObject({}))`);
+        // for (const key of inherits.keys()) {
+        //   if (inherits.get(key) === channelName)
+        //     addScheme(`${key}${titleCase(methodName)}Params`, `tType('${paramsName}')`);
+        // }
+
+        // const resultName = `${channelName}${titleCase(methodName)}Result`;
+        // const returns = objectType(method.returns || {}, '');
+        // ts_types.set(resultName, method.returns ? returns.ts : 'void');
+
+        // channels_ts.push(`  ${methodName}(params${method.parameters ? '' : '?'}: ${paramsName}, metadata?: Metadata): Promise<${resultName}>;`);
+      }
+      channel.push(`}`); // end of class
+      channel.push(`}`); // end of namespace
+      writeFile(`${channelName}.cs`, channel.join(`\n`), channelsDir);
+    }
+  }
+}
+
+// if (process.argv[3] !== "--skip-format") {
+//   // run the formatting tool for .net, to ensure the files are prepped
+//   execSync(`dotnet format -f "${process.argv[2]}" --include-generated --fix-whitespace`);
+// }
+
+module.exports = { generateChannels }
