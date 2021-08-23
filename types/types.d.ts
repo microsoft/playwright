953f19538a6ea5a5f41b7d0faa63007b1f8a6f83
Max Schmitt <max@schmitt.mx>
2021-08-23 13:57:38 +0200

    docs(errors): add example for catching Timeout errors (#8362)

--- types/types.d.ts
+++ types/types.d.ts
@@ -8802,6 +8802,26 @@ export namespace errors {
  * TimeoutError is emitted whenever certain operations are terminated due to timeout, e.g.
  * [page.waitForSelector(selector[, options])](https://playwright.dev/docs/api/class-page#page-wait-for-selector) or
  * [browserType.launch([options])](https://playwright.dev/docs/api/class-browsertype#browser-type-launch).
+ *
+ * ```js
+ * const playwright = require('playwright');
+ *
+ * (async () => {
+ *   const browser = await playwright.chromium.launch();
+ *   const context = await browser.newContext();
+ *   const page = await context.newPage();
+ *   try {
+ *     await page.click("text=Foo", {
+ *       timeout: 100,
+ *     })
+ *   } catch (error) {
+ *     if (error instanceof playwright.errors.TimeoutError)
+ *       console.log("Timeout!")
+ *   }
+ *   await browser.close();
+ * })();
+ * ```
+ *
  */
 class TimeoutError extends Error {}
 
