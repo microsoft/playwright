222151f2e1531516e861d9fb44531fc4d6a74087
Ross Wollman <rwoll@users.noreply.github.com>
2021-08-26 02:49:47 -0700

    docs: connect example with non-deprecated API (#8463)
    See [deprecation notice][notice].
    
    [notice]: https://github.com/microsoft/playwright/blob/c627927bf57af81d2a33aea00954800654c4e064/types/types.d.ts#L8205

--- docs/src/api/class-browsertype.md
+++ docs/src/api/class-browsertype.md
@@ -268,7 +268,7 @@ const { chromium } = require('playwright');  // Or 'webkit' or 'firefox'.
   const browserServer = await chromium.launchServer();
   const wsEndpoint = browserServer.wsEndpoint();
   // Use web socket endpoint later to establish a connection.
-  const browser = await chromium.connect({ wsEndpoint });
+  const browser = await chromium.connect(wsEndpoint);
   // Close browser instance.
   await browserServer.close();
 })();
--- types/types.d.ts
+++ types/types.d.ts
@@ -8614,7 +8614,7 @@ export interface BrowserType<Unused = {}> {
    *   const browserServer = await chromium.launchServer();
    *   const wsEndpoint = browserServer.wsEndpoint();
    *   // Use web socket endpoint later to establish a connection.
-   *   const browser = await chromium.connect({ wsEndpoint });
+   *   const browser = await chromium.connect(wsEndpoint);
    *   // Close browser instance.
    *   await browserServer.close();
    * })();
