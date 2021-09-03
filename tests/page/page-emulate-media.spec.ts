8cc404c68e08ef6a083254690bb2ea7581c53463
Max Schmitt <max@schmitt.mx>
2021-09-03 21:32:29 +0200

    skip forcedColors on Android

--- tests/page/page-emulate-media.spec.ts
+++ tests/page/page-emulate-media.spec.ts
@@ -126,8 +126,9 @@ it('should emulate reduced motion', async ({page}) => {
   await page.emulateMedia({ reducedMotion: null });
 });
 
-it('should emulate forcedColors ', async ({page, browserName}) => {
+it('should emulate forcedColors ', async ({page, browserName, isAndroid}) => {
   it.skip(browserName === 'webkit', 'https://bugs.webkit.org/show_bug.cgi?id=225281');
+  it.fixme(isAndroid);
   expect(await page.evaluate(() => matchMedia('(forced-colors: none)').matches)).toBe(true);
   await page.emulateMedia({ forcedColors: 'none' });
   expect(await page.evaluate(() => matchMedia('(forced-colors: none)').matches)).toBe(true);
