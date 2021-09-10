9f8e779ea1567e1a79d331c405e1d7107e86d449
Joel Einbinder <joel.einbinder@gmail.com>
2021-09-10 17:25:29 -0400

    remove stray "first"

--- types/types.d.ts
+++ types/types.d.ts
@@ -13285,9 +13285,8 @@ export interface Response {
   headersArray(): Promise<Array<Array<string>>>;
 
   /**
-   * Returns the first value of the header matching the name. The name is case insensitive. If multiple headers have the same
-   * name, for example `set-cookie`, the they are returned as a list separated by `, `. If no header is found, `null` is
-   * returned.
+   * Returns the value of the header matching the name. The name is case insensitive. If multiple headers have the same name,
+   * for example `set-cookie`, the they are returned as a list separated by `, `. If no header is found, `null` is returned.
    * @param name Name of the header.
    */
   headerValue(name: string): Promise<null|string>;
