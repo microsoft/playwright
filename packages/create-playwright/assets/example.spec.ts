87c2b4b200c4a41e8af34ccfb4fad228b1fd8edb
Max Schmitt <max@schmitt.mx>
2021-09-13 22:48:19 +0200

    chore(create-playwright): roll to 0.1.5 (#8889)

--- packages/create-playwright/assets/example.spec.ts
+++ packages/create-playwright/assets/example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await page.locator('text=Get Started').click();
-  await expect(page).toHaveTitle(/Getting Started/);
+  await page.locator('text=Get started').click();
+  await expect(page).toHaveTitle(/Getting started/);
 });
