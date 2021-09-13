microsoft/playwright

commit 477ef552fbcf186decf4939691d4ef8629965c8d

devops(ff-beta): fix rust installation while building firefox (#8049)

browser_patches/firefox-beta/build.sh
@@ -82,6 +82,23 @@ OBJ_FOLDER="obj-build-playwright"
 echo "mk_add_options MOZ_OBJDIR=@TOPSRCDIR@/${OBJ_FOLDER}" >> .mozconfig
 echo "ac_add_options --disable-crashreporter" >> .mozconfig
 
+if [[ $1 != "--juggler" ]]; then
+  # TODO: rustup is not in the PATH on Windows
+  if command -v rustup >/dev/null; then
+    # We manage Rust version ourselves.
+    echo "-- Using rust v${RUST_VERSION}"
+    rustup install "${RUST_VERSION}"
+    rustup default "${RUST_VERSION}"
+  fi
+
+  # TODO: cargo is not in the PATH on Windows
+  if command -v cargo >/dev/null; then
+    echo "-- Using cbindgen v${CBINDGEN_VERSION}"
+    cargo install cbindgen --version "${CBINDGEN_VERSION}"
+  fi
+  ./mach build
+fi
+
 if [[ $1 == "--full" || $2 == "--full" ]]; then
   if [[ "$(uname)" == "Darwin" || "$(uname)" == "Linux" ]]; then
     SHELL=/bin/sh ./mach --no-interactive bootstrap --application-choice=browser
@@ -98,21 +115,6 @@ fi
 
 if [[ $1 == "--juggler" ]]; then
   ./mach build faster
-else
-  # TODO: rustup is not in the PATH on Windows
-  if command -v rustup >/dev/null; then
-    # We manage Rust version ourselves.
-    echo "-- Using rust v${RUST_VERSION}"
-    rustup install "${RUST_VERSION}"
-    rustup default "${RUST_VERSION}"
-  fi
-
-  # TODO: cargo is not in the PATH on Windows
-  if command -v cargo >/dev/null; then
-    echo "-- Using cbindgen v${CBINDGEN_VERSION}"
-    cargo install cbindgen --version "${CBINDGEN_VERSION}"
-  fi
-  ./mach build
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then

browser_patches/firefox/build.sh
@@ -82,6 +82,23 @@ OBJ_FOLDER="obj-build-playwright"
 echo "mk_add_options MOZ_OBJDIR=@TOPSRCDIR@/${OBJ_FOLDER}" >> .mozconfig
 echo "ac_add_options --disable-crashreporter" >> .mozconfig
 
+if [[ $1 != "--juggler" ]]; then
+  # TODO: rustup is not in the PATH on Windows
+  if command -v rustup >/dev/null; then
+    # We manage Rust version ourselves.
+    echo "-- Using rust v${RUST_VERSION}"
+    rustup install "${RUST_VERSION}"
+    rustup default "${RUST_VERSION}"
+  fi
+
+  # TODO: cargo is not in the PATH on Windows
+  if command -v cargo >/dev/null; then
+    echo "-- Using cbindgen v${CBINDGEN_VERSION}"
+    cargo install cbindgen --version "${CBINDGEN_VERSION}"
+  fi
+  ./mach build
+fi
+
 if [[ $1 == "--full" || $2 == "--full" ]]; then
   if [[ "$(uname)" == "Darwin" || "$(uname)" == "Linux" ]]; then
     SHELL=/bin/sh ./mach --no-interactive bootstrap --application-choice=browser
@@ -98,21 +115,6 @@ fi
 
 if [[ $1 == "--juggler" ]]; then
   ./mach build faster
-else
-  # TODO: rustup is not in the PATH on Windows
-  if command -v rustup >/dev/null; then
-    # We manage Rust version ourselves.
-    echo "-- Using rust v${RUST_VERSION}"
-    rustup install "${RUST_VERSION}"
-    rustup default "${RUST_VERSION}"
-  fi
-
-  # TODO: cargo is not in the PATH on Windows
-  if command -v cargo >/dev/null; then
-    echo "-- Using cbindgen v${CBINDGEN_VERSION}"
-    cargo install cbindgen --version "${CBINDGEN_VERSION}"
-  fi
-  ./mach build
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then

commit 5953472899d47950a18c39d9031640293330d156

devops: fix --full command in FF which installs deps (#8878)
Broken since https://github.com/microsoft/playwright/pull/8049

Previously when doing `build.sh --full` it was installing deps + building. Currently when passing `--full` it does not install the deps before, since it tries to compile before.

browser_patches/firefox-beta/build.sh
@@ -102,7 +102,6 @@ if [[ $1 != "--juggler" ]]; then
     echo "-- Using cbindgen v${CBINDGEN_VERSION}"
     cargo install cbindgen --version "${CBINDGEN_VERSION}"
   fi
-  ./mach build
 fi
 
 if [[ $1 == "--full" || $2 == "--full" ]]; then
@@ -121,6 +120,8 @@ fi
 
 if [[ $1 == "--juggler" ]]; then
   ./mach build faster
+else
+  ./mach build
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then

browser_patches/firefox/build.sh
@@ -102,7 +102,6 @@ if [[ $1 != "--juggler" ]]; then
     echo "-- Using cbindgen v${CBINDGEN_VERSION}"
     cargo install cbindgen --version "${CBINDGEN_VERSION}"
   fi
-  ./mach build
 fi
 
 if [[ $1 == "--full" || $2 == "--full" ]]; then
@@ -121,6 +120,8 @@ fi
 
 if [[ $1 == "--juggler" ]]; then
   ./mach build faster
+else
+  ./mach build
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then