43213614a1eb83b9a328674c9f1ba6b3243d826a
Dmitry Gozman <dgozman@gmail.com>
2021-09-16 15:51:27 -0700

    fix(test runner): after hooks step should not be nested (#8969)

--- src/test/index.ts
+++ src/test/index.ts
@@ -212,8 +212,12 @@ export const test = _baseTest.extend<TestFixtures, WorkerAndFileFixtures>({
       }
       (context as any)._csi = {
         onApiCallBegin: (apiCall: string) => {
-          const testInfoImpl = testInfo as any;
-          const step = testInfoImpl._addStep('pw:api', apiCall, false);
+          const step = (testInfo as any)._addStep({
+            category: 'pw:api',
+            title: apiCall,
+            canHaveChildren: false,
+            forceNoParent: false,
+          });
           return { userObject: step };
         },
         onApiCallEnd: (data: { userObject: any }, error?: Error) => {
