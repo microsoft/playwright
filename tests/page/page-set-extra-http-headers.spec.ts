cc15227327dd9a0484e5d9eb158914e238434b65
Yury Semikhatsky <yurys@chromium.org>
2021-09-17 17:19:13 -0700

    fix: do not dedup header values (#8998)

--- tests/page/page-set-extra-http-headers.spec.ts
+++ tests/page/page-set-extra-http-headers.spec.ts
@@ -60,3 +60,11 @@ it('should throw for non-string header values', async ({page}) => {
   const error2 = await page.context().setExtraHTTPHeaders({ 'foo': true }).catch(e => e);
   expect(error2.message).toContain('Expected value of header "foo" to be String, but "boolean" is found.');
 });
+
+it('should not duplicate referer header', async ({page, server, browserName}) => {
+  it.fail(browserName === 'chromium', 'Request has referer and Referer');
+  await page.setExtraHTTPHeaders({ 'referer': server.EMPTY_PAGE });
+  const response = await page.goto(server.EMPTY_PAGE);
+  expect(response.ok()).toBe(true);
+  expect(response.request().headers()['referer']).toBe(server.EMPTY_PAGE);
+});
