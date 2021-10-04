ff02184acf5774477eeff83d68846e0ca3549525
Diego Pino <dpino@users.noreply.github.com>
2021-10-04 19:49:34 +0800

    browser(webkit): roll to 2021/09/30 (#9288)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/WebKit/WebKit.git"
 BASE_BRANCH="main"
-BASE_REVISION="6bfc8637e750c252fdfb9388954e34b18e7b9803"
+BASE_REVISION="5f99dfc43d30d3af1cabbf24d111acbff994808b"
