d278675a628ebeb66cd64508d6a2dbacae3042e3
Andrey Lushnikov <aslushnikov@gmail.com>
2021-10-08 13:05:32 -0700

    devops: start using Xcode 13 to compile Firefox (#9401)
    It didn't compile withh Xcode 12.2, but does compile with Xcode 13
    locally for me.
    
    Moving on to Xcode 13 then.

--- browser_patches/firefox-beta/build.sh
+++ browser_patches/firefox-beta/build.sh
@@ -26,9 +26,9 @@ rm -rf .mozconfig
 
 if [[ "$(uname)" == "Darwin" ]]; then
   CURRENT_HOST_OS_VERSION=$(getMacVersion)
-  # As of Oct 2021, building Firefox requires XCode 12.2
+  # As of Oct 2021, building Firefox requires XCode 13
   if [[ "${CURRENT_HOST_OS_VERSION}" == "11."* ]]; then
-    selectXcodeVersionOrDie "12.2"
+    selectXcodeVersionOrDie "13"
   else
     echo "ERROR: ${CURRENT_HOST_OS_VERSION} is not supported"
     exit 1
