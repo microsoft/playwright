fdf48f0673643bea1715d76c48481ce65cc214a3
Max Schmitt <max@schmitt.mx>
2021-10-13 19:44:14 +0200

    update fixme description

--- tests/page/page-request-fulfill.spec.ts
+++ tests/page/page-request-fulfill.spec.ts
@@ -287,7 +287,7 @@ it('should fulfill with fetch response that has multiple set-cookie', async ({ p
 
 it('headerValue should return set-cookie from intercepted response', async ({ page, server, browserName }) => {
   it.fail(browserName === 'chromium', 'Set-Cookie is missing in response after interception');
-  it.fixme(browserName === 'webkit', 'Set-Cookie is missing in response after interception');
+  it.fixme(browserName === 'webkit', 'Set-Cookie with \n in intercepted response does not pass validation in WebCore, see also https://github.com/microsoft/playwright/pull/9273');
   await page.route('**/empty.html', async route => {
     route.fulfill({
       status: 200,
