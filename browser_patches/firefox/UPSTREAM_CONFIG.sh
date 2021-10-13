283bdb8916cbd0df9c94dbe139ac6138cb3746df
Andrey Lushnikov <aslushnikov@gmail.com>
2021-10-13 15:49:41 -0700

    browser(firefox): roll Firefox to M93 (#9487)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="release"
-BASE_REVISION="167df0854070d0f75ba0d29a679207dc58a87785"
+BASE_REVISION="67d36a1aa8bf0b28a8c74294f93e52573ceda137"
