eda773adbb1143efaecf80fa5c09b06b0ae44a6b
Max Schmitt <max@schmitt.mx>
2021-10-14 11:09:07 +0200

    Fix icons

--- packages/playwright-core/src/web/traceViewer/webpack.config.js
+++ packages/playwright-core/src/web/traceViewer/webpack.config.js
@@ -34,10 +34,6 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
-      {
-        test: /\.ttf$/,
-        use: ['file-loader']
-      }
     ]
   },
   plugins: [
