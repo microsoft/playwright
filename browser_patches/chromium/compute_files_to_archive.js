90e204fe2b5bb518a763b7fdd619ed5832471faf
Andrey Lushnikov <lushnikov@chromium.org>
2021-11-02 07:34:45 -1000

    devops: fix archiving of Chromium Linux Arm64 build

--- browser_patches/chromium/compute_files_to_archive.js
+++ browser_patches/chromium/compute_files_to_archive.js
@@ -13,10 +13,18 @@ const excludeList = new Set([
   'interactive_ui_tests.exe',
 ]);
 
+// There is no upstream configuration for packaging Linux Arm64 builds,
+// so we build one by replacing x86_64-specific nacl binary with arm one.
+const replaceMap = {
+  '--linux-arm64': {
+    'nacl_irt_x86_64.nexe': 'nacl_irt_arm.nexe',
+  },
+}[process.argv[3]] || {};
+
 const entries = [
   ...(config.files || []),
   ...(config.dirs || []),
-].filter(entry => !excludeList.has(entry));
+].filter(entry => !excludeList.has(entry)).map(entry => replaceMap[entry] || entry);
 
 for (const entry of entries)
   console.log(entry);
