8f8c507e82773be41bffd19f65f8ed77ca81f057
Andrey Lushnikov <lushnikov@chromium.org>
2021-11-02 08:58:44 -1000

    drop win32

--- browser_patches/ffmpeg/crosscompile-from-linux.sh
+++ browser_patches/ffmpeg/crosscompile-from-linux.sh
@@ -20,7 +20,6 @@ function die() { echo "$@"; exit 1; }
 
 
 PREFIX="${HOME}/prefix"
-TOOLCHAIN_PREFIX_32="/usr/bin/i686-w64-mingw32-"
 TOOLCHAIN_PREFIX_64="/usr/bin/x86_64-w64-mingw32-"
 TOOLCHAIN_PREFIX_ARM64="/usr/bin/aarch64-linux-gnu-"
 
@@ -33,11 +32,7 @@ if [[ "$(uname)" != "Linux" ]]; then
   exit 1
 fi
 
-if [[ "$1" == "--win32" ]]; then
-  arch="win32";
-  toolchain_prefix="${TOOLCHAIN_PREFIX_32}"
-  binary="ffmpeg.exe"
-elif [[ "$1" == "--win64" ]]; then
+if [[ "$1" == "--win64" ]]; then
   arch="win64";
   toolchain_prefix="${TOOLCHAIN_PREFIX_64}"
   binary="ffmpeg.exe"
@@ -46,9 +41,9 @@ elif [[ "$1" == "--linux-arm64" ]]; then
   toolchain_prefix="${TOOLCHAIN_PREFIX_ARM64}"
   binary="ffmpeg"
 elif [[ -z "$1" ]]; then
-  die "ERROR: expect --win32, --win64 or --linux-arm64 as the first argument"
+  die "ERROR: expect --win64 or --linux-arm64 as the first argument"
 else
-  die "ERROR: unknown arch '$1' - expected --win32 or --win64"
+  die "ERROR: unknown arch '$1' - expected --win64 or --linux-arm64"
 fi
 
 output_path="$2"
@@ -85,9 +80,7 @@ function build_libvpx {
   # Cross-compiling libvpx according to the docs:
   # - https://chromium.googlesource.com/webm/libvpx/+/master/README
   local target=""
-  if [[ $arch == "win32" ]]; then
-    target="x86-win32-gcc";
-  elif [[ $arch == "win64" ]]; then
+  if [[ $arch == "win64" ]]; then
     target="x86_64-win64-gcc";
   elif [[ $arch == "linux-arm64" ]]; then
     target="arm64-linux-gcc";
@@ -109,10 +102,7 @@ function build_ffmpeg {
 
   local ffmpeg_arch=""
   local ffmpeg_target_os=""
-  if [[ $arch == "win32" ]]; then
-    ffmpeg_arch="x86";
-    ffmpeg_target_os="mingw32"
-  elif [[ $arch == "win64" ]]; then
+  if [[ $arch == "win64" ]]; then
     ffmpeg_arch="x86_64";
     ffmpeg_target_os="mingw32"
   elif [[ $arch == "linux-arm64" ]]; then
