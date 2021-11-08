12eead057a9b10c07928a9a27f14f107580d598b
Andrey Lushnikov <aslushnikov@gmail.com>
2021-11-08 01:04:59 -0800

    devops: support webkit compilation for Ubuntu 20.04 arm64 (#10122)

--- browser_patches/checkout_build_archive_upload.sh
+++ browser_patches/checkout_build_archive_upload.sh
@@ -285,6 +285,13 @@ elif [[ "$BUILD_FLAVOR" == "webkit-ubuntu-20.04" ]]; then
   EXPECTED_HOST_OS="Ubuntu"
   EXPECTED_HOST_OS_VERSION="20.04"
   BUILD_BLOB_NAME="webkit-ubuntu-20.04.zip"
+elif [[ "$BUILD_FLAVOR" == "webkit-ubuntu-20.04-arm64" ]]; then
+  BROWSER_NAME="webkit"
+  EXTRA_BUILD_ARGS="--full"
+  EXPECTED_HOST_OS="Ubuntu"
+  EXPECTED_HOST_OS_VERSION="20.04"
+  EXPECTED_ARCH="aarch64"
+  BUILD_BLOB_NAME="webkit-ubuntu-20.04-arm64.zip"
 elif [[ "$BUILD_FLAVOR" == "webkit-win64" ]]; then
   BROWSER_NAME="webkit"
   EXPECTED_HOST_OS="MINGW"
