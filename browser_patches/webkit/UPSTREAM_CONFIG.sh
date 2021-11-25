1abc36306d1f0e9ba0851b60a2f53402c36422d9
Diego Pino Garcia <dpino@igalia.com>
2021-11-25 03:14:16 +0000

    browser(webkit): roll to 25/11/21

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/WebKit/WebKit.git"
 BASE_BRANCH="main"
-BASE_REVISION="a8d27d2f68e72abd50ed4a142af0d05f9aef662c"
+BASE_REVISION="bed174a39a11c5fa8e1c3658dfffb46305ddf5f4"
