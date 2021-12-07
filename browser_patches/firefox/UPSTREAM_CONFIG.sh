00bc2ab53114a4b1a8f9b7b9d42a3a75c2836705
Andrey Lushnikov <aslushnikov@gmail.com>
2021-12-07 11:53:17 -0800

    browser(firefox): roll Firefox stable to M95 (#10760)
    https://github.com/microsoft/playwright/issues/10759

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="release"
-BASE_REVISION="e4cff24898044cf5d47b8b7d1d8fc2e2c95c6824"
+BASE_REVISION="28038f11907995e6e8c9a553e8f79bd0aa875637"
