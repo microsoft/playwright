66292a5895ad56255b3bcdf8aeb860eb40caac6a
Andrey Lushnikov <aslushnikov@gmail.com>
2021-12-14 23:24:25 -0800

    browser(firefox-beta): roll Firefox beta to 96.0b5 (#10924)
    Review URL: https://github.com/aslushnikov/juggler/commit/4cedc704b15ac9926819d79b04407d4f9a09108e
    
    Firefox 96 changes default sameSite cookie value, so this patch
    updates tests to accomodate for this.

--- browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="458fb00dcc8296a5a6d1c5b0b6f9417e1fa2c45c"
+BASE_REVISION="b4531b9d788a8ea89bbf75e504cecf04da4a396f"
