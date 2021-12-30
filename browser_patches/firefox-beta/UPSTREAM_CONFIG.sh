a28935dcb81d1ec2d6135a1a91381a1e216ec923
Andrey Lushnikov <aslushnikov@gmail.com>
2021-12-29 22:18:43 -0700

    browser(ff-beta): roll Firefox beta to 96.0b11 (#11139)

--- browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="b4531b9d788a8ea89bbf75e504cecf04da4a396f"
+BASE_REVISION="1be2cb1b587815804b4123d877d7a7be5aa8e342"
