136fab70417c18c69215e5c9bdd37a09066c2c89
Pavel Feldman <pavel.feldman@gmail.com>
2022-01-24 11:25:24 -0800

    fix(prepare): don't change package files on prepare (#11579)

--- packages/playwright-chromium/install.js
+++ packages/playwright-chromium/install.js
@@ -14,6 +14,13 @@
  * limitations under the License.
  */
 
-const { installBrowsersForNpmInstall } = require('playwright-core/lib/utils/registry');
+let install;
 
-installBrowsersForNpmInstall(['chromium', 'ffmpeg']);
+try {
+  install = require('playwright-core/lib/utils/registry').installBrowsersForNpmInstall;
+} catch (e) {
+  // Dev build, don't install browsers by default.
+}
+
+if (install)
+  install(['chromium', 'ffmpeg']);
