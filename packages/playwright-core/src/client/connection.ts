4d42f6a1c4bb182dae5bf614357fc8c47d1c4527
campersau <buchholz.bastian@googlemail.com>
2022-01-24 18:58:57 +0100

    chore: remove unused _waitingForObject from Connection (#11474)

--- packages/playwright-core/src/client/connection.ts
+++ packages/playwright-core/src/client/connection.ts
@@ -57,7 +57,6 @@ class Root extends ChannelOwner<channels.RootChannel> {
 
 export class Connection extends EventEmitter {
   readonly _objects = new Map<string, ChannelOwner>();
-  private _waitingForObject = new Map<string, any>();
   onmessage = (message: object): void => {};
   private _lastId = 0;
   private _callbacks = new Map<number, { resolve: (a: any) => void, reject: (a: Error) => void, stackTrace: ParsedStackTrace | null }>();
@@ -266,11 +265,6 @@ export class Connection extends EventEmitter {
         break;
       default:
         throw new Error('Missing type ' + type);
-    }
-    const callback = this._waitingForObject.get(guid);
-    if (callback) {
-      callback(result);
-      this._waitingForObject.delete(guid);
     }
     return result;
   }
