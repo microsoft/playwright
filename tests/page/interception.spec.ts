c9e99d5d97ec60de98bd2b91720bffb505b4a542
Dmitry Gozman <dgozman@gmail.com>
2022-02-03 17:16:40 -0800

    test: reenable "should intercept network activity from worker 2" (#11851)

--- tests/page/interception.spec.ts
+++ tests/page/interception.spec.ts
@@ -108,8 +108,6 @@ it('should intercept network activity from worker', async function({ page, serve
 it('should intercept network activity from worker 2', async function({ page, server, isElectron, isAndroid, browserName, browserMajorVersion }) {
   it.skip(isAndroid);
   it.fixme(isElectron);
-  it.fixme(browserName === 'chromium' && browserMajorVersion === 97, '@see https://github.com/microsoft/playwright/issues/10048');
-  it.fixme(browserName === 'chromium' && browserMajorVersion === 99, '@see https://github.com/microsoft/playwright/issues/11593');
 
   const url = server.PREFIX + '/worker/worker.js';
   await page.route(url, route => {
