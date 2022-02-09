afd0f86efa2b05a3ecae19ac475e82a4b28675f7
Andrey Lushnikov <lushnikov@chromium.org>
2022-02-09 12:28:51 -0700

    cherry-pick(#11973): fix: proper chrome-beta channel installation on MacOS
    SHA: 1e1a6acaf7f1f16cb4a83302802f040238611120
    
    chrome-beta installation on MacOS should download universal binaries.
    
    The old download URL for chrome-beta was downloading Chrome Beta M96

--- packages/playwright-core/bin/reinstall_chrome_beta_mac.sh
+++ packages/playwright-core/bin/reinstall_chrome_beta_mac.sh
@@ -4,7 +4,7 @@ set -x
 
 rm -rf "/Applications/Google Chrome Beta.app"
 cd /tmp
-curl -o ./googlechromebeta.dmg -k https://dl.google.com/chrome/mac/beta/googlechromebeta.dmg
+curl -o ./googlechromebeta.dmg -k https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg
 hdiutil attach -nobrowse -quiet -noautofsck -noautoopen -mountpoint /Volumes/googlechromebeta.dmg ./googlechromebeta.dmg
 cp -rf "/Volumes/googlechromebeta.dmg/Google Chrome Beta.app" /Applications
 hdiutil detach /Volumes/googlechromebeta.dmg
