ab525c6e80854eb26385d18119509b04e0dd622e
Pavel Feldman <pavel.feldman@gmail.com>
2022-02-10 06:31:11 -0800

    chore: revert "fix(test-runner): escape backslashes in win cli (#11763)"

--- packages/playwright-test/src/cli.ts
+++ packages/playwright-test/src/cli.ts
@@ -181,7 +181,7 @@ function forceRegExp(pattern: string): RegExp {
   const match = pattern.match(/^\/(.*)\/([gi]*)$/);
   if (match)
     return new RegExp(match[1], match[2]);
-  return new RegExp(pattern.replace(/\\/g, '\\\\'), 'gi');
+  return new RegExp(pattern, 'gi');
 }
 
 function overridesFromOptions(options: { [key: string]: any }): Config {
