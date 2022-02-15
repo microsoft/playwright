7789d0b73f7e3c22d3da944ce21750d04ad63b2d
Andrey Lushnikov <lushnikov@chromium.org>
2022-02-15 15:00:15 -0700

    devops: fix telegram reporting across platforms (#12135)
    Encoding emoji across platforms is a pain!

--- browser_patches/checkout_build_archive_upload.sh
+++ browser_patches/checkout_build_archive_upload.sh
@@ -421,7 +421,8 @@ if generate_and_upload_browser_build 2>&1 | ./sanitize_and_compress_log.js $LOG_
       fi
     done;
     LAST_COMMIT_MESSAGE=$(git log --format=%s -n 1 HEAD -- "./${BROWSER_NAME}/BUILD_NUMBER")
-    send_telegram_message "<b>${BROWSER_DISPLAY_NAME} r${BUILD_NUMBER} COMPLETE! \\u270</b> ${LAST_COMMIT_MESSAGE}"
+    CHECKMARK_CHAR=$(printf '\xe2\x9c\x85')
+    send_telegram_message "<b>${BROWSER_DISPLAY_NAME} r${BUILD_NUMBER} COMPLETE! ${CHECKMARK_CHAR}</b> ${LAST_COMMIT_MESSAGE}"
     if [[ "${BROWSER_DISPLAY_NAME}" != "chromium-with-symbols" ]]; then
       create_roll_into_playwright_pr $BROWSER_NAME $BUILD_NUMBER
     fi
@@ -447,7 +448,8 @@ else
   fi
   # Upload logs only in case of failure and report failure.
   ./upload.sh "${LOG_BLOB_PATH}" ${LOG_PATH} || true
-  send_telegram_message "$BUILD_ALIAS -- ${FAILED_STEP} failed! \\u274c <a href='https://playwright.azureedge.net/builds/${LOG_BLOB_PATH}'>${LOG_BLOB_NAME}</a> -- <a href='$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID'>GitHub Action Logs</a>"
+  CROSS_CHAR=$(printf '\xe2\x9d\x8c')
+  send_telegram_message "$BUILD_ALIAS -- ${FAILED_STEP} failed! ${CROSS_CHAR} <a href='https://playwright.azureedge.net/builds/${LOG_BLOB_PATH}'>${LOG_BLOB_NAME}</a> -- <a href='$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID'>GitHub Action Logs</a>"
   exit 1
 fi
 
