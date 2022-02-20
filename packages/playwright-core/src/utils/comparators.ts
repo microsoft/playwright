2add2817cb59d5fcfb726424de3e78f1a8ff2090
Ben Lu <ayroblu@gmail.com>
2022-02-20 15:31:21 +0000

    add ssim wip

--- packages/playwright-core/src/utils/comparators.ts
+++ packages/playwright-core/src/utils/comparators.ts
@@ -18,6 +18,8 @@
 import colors from 'colors/safe';
 import jpeg from 'jpeg-js';
 import pixelmatch from 'pixelmatch';
+import ssim, {Options} from 'ssim.js';
+import {PNGWithMetadata} from 'pngjs';
 import { diff_match_patch, DIFF_INSERT, DIFF_DELETE, DIFF_EQUAL } from '../third_party/diff_match_patch';
 
 // Note: we require the pngjs version of pixelmatch to avoid version mismatches.
@@ -43,29 +45,133 @@ function compareBuffersOrStrings(actualBuffer: Buffer | string, expectedBuffer: 
   return null;
 }
 
-function compareImages(mimeType: string, actualBuffer: Buffer | string, expectedBuffer: Buffer, options: ImageComparatorOptions = {}): ComparatorResult {
+function compareImages(
+  mimeType: string,
+  actualBuffer: Buffer | string,
+  expectedBuffer: Buffer,
+  options: ImageComparatorOptions = {}
+): ComparatorResult {
   if (!actualBuffer || !(actualBuffer instanceof Buffer))
     return { errorMessage: 'Actual result should be a Buffer.' };
 
-  const actual = mimeType === 'image/png' ? PNG.sync.read(actualBuffer) : jpeg.decode(actualBuffer);
-  const expected = mimeType === 'image/png' ? PNG.sync.read(expectedBuffer) : jpeg.decode(expectedBuffer);
+  const actual =
+    mimeType === 'image/png'
+      ? PNG.sync.read(actualBuffer)
+      : jpeg.decode(actualBuffer);
+  const expected =
+    mimeType === 'image/png'
+      ? PNG.sync.read(expectedBuffer)
+      : jpeg.decode(expectedBuffer);
   if (expected.width !== actual.width || expected.height !== actual.height) {
     return {
-      errorMessage: `Sizes differ; expected image ${expected.width}px X ${expected.height}px, but got ${actual.width}px X ${actual.height}px. `
+      errorMessage: `Sizes differ; expected image ${expected.width}px X ${expected.height}px, but got ${actual.width}px X ${actual.height}px. `,
     };
   }
   const diff = new PNG({ width: expected.width, height: expected.height });
   const thresholdOptions = { threshold: 0.2, ...options };
-  const count = pixelmatch(expected.data, actual.data, diff.data, expected.width, expected.height, thresholdOptions);
+  const count =
+    process.env['USE_SSIM'] && actual instanceof PNG && expected instanceof PNG
+      ? compareWithSsim(actual, expected, diff.data, {} as any)
+      : pixelmatch(
+          expected.data,
+          actual.data,
+          diff.data,
+          expected.width,
+          expected.height,
+          thresholdOptions
+      );
 
   const pixelCount1 = options.pixelCount;
-  const pixelCount2 = options.pixelRatio !== undefined ? expected.width * expected.height * options.pixelRatio : undefined;
+  const pixelCount2 =
+    options.pixelRatio !== undefined
+      ? expected.width * expected.height * options.pixelRatio
+      : undefined;
   let pixelCount;
   if (pixelCount1 !== undefined && pixelCount2 !== undefined)
     pixelCount = Math.min(pixelCount1, pixelCount2);
-  else
-    pixelCount = pixelCount1 ?? pixelCount2 ?? 0;
+  else pixelCount = pixelCount1 ?? pixelCount2 ?? 0;
   return count > pixelCount ? { diff: PNG.sync.write(diff) } : null;
+}
+
+function compareWithSsim(
+  actual: PNGWithMetadata,
+  expected: PNGWithMetadata,
+  output: Buffer,
+  options: Options
+): number {
+  const width = expected.width;
+  const height = expected.height;
+  const reference: ImageData = {
+    data: new Uint8ClampedArray(expected.data),
+    width,
+    height,
+  };
+  const test: ImageData = {
+    data: new Uint8ClampedArray(actual.data),
+    width: actual.width,
+    height: actual.height,
+  };
+  const { ssim_map, mssim } = ssim(test, reference, {});
+  const diffPixels = (1 - mssim) * width * height;
+  const diffRgbaPixels = new DataView(output.buffer, output.byteOffset);
+
+  for (let ln = 0; ln !== height; ++ln) {
+    for (let pos = 0; pos !== width; ++pos) {
+      const rpos = ln * width + pos;
+      // initial value is transparent.  We'll add in the SSIM offset.
+      const diffResult = Math.floor(
+          0xff *
+          (1 -
+            ssim_map.data[
+                ssim_map.width * Math.round((ssim_map.height * ln) / height) +
+                Math.round((ssim_map.width * pos) / width)
+            ])
+      );
+      // red (ff) green (00) blue (00) alpha (00)
+      const diffValue = handleTransparent(
+          0xff000000 + diffResult,
+          actual.data,
+          rpos * 4
+      );
+
+      diffRgbaPixels.setUint32(rpos * 4, diffValue);
+    }
+  }
+
+  return diffPixels;
+}
+function isTransparent(rgba: number) {
+  const a = rgba & 0xff;
+  // const b = (rgba >> 8) & 0xff;
+  // const g = (rgba >> 16) & 0xff;
+  // const r = (rgba >> 24) & 0xff;
+  return a === 0;
+}
+function handleTransparent(
+  diffValue: number,
+  testImageBuffer: Buffer,
+  i: number
+) {
+  if (isTransparent(diffValue)) {
+    const v = Math.floor(getGrayPixel(testImageBuffer, i, 0.2));
+    return 255 + (v << 8) + (v << 16) + (v << 24);
+  } else {
+    return diffValue;
+  }
+}
+function rgb2y(r: number, g: number, b: number) {
+  return r * 0.29889531 + g * 0.58662247 + b * 0.11448223;
+}
+// blend semi-transparent color with white
+function blend(c: number, a: number) {
+  return 255 + (c - 255) * a;
+}
+function getGrayPixel(img: Buffer, i: number, alpha: number) {
+  const r = img[i + 0];
+  const g = img[i + 1];
+  const b = img[i + 2];
+  const a = img[i + 3];
+  return blend(rgb2y(r, g, b), (alpha * a) / 255);
 }
 
 function compareText(actual: Buffer | string, expectedBuffer: Buffer): ComparatorResult {
