689d8196e0a96ba1bfe386301a0e9ced283626b9
Andrey Lushnikov <lushnikov@chromium.org>
2022-03-10 18:19:44 -0700

    chore: follow-up to fix docs (#12668)
    This is a follow-up to 49e66c7f08c192f8b79664f8ccb032eeaf665b84 that was landed without bots.
    
    I ran bots manually on Mac & Linux.

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -8076,7 +8076,7 @@ export interface ElementHandle<T=Node> extends JSHandle<T> {
 
     /**
      * When set to `"ready"`, screenshot will wait for
-     * [`document.fonts.ready()`](https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/ready) promise to resolve in all
+     * [`document.fonts.ready`](https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/ready) promise to resolve in all
      * frames. Defaults to `"nowait"`.
      */
     fonts?: "ready"|"nowait";
@@ -15606,7 +15606,7 @@ export interface LocatorScreenshotOptions {
 
   /**
    * When set to `"ready"`, screenshot will wait for
-   * [`document.fonts.ready()`](https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/ready) promise to resolve in all
+   * [`document.fonts.ready`](https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/ready) promise to resolve in all
    * frames. Defaults to `"nowait"`.
    */
   fonts?: "ready"|"nowait";
@@ -15784,7 +15784,7 @@ export interface PageScreenshotOptions {
 
   /**
    * When set to `"ready"`, screenshot will wait for
-   * [`document.fonts.ready()`](https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/ready) promise to resolve in all
+   * [`document.fonts.ready`](https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/ready) promise to resolve in all
    * frames. Defaults to `"nowait"`.
    */
   fonts?: "ready"|"nowait";
