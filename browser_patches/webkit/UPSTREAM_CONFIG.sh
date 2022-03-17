8eb56a4f50ddd4f418bae0a0df8f6ead3c3a1174
Diego Pino Garcia <dpino@igalia.com>
2022-03-17 13:07:42 +0000

    browser(webkit): rebase to 03/10/22 (r291123)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/WebKit/WebKit.git"
 BASE_BRANCH="main"
-BASE_REVISION="fe3b67d303e587d46dd8eb9793327fa965a60aa7"
+BASE_REVISION="196fcd71682fd2f6a2319bca077b8e7ff0f0daac"
