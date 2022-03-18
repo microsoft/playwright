4c50728a1b59167937fdb8edf4e0b5528c8cab60
Dmitry Gozman <dgozman@gmail.com>
2022-03-18 13:34:45 -0700

    fix(click): workaround elementsFromPoint issue in Chromium
    Block elements inside elements are not returned correctly
    from `document.elementsFromPoint(x, y)` in some cases.
    See https://bugs.chromium.org/p/chromium/issues/detail?id=1307458 for details.

--- packages/playwright-core/src/server/injected/injectedScript.ts
+++ packages/playwright-core/src/server/injected/injectedScript.ts
@@ -804,8 +804,11 @@ export class InjectedScript {
     while (container) {
       // elementFromPoint works incorrectly in Chromium (http://crbug.com/1188919),
       // so we use elementsFromPoint instead.
-      const elements = (container as Document).elementsFromPoint(x, y);
-      const innerElement = elements[0] as Element | undefined;
+      const elements: Element[] = container.elementsFromPoint(x, y);
+      let innerElement = elements[0] as Element | undefined;
+      // Workaround https://bugs.chromium.org/p/chromium/issues/detail?id=1307458.
+      if (elements[0] && elements[1] && elements[0].contains(elements[1]) && container.elementFromPoint(x, y) === elements[1])
+        innerElement = elements[1];
       if (!innerElement || element === innerElement)
         break;
       element = innerElement;
