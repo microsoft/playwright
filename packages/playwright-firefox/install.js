90d5990e7551c8d84e982a00b8141353bec24047
Pavel Feldman <pavel.feldman@gmail.com>
2022-04-06 22:09:10 -0800

    chore: fix installation scripts (#13376)

--- packages/playwright-firefox/install.js
+++ packages/playwright-firefox/install.js
@@ -17,7 +17,7 @@
 let install;
 
 try {
-  install = require('playwright-core/lib/server').registry.installBrowsersForNpmInstall;
+  install = require('playwright-core/lib/server').installBrowsersForNpmInstall;
 } catch (e) {
   // Dev build, don't install browsers by default.
 }
