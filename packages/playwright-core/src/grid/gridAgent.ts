c3b8f40c84ed99600534bb098a469bb83787b831
Yury Semikhatsky <yurys@chromium.org>
2022-04-13 12:01:19 -0700

    chore: send run_id from agent to server

--- packages/playwright-core/src/grid/gridAgent.ts
+++ packages/playwright-core/src/grid/gridAgent.ts
@@ -19,12 +19,14 @@ import WebSocket from 'ws';
 import { fork } from 'child_process';
 import { getPlaywrightVersion } from '../common/userAgent';
 
-export function launchGridAgent(agentId: string, gridURL: string) {
+export function launchGridAgent(agentId: string, gridURL: string, runId: string | undefined) {
   const log = debug(`pw:grid:agent:${agentId}`);
   log('created');
   const params = new URLSearchParams();
   params.set('pwVersion', getPlaywrightVersion(true /* majorMinorOnly */));
   params.set('agentId', agentId);
+  if (runId)
+    params.set('runId', runId);
   const ws = new WebSocket(gridURL.replace('http://', 'ws://') + `/registerAgent?` + params.toString());
   ws.on('message', (message: string) => {
     log('worker requested ' + message);
