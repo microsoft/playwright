2757b8703ccaec15c055c51b843466fa8c2ecbd1
Ross Wollman <rwoll@users.noreply.github.com>
2022-04-20 20:19:26 +0000

    add tests

---
+++ tests/installation/playwright-xyz-should-work.spec.ts
@@ -0,0 +1,29 @@
+
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { test, expect } from './npmTest';
+
+for (const pkg of ['playwright-chromium', 'playwright-firefox', 'playwright-webkit']) {
+  test(`${pkg} should work`, async ({ npm, exec, nodeVersion }) => {
+    const result = await npm('i', '--foreground-scripts', pkg);
+    expect(result).toHaveDownloaded([pkg.split('-')[1] as any]);
+    expect(result.combined()).not.toContain(`To avoid unexpected behavior, please install your dependencies first`);
+    await exec('node', ['./sanity.js', pkg]);
+    if (nodeVersion >= 14)
+      await exec('node', [`esm-${pkg}.mjs`]);
+
+  });
+}
