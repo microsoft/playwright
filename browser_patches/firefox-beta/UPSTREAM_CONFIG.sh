b01fc867bd75c55456b95a937866ce8b81ce2291
Andrey Lushnikov <lushnikov@chromium.org>
2022-04-20 09:12:43 -0700

    browser(firefox-beta): roll Firefox-Beta to m100
    conflict resolution: https://github.com/aslushnikov/juggler/commit/4b055478bf0ee776844005b46953a334e5919a30

--- browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="b98f0adbce7b259b1d929b8b9461c2830297daf9"
+BASE_REVISION="59b472e096b9d1d14d18fd844f20900a018f9d62"
