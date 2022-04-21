ec0c31aea9cbb6c094f0766b5c407dc0b006c430
Andrey Lushnikov <lushnikov@chromium.org>
2022-04-20 23:40:07 -0400

    port typescript types test

---
+++ tests/installation/typescript-types.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { test } from './npmTest';
+
+test('android types', async ({ npm, tsc, envOverrides, writeFiles }) => {
+  envOverrides['PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD'] = '1';
+  await npm('i', '-D', '@types/node@14.18.9');
+
+  const libraryPackages = [
+    'playwright',
+    'playwright-core',
+    'playwright-firefox',
+    'playwright-webkit',
+    'playwright-chromium',
+  ];
+  await npm('i', '@playwright/test', ...libraryPackages);
+
+  for (const libraryPackage of libraryPackages) {
+    const filename = libraryPackage + '.ts';
+    await writeFiles({
+      [filename]: `import { Page } from '${libraryPackage}';`,
+    });
+    await tsc(filename);
+  }
+
+  await tsc('playwright-test-types.ts');
+});
