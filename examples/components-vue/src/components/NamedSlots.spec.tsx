f933f766f4244d7e307c9b8a75eb8658858183da
Pavel Feldman <pavel.feldman@gmail.com>
2022-04-25 21:10:08 -0800

    chore: migrate vue component example to a new vite plugin (#13750)

--- examples/components-vue/src/components/NamedSlots.spec.tsx
+++ examples/components-vue/src/components/NamedSlots.spec.tsx
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/experimental-ct-vue/test'
-import NamedSlots from './components/NamedSlots.vue'
+
+import NamedSlots from './NamedSlots.vue'
 
 test.use({ viewport: { width: 500, height: 500 } })
 
