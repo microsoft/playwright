0abe369c1b4af52081d85d53cedc2600bf060b5c
Andrey Lushnikov <lushnikov@chromium.org>
2022-05-02 20:45:09 -0700

    browser(firefox): roll Firefox-stable to 99.0.1

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="release"
-BASE_REVISION="8e5f1f8a340acdc8e5327874ddebc317cfd69a5b"
+BASE_REVISION="d9b246069918b30ce0f8e16d2d404f95e4427f63"
