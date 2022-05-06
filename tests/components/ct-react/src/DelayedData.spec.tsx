96873f059b0531ac3b1d9ad003026e62e31a9982
Ross A. Wollman <ross.wollman@gmail.com>
2022-05-06 12:19:55 -0700

    fix broken test

--- tests/components/ct-react/src/DelayedData.spec.tsx
+++ tests/components/ct-react/src/DelayedData.spec.tsx
@@ -2,8 +2,7 @@ import { test, expect } from '@playwright/test';
 import { DelayedData } from './DelayedData';
 
 test('toHaveText works on delayed data', async ({ mount }) => {
-    test.fail();
-    test.setTimeout(1000);
+    test.setTimeout(5000);
 
     const component = await mount(<DelayedData data='complete' />);
     await expect(component).toHaveText('complete');
