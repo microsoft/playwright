836f7d83cac3743cee3c6667748ac555c75f6abb
Ross A. Wollman <ross.wollman@gmail.com>
2022-05-06 13:09:31 -0700

    add react 18 tests

---
+++ tests/components/ct-react-18/src/Button.spec.tsx
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { Button } from './Button';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should work', async ({ mount }) => {
+  let clicked = false;
+  const component = await mount(<Button onClick={() => clicked = true }></Button>);
+  await expect(component).toContainText('click me');
+  expect(clicked).toBe(false);
+  await component.click();
+  expect.poll(() => clicked).toBe(true);
+});
+
+test('should work inline', async ({ mount }) => {
+  let clicked = false;
+  const component = await mount(<div onClick={() => clicked = true }>click me</div>);
+  await expect(component).toContainText('click me');
+  expect(clicked).toBe(false);
+  await component.click();
+  expect.poll(() => clicked).toBe(true);
+});
