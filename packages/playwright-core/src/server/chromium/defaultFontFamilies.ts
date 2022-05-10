3377bb032adcb02e065f456ac59ab82aadbaef33
Dmitry Gozman <dgozman@gmail.com>
2022-05-10 17:40:13 +0100

    Revert "Revert "feat(chromium): roll to r1005 (#13999)""
    This reverts commit f0f65fa247ac9cb594acd45b52dc851e60109172.

--- packages/playwright-core/src/server/chromium/defaultFontFamilies.ts
+++ packages/playwright-core/src/server/chromium/defaultFontFamilies.ts
@@ -26,7 +26,6 @@ export const platformToFontFamilies: { [key in 'linux'|'mac'|'win']: Protocol.Pa
       'sansSerif': 'Arial',
       'cursive': 'Comic Sans MS',
       'fantasy': 'Impact',
-      'pictograph': 'Times New Roman'
     }
   },
   'mac': {
@@ -37,7 +36,6 @@ export const platformToFontFamilies: { [key in 'linux'|'mac'|'win']: Protocol.Pa
       'sansSerif': 'Helvetica',
       'cursive': 'Apple Chancery',
       'fantasy': 'Papyrus',
-      'pictograph': 'Apple Color Emoji'
     },
     'forScripts': [
       {
@@ -85,7 +83,6 @@ export const platformToFontFamilies: { [key in 'linux'|'mac'|'win']: Protocol.Pa
       'sansSerif': 'Arial',
       'cursive': 'Comic Sans MS',
       'fantasy': 'Impact',
-      'pictograph': 'Segoe UI Symbol'
     },
     'forScripts': [
       {
