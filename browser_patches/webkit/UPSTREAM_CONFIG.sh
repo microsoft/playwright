9159ceafb5bb307c54919df802b6788947ee22cc
Yury Semikhatsky <yurys@chromium.org>
2022-05-16 09:20:34 -0700

    browser(webkit): revert last change and reapply it properly (#14197)

--- browser_patches/webkit/UPSTREAM_CONFIG.sh
+++ browser_patches/webkit/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/WebKit/WebKit.git"
 BASE_BRANCH="main"
-BASE_REVISION="c2469dd6baa0034fe1ad0159788267d17f0bc7c2"
+BASE_REVISION="38f99b1251a27027b3eeea66ae7ae4a57b4144f2"
