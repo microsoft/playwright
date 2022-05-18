50b2d4aabecaafab05f55c11b108515b0d907715
Pavel Feldman <pavel.feldman@gmail.com>
2022-05-18 12:01:18 -0700

    chore: ease the stress tests (#14260)

--- tests/stress/contexts.spec.ts
+++ tests/stress/contexts.spec.ts
@@ -16,7 +16,7 @@
 
 import { contextTest as test, expect } from '../config/browserTest';
 
-for (let i = 0; i < 5000; ++i) {
+for (let i = 0; i < 1000; ++i) {
   test('cycle contexts ' + i, async ({ page, server }) => {
     await page.goto(server.PREFIX + '/stress/index.html');
     await expect(page.locator('text=Learn React')).toBeVisible();
