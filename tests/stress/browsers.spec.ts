bbb8672c3fd340d9e00ef85715879b426eab3164
Pavel Feldman <pavel.feldman@gmail.com>
2022-05-17 17:22:43 -0700

    test(stress): add stress tests

---
+++ tests/stress/browsers.spec.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { baseTest as test } from '../config/baseTest';
+import { expect } from '@playwright/test';
+
+for (let i = 0; i < 100; ++i) {
+  test('cycle browsers ' + i, async ({ playwright, browserName, server }) => {
+    const browser = await playwright[browserName].launch();
+    try {
+      const page = await browser.newPage();
+      await page.goto(server.PREFIX + '/stress/index.html');
+      await expect(page.locator('text=Learn React')).toBeVisible();
+    } finally {
+      await browser.close();
+    }
+  });
+}
