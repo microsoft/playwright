microsoft/playwright

commit 7a5b070e9507b622877a2af010373585f2184196

fix(type): focus switch between contenteditables in shadow dom (#13510)
Firefox has a bug: calling `node.focus()` does make the node focused,
but some internal "current contenteditable node" is not changed.
Blurring the previous one and focusing the new one helps.

packages/playwright-core/src/server/injected/injectedScript.ts
@@ -663,7 +663,14 @@ export class InjectedScript {
       return 'error:notconnected';
     if (node.nodeType !== Node.ELEMENT_NODE)
       throw this.createStacklessError('Node is not an element');
-    const wasFocused = (node.getRootNode() as (Document | ShadowRoot)).activeElement === node && node.ownerDocument && node.ownerDocument.hasFocus();
+
+    const activeElement = (node.getRootNode() as (Document | ShadowRoot)).activeElement;
+    const wasFocused = activeElement === node && node.ownerDocument && node.ownerDocument.hasFocus();
+    if (!wasFocused && activeElement && (activeElement as HTMLElement | SVGElement).blur) {
+      // Workaround the Firefox bug where focusing the element does not switch current
+      // contenteditable to the new element. However, blurring the previous one helps.
+      (activeElement as HTMLElement | SVGElement).blur();
+    }
     (node as HTMLElement | SVGElement).focus();
 
     if (resetSelectionIfNotFocused && !wasFocused && node.nodeName.toLowerCase() === 'input') {

tests/page/page-keyboard.spec.ts
@@ -476,6 +476,48 @@ it('should support simple copy-pasting', async ({ page, isMac, browserName }) =>
   expect(await page.evaluate(() => document.querySelector('div').textContent)).toBe('123123');
 });
 
+it('should type repeatedly in contenteditable in shadow dom', async ({ page }) => {
+  it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/12941' });
+
+  await page.setContent(`
+    <html>
+      <body>
+        <shadow-element></shadow-element>
+        <script>
+          customElements.define('shadow-element', class extends HTMLElement {
+            constructor() {
+              super();
+              this.attachShadow({ mode: 'open' });
+            }
+
+            connectedCallback() {
+              this.shadowRoot.innerHTML = \`
+                <style>
+                  .editor { padding: 1rem; margin: 1rem; border: 1px solid #ccc; }
+                </style>
+                <div class=editor contenteditable id=foo></div>
+                <hr>
+                <section>
+                  <div class=editor contenteditable id=bar></div>
+                </section>
+              \`;
+            }
+          });
+        </script>
+      </body>
+    </html>
+  `);
+
+  const editor = page.locator('shadow-element > .editor').first();
+  await editor.type('This is the first box.');
+
+  const sectionEditor = page.locator('section .editor');
+  await sectionEditor.type('This is the second box.');
+
+  expect(await editor.textContent()).toBe('This is the first box.');
+  expect(await sectionEditor.textContent()).toBe('This is the second box.');
+});
+
 async function captureLastKeydown(page) {
   const lastEvent = await page.evaluateHandle(() => {
     const lastEvent = {

commit 1c60547069b3785ab1b151a06b2111c2e19824a0

test: add repro for focus misfiring blur
Repro for #14254.
Regressed by 7a5b070e9507b622877a2af010373585f2184196.

The FF workaround introduced by
7a5b070e9507b622877a2af010373585f2184196, triggers `#focusable` to be
blurred when `#non-focusable` attempts to be focused.

This test currently fails on all three browsers:

```
  3) [firefox] › page/page-focus.spec.ts:121:4 › keeps focus on element when attempting to focus a non-focusable element

    Error: expect(received).toBe(expected) // Object.is equality

    Expected: "focusable"
    Received: ""

      144 |   expect.soft(await page.evaluate(() => document.activeElement?.id)).toBe('focusable');
      145 |   await page.locator('#non-focusable').focus();
    > 146 |   expect.soft(await page.evaluate(() => document.activeElement?.id)).toBe('focusable');
          |                                                                      ^
      147 |   expect.soft(await page.evaluate(() => window['eventLog'])).toEqual([
      148 |     'focus focusable',
      149 |   ]);

        at //tests/page/page-focus.spec.ts:146:70

    Error: expect(received).toEqual(expected) // deep equality

    - Expected  - 0
    + Received  + 1

      Array [
        "focus focusable",
    +   "blur focusable",
      ]

      145 |   await page.locator('#non-focusable').focus();
      146 |   expect.soft(await page.evaluate(() => document.activeElement?.id)).toBe('focusable');
    > 147 |   expect.soft(await page.evaluate(() => window['eventLog'])).toEqual([
          |                                                              ^
      148 |     'focus focusable',
      149 |   ]);
      150 | });

        at //tests/page/page-focus.spec.ts:147:62
```

tests/page/page-focus.spec.ts
@@ -117,3 +117,35 @@ it('clicking checkbox should activate it', async ({ page, browserName, headless,
   const nodeName = await page.evaluate(() => document.activeElement.nodeName);
   expect(nodeName).toBe('INPUT');
 });
+
+it('keeps focus on element when attempting to focus a non-focusable element', async ({ page }) => {
+  it.fail();
+  it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/14254' });
+
+  await page.setContent(`
+      <div id="focusable" tabindex="0">focusable</div>
+      <div id="non-focusable">not focusable</div>
+      <script>
+        window.eventLog = [];
+
+        const focusable = document.getElementById("focusable");
+        focusable.addEventListener('click', () => {
+          focusable.focus();
+        });
+
+        focusable.addEventListener('blur', () => window.eventLog.push('blur focusable'));
+        focusable.addEventListener('focus', () => window.eventLog.push('focus focusable'));
+
+        const nonFocusable = document.getElementById("non-focusable");
+        nonFocusable.addEventListener('blur', () => window.eventLog.push('blur non-focusable'));
+        nonFocusable.addEventListener('focus', () => window.eventLog.push('focus non-focusable'));
+      </script>
+    `);
+  await page.locator('#focusable').click();
+  expect.soft(await page.evaluate(() => document.activeElement?.id)).toBe('focusable');
+  await page.locator('#non-focusable').focus();
+  expect.soft(await page.evaluate(() => document.activeElement?.id)).toBe('focusable');
+  expect.soft(await page.evaluate(() => window['eventLog'])).toEqual([
+    'focus focusable',
+  ]);
+});