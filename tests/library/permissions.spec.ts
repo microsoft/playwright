b811b1d208e65d31501054a4af31f69fa08aa68d
Max Schmitt <max@schmitt.mx>
2022-05-27 21:54:47 +0200

    fix it

--- tests/library/permissions.spec.ts
+++ tests/library/permissions.spec.ts
@@ -100,9 +100,9 @@ it.describe('permissions', () => {
     expect(await getPermission(page, 'geolocation')).toBe('prompt');
   });
 
-  it('should trigger permission onchange', async ({ page, context, server, browserName, headless, channel }) => {
+  it('should trigger permission onchange', async ({ page, context, server, browserName, headless, browserMajorVersion }) => {
     it.fail(browserName === 'webkit');
-    it.fail(browserName === 'chromium' && !headless && !channel, 'Fixed on ToT, remove once Beta rolls');
+    it.fail(browserName === 'chromium' && !headless && browserMajorVersion <= 102, 'Fixed on ToT, remove once Beta rolls');
 
     await page.goto(server.EMPTY_PAGE);
     await page.evaluate(() => {
