fcce9136341ea96d08752507a20ae794dacec1d8
Andrey Lushnikov <lushnikov@chromium.org>
2022-06-01 03:02:00 -0700

    browser(firefox-beta): roll Firefox-Beta to 102.0b3
    This patch rolls Firefox-Beta to 102.0b3
    
    Pretty diff: https://github.com/aslushnikov/juggler/commit/bc1e4826ce0fb9891ee5cdfa8a1e718b016c1912

--- browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox-beta/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="beta"
-BASE_REVISION="4eec8e1d31fe3650e87754cb2d12bf4336cc901a"
+BASE_REVISION="ca83f93603b36eda53c9f4df03be38b6fb8082b9"
