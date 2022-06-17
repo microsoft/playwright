da361d5e6e8259068a6d12ba38fbb6e6fbdbca26
Max Schmitt <max@schmitt.mx>
2022-06-17 18:33:38 +0200

    review feedback

--- packages/playwright-test/src/reporters/html.ts
+++ packages/playwright-test/src/reporters/html.ts
@@ -218,12 +218,8 @@ export async function showHTMLReport(reportFolder: string | undefined, testId?: 
   console.log(colors.cyan(`  Serving HTML report at ${url}. Press Ctrl+C to quit.`));
   if (testId)
     url += `#?testId=${testId}`;
-  try {
-    await open(url, { wait: true });
-    await new Promise(() => {});
-  } catch (error) {
-    console.log(`Failed to open browser on ${url}`);
-  }
+  await open(url, { wait: true }).catch(() => console.log(`Failed to open browser on ${url}`));
+  await new Promise(() => {});
 }
 
 export function startHtmlReportServer(folder: string): HttpServer {
