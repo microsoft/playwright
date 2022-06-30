b0bb99f413113576ab31bf923727798208b48051
Max Schmitt <max@schmitt.mx>
2022-06-30 16:00:05 +0200

    test: unflake React.js and Vue tests (#15264)
    React takes time to render on overbooked machines. Let's use web-first assertions to test that they work as expected.
    
    Error: expect(received).toBe(expected) // Object.is equality
    
    Expected: 1
    Received: 0
        at /home/runner/work/playwright/playwright/tests/page/selectors-react.spec.ts:34:71
    
    <img width="887" alt="image" src="https://user-images.githubusercontent.com/17984549/176618419-7653e5d8-4853-4618-af3b-2698827879d2.png">

--- tests/page/selectors-vue.spec.ts
+++ tests/page/selectors-vue.spec.ts
@@ -29,90 +29,91 @@ for (const [name, url] of Object.entries(vues)) {
     });
 
     it('should work with single-root elements @smoke', async ({ page }) => {
-      expect(await page.$$eval(`_vue=book-list`, els => els.length)).toBe(1);
+      await expect(page.locator(`_vue=book-list`)).toHaveCount(1);
+      // count() was not working, see: https://github.com/microsoft/playwright/issues/12887
       expect(await page.locator(`_vue=book-list`).count()).toBe(1);
       await expect(page.locator(`_vue=book-list`)).toHaveCount(1);
-      expect(await page.$$eval(`_vue=book-item`, els => els.length)).toBe(3);
+      await expect(page.locator(`_vue=book-item`)).toHaveCount(3);
       expect(await page.locator(`_vue=book-item`).count()).toBe(3);
       await expect(page.locator(`_vue=book-item`)).toHaveCount(3);
-      expect(await page.$$eval(`_vue=book-list >> _vue=book-item`, els => els.length)).toBe(3);
+      await expect(page.locator(`_vue=book-list >> _vue=book-item`)).toHaveCount(3);
       expect(await page.locator(`_vue=book-list >> _vue=book-item`).count()).toBe(3);
-      expect(await page.$$eval(`_vue=book-item >> _vue=book-list`, els => els.length)).toBe(0);
+      await expect(page.locator(`_vue=book-item >> _vue=book-list`)).toHaveCount(0);
     });
 
     it('should work with multi-root elements (fragments)', async ({ page }) => {
       it.skip(name === 'vue2', 'vue2 does not support fragments');
-      expect(await page.$$eval(`_vue=Root`, els => els.length)).toBe(15);
-      expect(await page.$$eval(`_vue=app-header`, els => els.length)).toBe(2);
-      expect(await page.$$eval(`_vue=new-book`, els => els.length)).toBe(2);
+      await expect(page.locator(`_vue=Root`)).toHaveCount(15);
+      await expect(page.locator(`_vue=app-header`)).toHaveCount(2);
+      await expect(page.locator(`_vue=new-book`)).toHaveCount(2);
     });
 
     it('should not crash when there is no match', async ({ page }) => {
-      expect(await page.$$eval(`_vue=apps`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=book-li`, els => els.length)).toBe(0);
+      await expect(page.locator(`_vue=apps`)).toHaveCount(0);
+      await expect(page.locator(`_vue=book-li`)).toHaveCount(0);
     });
 
     it('should compose', async ({ page }) => {
-      expect(await page.$eval(`_vue=book-item >> text=Gatsby`, el => el.textContent.trim())).toBe('The Great Gatsby');
+      await expect(page.locator(`_vue=book-item >> text=Gatsby`)).toHaveText('The Great Gatsby');
     });
 
     it('should query by props combinations', async ({ page }) => {
-      expect(await page.$$eval(`_vue=book-item[name="The Great Gatsby"]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=book-item[name="the great gatsby" i]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=color-button[nested.index = 0]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=color-button[nested.nonexisting.index = 0]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=color-button[nested.index.nonexisting = 0]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=color-button[nested.index.nonexisting = 1]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=color-button[nested.value = 4.1]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=color-button[enabled = false]`, els => els.length)).toBe(4);
-      expect(await page.$$eval(`_vue=color-button[enabled = true] `, els => els.length)).toBe(5);
-      expect(await page.$$eval(`_vue=color-button[enabled = true][color = "red"]`, els => els.length)).toBe(2);
-      expect(await page.$$eval(`_vue=color-button[enabled = true][color = "red"i][nested.index =  6]`, els => els.length)).toBe(1);
+      await expect(page.locator(`_vue=book-item[name="The Great Gatsby"]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=book-item[name="the great gatsby" i]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=color-button[nested.index = 0]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=color-button[nested.nonexisting.index = 0]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=color-button[nested.index.nonexisting = 0]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=color-button[nested.index.nonexisting = 1]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=color-button[nested.value = 4.1]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=color-button[enabled = false]`)).toHaveCount(4);
+      await expect(page.locator(`_vue=color-button[enabled = true] `)).toHaveCount(5);
+      await expect(page.locator(`_vue=color-button[enabled = true][color = "red"]`)).toHaveCount(2);
+      await expect(page.locator(`_vue=color-button[enabled = true][color = "red"i][nested.index =  6]`)).toHaveCount(1);
     });
 
     it('should exact match by props', async ({ page }) => {
-      expect(await page.$eval(`_vue=book-item[name = "The Great Gatsby"]`, el => el.textContent)).toBe('The Great Gatsby');
-      expect(await page.$$eval(`_vue=book-item[name = "The Great Gatsby"]`, els => els.length)).toBe(1);
+      await expect(page.locator(`_vue=book-item[name = "The Great Gatsby"]`)).toHaveText('The Great Gatsby');
+      await expect(page.locator(`_vue=book-item[name = "The Great Gatsby"]`)).toHaveCount(1);
       // case sensetive by default
-      expect(await page.$$eval(`_vue=book-item[name = "the great gatsby"]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=book-item[name = "the great gatsby" s]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=book-item[name = "the great gatsby" S]`, els => els.length)).toBe(0);
+      await expect(page.locator(`_vue=book-item[name = "the great gatsby"]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=book-item[name = "the great gatsby" s]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=book-item[name = "the great gatsby" S]`)).toHaveCount(0);
       // case insensetive with flag
-      expect(await page.$$eval(`_vue=book-item[name = "the great gatsby" i]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=book-item[name = "the great gatsby" I]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=book-item[name = "  The Great Gatsby  "]`, els => els.length)).toBe(0);
+      await expect(page.locator(`_vue=book-item[name = "the great gatsby" i]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=book-item[name = "the great gatsby" I]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=book-item[name = "  The Great Gatsby  "]`)).toHaveCount(0);
     });
 
     it('should partially match by props', async ({ page }) => {
       // Check partial matching
-      expect(await page.$eval(`_vue=book-item[name *= "Gatsby"]`, el => el.textContent)).toBe('The Great Gatsby');
-      expect(await page.$$eval(`_vue=book-item[name *= "Gatsby"]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=[name *= "Gatsby"]`, els => els.length)).toBe(1);
+      await expect(page.locator(`_vue=book-item[name *= "Gatsby"]`)).toHaveText('The Great Gatsby');
+      await expect(page.locator(`_vue=book-item[name *= "Gatsby"]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=[name *= "Gatsby"]`)).toHaveCount(1);
 
-      expect(await page.$$eval(`_vue=book-item[name = "Gatsby"]`, els => els.length)).toBe(0);
+      await expect(page.locator(`_vue=book-item[name = "Gatsby"]`)).toHaveCount(0);
     });
 
     it('should support all string operators', async ({ page }) => {
-      expect(await page.$$eval(`_vue=color-button[color = "red"]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color |= "red"]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color $= "ed"]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color ^= "gr"]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color ~= "e"]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=book-item[name ~= "gatsby" i]`, els => els.length)).toBe(1);
-      expect(await page.$$eval(`_vue=book-item[name *= " gatsby" i]`, els => els.length)).toBe(1);
+      await expect(page.locator(`_vue=color-button[color = "red"]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color |= "red"]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color $= "ed"]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color ^= "gr"]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color ~= "e"]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=book-item[name ~= "gatsby" i]`)).toHaveCount(1);
+      await expect(page.locator(`_vue=book-item[name *= " gatsby" i]`)).toHaveCount(1);
     });
 
     it('should support regex', async ({ page }) => {
-      expect(await page.$$eval(`_vue=color-button[color = /red/]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color = /^red$/]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color = /RED/i]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color = /[pqr]ed/]`, els => els.length)).toBe(3);
-      expect(await page.$$eval(`_vue=color-button[color = /[pq]ed/]`, els => els.length)).toBe(0);
-      expect(await page.$$eval(`_vue=book-item[name = /gat.by/i]`, els => els.length)).toBe(1);
+      await expect(page.locator(`_vue=color-button[color = /red/]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color = /^red$/]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color = /RED/i]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color = /[pqr]ed/]`)).toHaveCount(3);
+      await expect(page.locator(`_vue=color-button[color = /[pq]ed/]`)).toHaveCount(0);
+      await expect(page.locator(`_vue=book-item[name = /gat.by/i]`)).toHaveCount(1);
     });
 
     it('should support truthy querying', async ({ page }) => {
-      expect(await page.$$eval(`_vue=color-button[enabled]`, els => els.length)).toBe(5);
+      await expect(page.locator(`_vue=color-button[enabled]`)).toHaveCount(5);
     });
 
     it('should support nested vue trees', async ({ page }) => {
