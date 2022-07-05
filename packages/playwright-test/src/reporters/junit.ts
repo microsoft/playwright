71c08a5dcf66ffe6b4e7be12192d8db4615a59b6
Sergio Freire <sergio.freire@gmail.com>
2022-07-05 18:35:14 +0100

    fix(junit reporter): embedding attachments on report didnt work for tests outside root folder (#15359)

--- packages/playwright-test/src/reporters/junit.ts
+++ packages/playwright-test/src/reporters/junit.ts
@@ -191,11 +191,10 @@ class JUnitReporter implements Reporter {
             if (!attachment.path)
               continue;
             try {
-              const attachmentPath = path.relative(this.config.rootDir, attachment.path);
-              if (fs.existsSync(attachmentPath))
-                contents = fs.readFileSync(attachmentPath, { encoding: 'base64' });
+              if (fs.existsSync(attachment.path))
+                contents = fs.readFileSync(attachment.path, { encoding: 'base64' });
               else
-                systemErr.push(`\nWarning: attachment ${attachmentPath} is missing`);
+                systemErr.push(`\nWarning: attachment ${attachment.path} is missing`);
             } catch (e) {
             }
           }
