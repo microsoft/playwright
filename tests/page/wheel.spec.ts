a128dda84eccefe343ba46d4ef5839263a17c8b5
Yury Semikhatsky <yurys@chromium.org>
2022-07-27 16:39:01 -0700

    feat(webkit): roll to r1691 (#16000)

--- tests/page/wheel.spec.ts
+++ tests/page/wheel.spec.ts
@@ -66,7 +66,6 @@ it('should dispatch wheel events @smoke', async ({ page, server }) => {
 
 it('should dispatch wheel event on svg element', async ({ page, browserName, headless, isLinux }) => {
   it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/15566' });
-  it.fixme(browserName === 'webkit' && headless && isLinux);
   await page.setContent(`
     <body>
       <svg class="scroll-box"></svg>
