e521137bbf8a3a5f8d08a642e7259011e3347d88
Yury Semikhatsky <yurys@chromium.org>
2022-08-15 11:35:18 -0700

    fix(driver): PLAYWRIGHT_NODEJS_PATH test in sh

--- utils/build/run-driver-posix.sh
+++ utils/build/run-driver-posix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_PATH="$(cd "$(dirname "$0")" ; pwd -P)"
-if [[ -z "$PLAYWRIGHT_NODEJS_PATH" ]]; then
+if [ -z "$PLAYWRIGHT_NODEJS_PATH" ]; then
   PLAYWRIGHT_NODEJS_PATH="$SCRIPT_PATH/node"
 fi
 "$PLAYWRIGHT_NODEJS_PATH" "$SCRIPT_PATH/package/lib/cli/cli.js" "$@"
