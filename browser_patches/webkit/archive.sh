aea38e10ed82a73a191c550a45c1a612c9364e91
Andrey Lushnikov <lushnikov@chromium.org>
2022-08-18 18:53:16 +0300

    fix

--- browser_patches/webkit/archive.sh
+++ browser_patches/webkit/archive.sh
@@ -60,12 +60,6 @@ createZipForLinux() {
   # copy protocol
   node "$SCRIPTS_DIR"/concat_protocol.js > "$tmpdir"/protocol.json
 
-  # Bundle libstdc++ version that comes from gcc-9. gcc-9 is not default on Ubuntu 18.04
-  if is_linux ubuntu 18.04; then
-    cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 "${tmpdir}/minibrowser-wpe/lib/libstdc++.so.6"
-    cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 "${tmpdir}/minibrowser-gtk/lib/libstdc++.so.6"
-  fi
-
   # Generate and unpack MiniBrowser bundles for each port
   for port in gtk wpe; do
     WEBKIT_OUTPUTDIR=$(pwd)/WebKitBuild/${port^^} \
@@ -76,6 +70,12 @@ createZipForLinux() {
     unzip "${tmpdir}"/MiniBrowser_${port}_release.zip -d "${tmpdir}"/minibrowser-${port}
     rm -f "${tmpdir}"/MiniBrowser_${port}_release.zip
   done
+
+  # Bundle libstdc++ version that comes from gcc-9. gcc-9 is not default on Ubuntu 18.04
+  if is_linux ubuntu 18.04; then
+    cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 "${tmpdir}/minibrowser-wpe/lib/libstdc++.so.6"
+    cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 "${tmpdir}/minibrowser-gtk/lib/libstdc++.so.6"
+  fi
 
   cd "$tmpdir"
 
