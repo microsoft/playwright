56265a576a4874376a4fa4716a9455f311dd5be6
Max Schmitt <max@schmitt.mx>
2022-08-19 10:11:48 +0200

    Update page-history.spec.ts

--- tests/page/page-history.spec.ts
+++ tests/page/page-history.spec.ts
@@ -156,7 +156,6 @@ it('page.reload should work with same origin redirect', async ({ page, server })
 
 it('page.reload should work with cross-origin redirect', async ({ page, server, browserName }) => {
   it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/16147' });
-  it.fixme(browserName === 'webkit');
   await page.goto(server.EMPTY_PAGE);
   server.setRedirect('/empty.html', server.CROSS_PROCESS_PREFIX + '/title.html');
   await page.reload();
