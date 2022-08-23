6d1a7f33153b5ce7be1982098b1364829cbac4ca
sand4rt <info@mesander.com>
2022-08-23 20:34:20 +0200

    test(ct): svelte (#16761)

---
+++ tests/components/ct-svelte/playwright/index.ts
@@ -0,0 +1,10 @@
+//@ts-check
+import { beforeMount, afterMount } from '@playwright/experimental-ct-svelte/hooks';
+
+beforeMount(async ({ hooksConfig }) => {
+  console.log(`Before mount: ${JSON.stringify(hooksConfig)}`);
+});
+  
+afterMount(async ({}) => {
+  console.log(`After mount`);
+});
