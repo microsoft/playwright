b3fe96d03d4e98e94eb32873988edc5b0446dd21
sand4rt <info@mesander.com>
2022-08-24 23:19:44 +0200

    feat(ct): solid set props

--- tests/components/ct-solid/src/tests.spec.tsx
+++ tests/components/ct-solid/src/tests.spec.tsx
@@ -3,7 +3,7 @@ import Button from './components/Button';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-test('should work', async ({ mount }) => {
-  const component = await mount(<Button />);
+test('props should work', async ({ mount }) => {
+  const component = await mount(<Button title="Submit" />);
   await expect(component).toContainText('Submit');
 });
