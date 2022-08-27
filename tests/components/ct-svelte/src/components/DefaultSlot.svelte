cf19c7fd0776a9c9c5cf3f15b53fc432d5a4ad35
sand4rt <info@mesander.com>
2022-08-27 16:09:21 +0200

    removed template tag

--- tests/components/ct-svelte/src/components/DefaultSlot.svelte
+++ tests/components/ct-svelte/src/components/DefaultSlot.svelte
@@ -1,11 +1,9 @@
-<template>
-  <div>
-    <h1>Welcome!</h1>
-    <main>
-      <slot />
-    </main>
-    <footer>
-      Thanks for visiting.
-    </footer>
-  </div>
-</template>
+<div>
+  <h1>Welcome!</h1>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    Thanks for visiting.
+  </footer>
+</div>
