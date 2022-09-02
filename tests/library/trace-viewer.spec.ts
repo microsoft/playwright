3aff2185b5d8da56627da72684ddb2a0b00b4d24
Yury Semikhatsky <yurys@chromium.org>
2022-09-02 16:57:06 -0700

    Add trace viewer test

--- tests/library/trace-viewer.spec.ts
+++ tests/library/trace-viewer.spec.ts
@@ -686,3 +686,26 @@ test('should include requestUrl in route.abort', async ({ page, runAndTrace, ser
   await expect(callLine.locator('text=requestUrl')).toContainText('http://test.com');
 });
 
+test.only('should serve overridden request', async ({ page, runAndTrace, server }) => {
+  server.setRoute('/custom.css', (req, res) => {
+    res.writeHead(200, {
+      'Content-Type': 'text/css',
+    });
+    res.end(`body { background: red }`);
+    console.log('serve custom ')
+  });
+  await page.route('**/one-style.css', route => {
+    console.log('route.continue!');
+    route.continue({
+      url: server.PREFIX + '/custom.css'
+    });
+  });
+  const traceViewer = await runAndTrace(async () => {
+    await page.goto(server.PREFIX + '/one-style.html');
+  });
+  // Render snapshot, check expectations.
+  const snapshotFrame = await traceViewer.snapshotFrame('page.goto');
+  const color = await snapshotFrame.locator('body').evaluate(body => getComputedStyle(body).backgroundColor);
+  expect(color).toBe('rgb(255, 0, 0)');
+});
+
