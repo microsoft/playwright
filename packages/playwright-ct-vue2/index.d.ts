c4a5685da67e4821b74129f3afbec6e92f779756
sand4rt <info@mesander.com>
2022-09-04 14:37:17 +0200

    fix(ct): vue2 slot type

--- packages/playwright-ct-vue2/index.d.ts
+++ packages/playwright-ct-vue2/index.d.ts
@@ -34,11 +34,13 @@ export type PlaywrightTestConfig = Omit<BasePlaywrightTestConfig, 'use'> & {
   }
 };
 
+type Slot = string | string[];
+
 export interface MountOptions<Props = Record<string, unknown>> {
-  props?: Props,
-  slots?: Record<string, unknown>,
-  on?: Record<string, Function>,
-  hooksConfig?: any,
+  props?: Props;
+  slots?: Record<string, Slot> & { default?: Slot };
+  on?: Record<string, Function>;
+  hooksConfig?: any;
 }
 
 interface MountResult<Props = Record<string, unknown>> extends Locator {
