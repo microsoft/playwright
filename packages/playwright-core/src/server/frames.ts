35d1aff7ca3b9ec2d26fa987443a48e69d067531
Pavel Feldman <pavel.feldman@gmail.com>
2022-09-06 12:48:40 -0700

    cherry-pick(#17137): chore: silenсe best effort cleanup errors

--- packages/playwright-core/src/server/frames.ts
+++ packages/playwright-core/src/server/frames.ts
@@ -1714,8 +1714,8 @@ export class Frame extends SdkObject {
       localStorage.clear();
 
       // Clean Service Workers
-      const registrations = await navigator.serviceWorker.getRegistrations();
-      await Promise.all(registrations.map(r => r.unregister()));
+      const registrations = navigator.serviceWorker ? await navigator.serviceWorker.getRegistrations() : [];
+      await Promise.all(registrations.map(r => r.unregister())).catch(() => {});
 
       // Clean IndexedDB
       for (const db of await indexedDB.databases?.() || []) {
@@ -1723,7 +1723,7 @@ export class Frame extends SdkObject {
         if (db.name)
           indexedDB.deleteDatabase(db.name!);
       }
-    });
+    }).catch(() => {});
   }
 }
 
