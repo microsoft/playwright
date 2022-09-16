e92bdce383a093120cc1ff76bf823f578a1a0982
Pavel Feldman <pavel.feldman@gmail.com>
2022-09-15 20:37:21 -0700

    chore: remove connect options after testing (#17380)

--- tests/library/browsertype-connect.spec.ts
+++ tests/library/browsertype-connect.spec.ts
@@ -642,4 +642,6 @@ test('should connect when launching', async ({ browserType, startRemoteServer, h
     new Promise(f => browser.on('disconnected', f)),
     remoteServer.close(),
   ]);
+
+  (browserType as any)._defaultConnectOptions = undefined;
 });
