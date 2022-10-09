1fb7d8697eca9a61dcc1137c9decd69f8ca8b589
sand4rt <info@mesander.com>
2022-10-09 23:14:52 +0200

    test(ct): svelte named slots

--- tests/components/ct-svelte/src/tests.spec.ts
+++ tests/components/ct-svelte/src/tests.spec.ts
@@ -18,6 +18,7 @@ import { test, expect } from '@playwright/experimental-ct-svelte';
 import Button from './components/Button.svelte';
 import Component from './components/Component.svelte';
 import DefaultSlot from './components/DefaultSlot.svelte';
+import NamedSlots from './components/NamedSlots.svelte'
 import MultiRoot from './components/MultiRoot.svelte';
 import Empty from './components/Empty.svelte';
 
@@ -53,6 +54,19 @@ test('render a default slot', async ({ mount }) => {
     }
   })
   await expect(component).toContainText('Main Content')
+})
+
+test('render a component with a named slot', async ({ mount }) => {
+  const component = await mount(NamedSlots, {
+    slots: {
+      header: 'Header',
+      main: 'Main Content',
+      footer: 'Footer'
+    }
+  })
+  await expect(component).toContainText('Header')
+  await expect(component).toContainText('Main Content')
+  await expect(component).toContainText('Footer')
 })
 
 test('render a component without options', async ({ mount }) => {
