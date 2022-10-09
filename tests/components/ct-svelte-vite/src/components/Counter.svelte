5a1e172a0628467a229cddbc5279daec1676947a
sand4rt <info@mesander.com>
2022-10-09 21:40:33 +0200

    feat(ct): svelte rerender

---
+++ tests/components/ct-svelte-vite/src/components/Counter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { update, remountCount } from '../store'
+import { createEventDispatcher } from "svelte";
+export let count;
+const dispatch = createEventDispatcher();
+update();
+</script>
+
+<div on:click={() => dispatch('submit', 'hello')}>
+  <div id="props">{count}</div>
+  <div id="remount-count">{remountCount}</div>
+  <slot name="main" />
+  <slot />
+</div>
