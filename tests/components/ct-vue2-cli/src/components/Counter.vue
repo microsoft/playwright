da9276bd039e5cff7147a3757095e6969478c748
sand4rt <info@mesander.com>
2022-10-10 21:49:52 +0200

    feat(ct): vue2 rerender complete (#17929)
    Partial fix for https://github.com/microsoft/playwright/issues/15919

--- tests/components/ct-vue2-cli/src/components/Counter.vue
+++ tests/components/ct-vue2-cli/src/components/Counter.vue
@@ -1,8 +1,10 @@
 <template>
-   <div>
-     <span id="remount-count">{{ remountCount }}</span>
-     <span id="rerender-count">{{ count }}</span>
-   </div>
+  <div @click="$emit('submit', 'hello')">
+    <div id="props">{{ count }}</div>
+    <div id="remount-count">{{ remountCount }}</div>
+    <slot name="main" />
+    <slot />
+  </div>
  </template>
 
 <script>
