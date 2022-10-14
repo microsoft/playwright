sand4rt/playwright-ct-angular

fix(ct): vite base url

tests/components/ct-vue-vite/src/router/index.ts
@@ -1,0 +1,11 @@
+import Dashboard from '../pages/Dashboard.vue';
+import Login from '../pages/Login.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+export const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: Login },
+    { path: '/dashboard', component: Dashboard },
+  ],
+})

microsoft/playwright

tests/components/ct-vue-vite/src/router/index.ts
@@ -1,0 +1,11 @@
+import DashboardPage from '../pages/DashboardPage.vue';
+import LoginPage from '../pages/LoginPage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+export const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: LoginPage },
+    { path: '/dashboard', component: DashboardPage },
+  ],
+})