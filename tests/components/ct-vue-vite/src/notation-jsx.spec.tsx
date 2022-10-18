e76adafc5bfae9b08febc6596fa0fde6c74aab80
sand4rt <info@mesander.com>
2022-10-18 22:06:36 +0200

    feat(ct): vue3 html as slot (#18007)

--- tests/components/ct-vue-vite/src/notation-jsx.spec.tsx
+++ tests/components/ct-vue-vite/src/notation-jsx.spec.tsx
@@ -71,18 +71,18 @@ test('emit an submit event when the button is clicked', async ({ mount }) => {
 
 test('render a default slot', async ({ mount }) => {
   const component = await mount(<DefaultSlot>
-    Main Content
+    <strong>Main Content</strong>
   </DefaultSlot>)
-  await expect(component).toContainText('Main Content')
+  await expect(component.getByRole('strong')).toContainText('Main Content')
 })
 
 test('render a component with multiple slots', async ({ mount }) => {
   const component = await mount(<DefaultSlot>
-    <div id="one">One</div>
-    <div id="two">Two</div>
+    <div data-testid="one">One</div>
+    <div data-testid="two">Two</div>
   </DefaultSlot>)
-  await expect(component.locator('#one')).toContainText('One')
-  await expect(component.locator('#two')).toContainText('Two')
+  await expect(component.getByTestId('one')).toContainText('One')
+  await expect(component.getByTestId('two')).toContainText('Two')
 })
 
 test('render a component with a named slot', async ({ mount }) => {
