6f992b5b6feb37b424cf9084ae5de35871efdc0e
sand4rt <info@mesander.com>
2022-11-06 18:02:20 +0100

    test(ct): react vite router

--- tests/components/ct-react-vite/src/App.tsx
+++ tests/components/ct-react-vite/src/App.tsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './assets/logo.svg'
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import logo from './assets/logo.svg';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+export default function App() {
+  return <>
+    <header>
+      <img src={logo} alt="logo" width={125} height={125} />
+      <Link to="/">Login</Link>
+      <Link to="/dashboard">Dashboard</Link>
+    </header>
+    <Routes>
+      <Route path="/">
+        <Route index element={<LoginPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+      </Route>
+    </Routes>
+  </>
 }
-
-export default App
