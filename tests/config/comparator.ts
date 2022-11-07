2435851a7640a8639fc9243bd7aab8de9e6355da
Dmitry Gozman <dgozman@gmail.com>
2022-11-07 12:52:48 -0800

    test: use strict png comparator in tests

---
+++ tests/config/comparator.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { getComparator } from 'playwright-core/lib/utils/comparators';
+
+const pngComparator = getComparator('image/png');
+type ComparatorResult = { diff?: Buffer; errorMessage: string; } | null;
+type ImageComparatorOptions = { threshold?: number, maxDiffPixels?: number, maxDiffPixelRatio?: number };
+
+export function comparePNGs(actual: Buffer, expected: Buffer, options: ImageComparatorOptions = {}): ComparatorResult {
+  // Strict threshold by default in our tests.
+  return pngComparator(actual, expected, { threshold: 0, ...options });
+}
