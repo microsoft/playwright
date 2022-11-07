c33c025512e997729d1df96bf8dda3115f4b1477
Dmitry Gozman <dgozman@gmail.com>
2022-11-07 14:23:24 -0800

    feat(locators): asLocator supports internal:has=
    The following snippet:
    ```js
    rowLocator
        .filter({ hasText: 'John' })
        .filter({ has: page.getByRole('button', { name: 'Say hello' }) })
    
    ```
    
    is shown in the logs:
    
    ```log
    pw:api waiting for getByRole('listitem').filter({ hasText: 'John' }).filter({ has: getByRole('button', { name: 'Say hello' }) })
    ```

--- packages/playwright-core/src/server/isomorphic/locatorParser.ts
+++ packages/playwright-core/src/server/isomorphic/locatorParser.ts
@@ -19,11 +19,12 @@ import { asLocator } from './locatorGenerators';
 import type { Language } from './locatorGenerators';
 import { parseSelector } from './selectorParser';
 
+type TemplateParams = { quote: string, text: string }[];
 function parseLocator(locator: string): string {
   locator = locator
       .replace(/AriaRole\s*\.\s*([\w]+)/g, (_, group) => group.toLowerCase())
       .replace(/(get_by_role|getByRole)\s*\(\s*(?:["'`])([^'"`]+)['"`]/g, (_, group1, group2) => `${group1}(${group2.toLowerCase()}`);
-  const params: { quote: string, text: string }[] = [];
+  const params: TemplateParams = [];
   let template = '';
   for (let i = 0; i < locator.length; ++i) {
     const quote = locator[i];
@@ -86,7 +87,53 @@ function parseLocator(locator: string): string {
       .replace(/regex=/g, '=')
       .replace(/,,/g, ',');
 
-  // Transform.
+  return transform(template, params);
+}
+
+function countParams(template: string) {
+  return [...template.matchAll(/\$\d+/g)].length;
+}
+
+function shiftParams(template: string, sub: number) {
+  return template.replace(/\$(\d+)/g, (_, ordinal) => `$${ordinal - sub}`);
+}
+
+function transform(template: string, params: TemplateParams): string {
+  // Recursively handle filter(has=).
+  while (true) {
+    const hasMatch = template.match(/filter\(,?has=/);
+    if (!hasMatch)
+      break;
+
+    // Extract inner locator based on balanced parens.
+    const start = hasMatch.index! + hasMatch[0].length;
+    let balance = 0;
+    let end = start;
+    for (; end < template.length; end++) {
+      if (template[end] === '(')
+        balance++;
+      else if (template[end] === ')')
+        balance--;
+      if (balance < 0)
+        break;
+    }
+
+    const paramsCountBeforeHas = countParams(template.substring(0, start));
+    const hasTemplate = shiftParams(template.substring(start, end), paramsCountBeforeHas);
+    const paramsCountInHas = countParams(hasTemplate);
+    const hasParams = params.slice(paramsCountBeforeHas, paramsCountBeforeHas + paramsCountInHas);
+    const hasSelector = JSON.stringify(transform(hasTemplate, hasParams));
+
+    // Replace filter(has=...) with filter(has2=$5). Use has2 to avoid matching the same filter again.
+    template = template.substring(0, start - 1) + `2=$${paramsCountBeforeHas + 1}` + shiftParams(template.substring(end), paramsCountInHas - 1);
+
+    // Replace inner params with $5 value.
+    const paramsBeforeHas = params.slice(0, paramsCountBeforeHas);
+    const paramsAfterHas = params.slice(paramsCountBeforeHas + paramsCountInHas);
+    params = paramsBeforeHas.concat([{ quote: '"', text: hasSelector }]).concat(paramsAfterHas);
+  }
+
+  // Transform to selector engines.
   template = template
       .replace(/locator\(([^)]+)\)/g, '$1')
       .replace(/getbyrole\(([^)]+)\)/g, 'internal:role=$1')
@@ -97,11 +144,13 @@ function parseLocator(locator: string): string {
       .replace(/first(\(\))?/g, 'nth=0')
       .replace(/last(\(\))?/g, 'nth=-1')
       .replace(/nth\(([^)]+)\)/g, 'nth=$1')
-      .replace(/filter\(.*hastext=([^)]+)\)/g, 'internal:has-text=$1')
+      .replace(/filter\(,?hastext=([^)]+)\)/g, 'internal:has-text=$1')
+      .replace(/filter\(,?has2=([^)]+)\)/g, 'internal:has=$1')
       .replace(/,exact=false/g, '')
       .replace(/,exact=true/g, 's')
       .replace(/\,/g, '][');
 
+  // Substitute params.
   return template.split('.').map(t => {
     if (!t.startsWith('internal:'))
       return t.replace(/\$(\d+)/g, (_, ordinal) => { const param = params[+ordinal - 1]; return param.text; });
@@ -115,6 +164,8 @@ function parseLocator(locator: string): string {
         })
         .replace(/\$(\d+)(i|s)?/g, (_, ordinal, suffix) => {
           const param = params[+ordinal - 1];
+          if (t.startsWith('internal:has='))
+            return param.text;
           if (t.startsWith('internal:attr') || t.startsWith('internal:role'))
             return escapeForAttributeSelector(param.text, suffix === 's');
           return escapeForTextSelector(param.text, suffix === 's');
