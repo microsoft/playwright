dbf3fc50896494d13153e45d08b75deae43afd7c
Pavel Feldman <pavel.feldman@gmail.com>
2022-11-14 22:01:54 -0800

    fix(electron): stall node while browser is starting (#18804)
    Fixes https://github.com/microsoft/playwright/issues/17765

--- tests/electron/electron-app.js
+++ tests/electron/electron-app.js
@@ -1,9 +1,6 @@
 const { app, protocol } = require('electron');
 const path = require('path');
 
-app.commandLine.appendSwitch('disable-features', 'AutoExpandDetailsElement');
-app.commandLine.appendSwitch('allow-pre-commit-input')
-
 app.on('window-all-closed', e => e.preventDefault());
 
 app.whenReady().then(() => {
