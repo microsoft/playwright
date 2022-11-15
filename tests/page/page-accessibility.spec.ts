b4167d68a6b4c9b17ddd77bf7665ba03226ae940
Pavel <pavel.feldman@gmail.com>
2022-11-15 10:39:25 -0800

    chore: skip a couple of a11y tests on Edge temporarily

--- tests/page/page-accessibility.spec.ts
+++ tests/page/page-accessibility.spec.ts
@@ -141,8 +141,9 @@ it('should not report text nodes inside controls', async function({ page, browse
   expect(await page.accessibility.snapshot()).toEqual(golden);
 });
 
-it('rich text editable fields should have children', async function({ page, browserName, browserVersion }) {
+it('rich text editable fields should have children', async function({ page, browserName, browserVersion, channel }) {
   it.skip(browserName === 'webkit', 'WebKit rich text accessibility is iffy');
+  it.skip(channel && channel.startsWith('msedge'), 'Edge is missing a Chromium fix');
 
   await page.setContent(`
   <div contenteditable="true">
@@ -174,8 +175,9 @@ it('rich text editable fields should have children', async function({ page, brow
   expect(snapshot.children[0]).toEqual(golden);
 });
 
-it('rich text editable fields with role should have children', async function({ page, browserName, browserMajorVersion, browserVersion }) {
+it('rich text editable fields with role should have children', async function({ page, browserName, browserMajorVersion, browserVersion, channel }) {
   it.skip(browserName === 'webkit', 'WebKit rich text accessibility is iffy');
+  it.skip(channel && channel.startsWith('msedge'), 'Edge is missing a Chromium fix');
 
   await page.setContent(`
   <div contenteditable="true" role='textbox'>
