b1b21bdac5c39e11b3293a28916fb4f62e70832d
Dmitry Gozman <dgozman@gmail.com>
2022-11-29 16:02:11 -0800

    fix(shards): zero-sized shard should not include all tests (#19148)
    Fixes #19073.

--- tests/playwright-test/shard.spec.ts
+++ tests/playwright-test/shard.spec.ts
@@ -63,6 +63,15 @@ test('should respect shard=2/2', async ({ runInlineTest }) => {
   expect(result.output).toContain('test5-done');
 });
 
+test('should not produce skipped tests for zero-sized shards', async ({ runInlineTest }) => {
+  const result = await runInlineTest(tests, { shard: '10/10' });
+  expect(result.exitCode).toBe(0);
+  expect(result.passed).toBe(0);
+  expect(result.failed).toBe(0);
+  expect(result.skipped).toBe(0);
+  expect(result.output).not.toContain('-done');
+});
+
 test('should respect shard=1/2 in config', async ({ runInlineTest }) => {
   const result = await runInlineTest({
     ...tests,
