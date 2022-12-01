1d3feba578fa217b7feebb2774b21db3200672d2
Max Schmitt <max@schmitt.mx>
2022-12-01 12:39:40 -0800

    chore: do not set bypassCSP when inspector is used (#19190)
    Fixes https://github.com/microsoft/playwright/issues/19186

--- packages/playwright-core/src/server/browserContext.ts
+++ packages/playwright-core/src/server/browserContext.ts
@@ -612,8 +612,6 @@ export function validateBrowserContextOptions(options: channels.BrowserNewContex
       throw new Error(`Browser needs to be launched with the global proxy. If all contexts override the proxy, global proxy will be never used and can be any string, for example "launch({ proxy: { server: 'http://per-context' } })"`);
     options.proxy = normalizeProxySettings(options.proxy);
   }
-  if (debugMode() === 'inspector')
-    options.bypassCSP = true;
   verifyGeolocation(options.geolocation);
 }
 
