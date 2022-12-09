526bc3f2528468c1ad84286ec3597b67950aa67b
Andrey Lushnikov <aslushnikov@gmail.com>
2022-12-08 16:08:41 -0800

    fix(ssim-cie94): further tune SSIM-CIE94 to fight wk artifacts (#19370)
    This patch adds a grid border around the image so that the SSIM
    resolution doesn't drop for the border pixels.
    
    We also add a test with WebKit rendering artifacts to make sure
    new approach helps to fight this.

--- packages/playwright-core/src/image_tools/imageChannel.ts
+++ packages/playwright-core/src/image_tools/imageChannel.ts
@@ -16,29 +16,49 @@
 
 import { blendWithWhite } from './colorUtils';
 
+export type PaddingOptions = {
+  paddingSize?: number,
+  paddingColorOdd?: number[],
+  paddingColorEven?: number[],
+};
+
 export class ImageChannel {
   data: Uint8Array;
   width: number;
   height: number;
 
-  static intoRGB(width: number, height: number, data: Buffer): ImageChannel[] {
-    const r = new Uint8Array(width * height);
-    const g = new Uint8Array(width * height);
-    const b = new Uint8Array(width * height);
-    for (let y = 0; y < height; ++y) {
-      for (let x = 0; x < width; ++x) {
-        const index = y * width + x;
-        const offset = index * 4;
-        const alpha = data[offset + 3] === 255 ? 1 : data[offset + 3] / 255;
-        r[index] = blendWithWhite(data[offset], alpha);
-        g[index] = blendWithWhite(data[offset + 1], alpha);
-        b[index] = blendWithWhite(data[offset + 2], alpha);
+  static intoRGB(width: number, height: number, data: Buffer, options: PaddingOptions = {}): ImageChannel[] {
+    const {
+      paddingSize = 0,
+      paddingColorOdd = [255, 0, 255],
+      paddingColorEven = [0, 255, 0],
+    } = options;
+    const newWidth = width + 2 * paddingSize;
+    const newHeight = height + 2 * paddingSize;
+    const r = new Uint8Array(newWidth * newHeight);
+    const g = new Uint8Array(newWidth * newHeight);
+    const b = new Uint8Array(newWidth * newHeight);
+    for (let y = 0; y < newHeight; ++y) {
+      for (let x = 0; x < newWidth; ++x) {
+        const index = y * newWidth + x;
+        if (y >= paddingSize && y < newHeight - paddingSize && x >= paddingSize && x < newWidth - paddingSize) {
+          const offset = ((y - paddingSize) * width + (x - paddingSize)) * 4;
+          const alpha = data[offset + 3] === 255 ? 1 : data[offset + 3] / 255;
+          r[index] = blendWithWhite(data[offset], alpha);
+          g[index] = blendWithWhite(data[offset + 1], alpha);
+          b[index] = blendWithWhite(data[offset + 2], alpha);
+        } else {
+          const color = (y + x) % 2 === 0 ? paddingColorEven : paddingColorOdd;
+          r[index] = color[0];
+          g[index] = color[1];
+          b[index] = color[2];
+        }
       }
     }
     return [
-      new ImageChannel(width, height, r),
-      new ImageChannel(width, height, g),
-      new ImageChannel(width, height, b),
+      new ImageChannel(newWidth, newHeight, r),
+      new ImageChannel(newWidth, newHeight, g),
+      new ImageChannel(newWidth, newHeight, b),
     ];
   }
 
