2c8f448fb0e755ec4e34d42e9274847a546b353c
Max Schmitt <max@schmitt.mx>
2022-12-20 15:20:22 +0100

    chore: format discouraged/deprecated

--- utils/doclint/documentation.js
+++ utils/doclint/documentation.js
@@ -170,15 +170,18 @@ class Documentation {
     this._patchLinksInText = (classOrMember, text) => patchLinksInText(classOrMember, text, classesMap, membersMap, linkRenderer);
 
     for (const clazz of this.classesArray)
-      clazz.visit(item => item.spec && this.renderLinksInNodes(item.spec, item));
+      clazz.visit(item => this.renderLinksInNodes(item));
   }
 
   /**
-   * @param {MarkdownNode[]} nodes
    * @param {Class|Member=} classOrMember
    */
-  renderLinksInNodes(nodes, classOrMember) {
-    md.visitAll(nodes, node => {
+  renderLinksInNodes(classOrMember) {
+    if (!classOrMember?.spec)
+      return;
+    classOrMember.discouraged = this.renderLinksInText(classOrMember.discouraged || '', classOrMember);
+    classOrMember.deprecated = this.renderLinksInText(classOrMember.deprecated || '', classOrMember);
+    md.visitAll(classOrMember.spec, node => {
       if (!node.text)
         return;
       node.text = this.renderLinksInText(node.text, classOrMember);
