b695478840390a3974196c7578775d05fbc87a15
Hylke Postma <20515386+hylkepostma@users.noreply.github.com>
2023-01-02 14:36:07 +0100

    fix(driver): AttributeError error when CWD contains space (#19801)

--- utils/build/run-driver-win.cmd
+++ utils/build/run-driver-win.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 if not defined PLAYWRIGHT_NODEJS_PATH (
-  set PLAYWRIGHT_NODEJS_PATH=%~dp0\node.exe
+  set PLAYWRIGHT_NODEJS_PATH="%~dp0\node.exe"
 )
-"""%PLAYWRIGHT_NODEJS_PATH%""" "%~dp0\package\lib\cli\cli.js" %*
+""%PLAYWRIGHT_NODEJS_PATH%"" "%~dp0\package\lib\cli\cli.js" %*
