53e1526d13d5cdf505b2bd5207479f3258dfdded
sand4rt <info@mesander.com>
2023-01-11 19:26:12 +0100

    chore(ct): svelte vite slice test by feature

--- tests/components/ct-svelte-vite/playwright.config.ts
+++ tests/components/ct-svelte-vite/playwright.config.ts
@@ -16,14 +16,22 @@
 
 import type { PlaywrightTestConfig } from '@playwright/experimental-ct-svelte';
 import { devices } from '@playwright/test';
+import { resolve } from 'path';
 
 const config: PlaywrightTestConfig = {
-  testDir: 'src',
+  testDir: 'tests',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
+    ctViteConfig: {
+      resolve: {
+        alias: {
+          '@': resolve('./src'),
+        }
+      }
+    }
   },
   projects: [
     {
---
+++ tests/components/ct-svelte-vite/tests/unmount.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/experimental-ct-svelte';
+import Button from '@/components/Button.svelte';
+import MultiRoot from '@/components/MultiRoot.svelte';
+
+test('unmount', async ({ page, mount }) => {
+  const component = await mount(Button, {
+    props: {
+      title: 'Submit',
+    },
+  });
+  await expect(page.locator('#root')).toContainText('Submit');
+  await component.unmount();
+  await expect(page.locator('#root')).not.toContainText('Submit');
+});
+
+test('unmount a multi root component', async ({ mount, page }) => {
+  const component = await mount(MultiRoot);
+  await expect(page.locator('#root')).toContainText('root 1');
+  await expect(page.locator('#root')).toContainText('root 2');
+  await component.unmount();
+  await expect(page.locator('#root')).not.toContainText('root 1');
+  await expect(page.locator('#root')).not.toContainText('root 2');
+});
