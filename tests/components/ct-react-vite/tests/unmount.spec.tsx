4a7e5fc12262f2d70c2d15bbc5e74ea0e61b7ff7
sand4rt <info@mesander.com>
2023-01-13 11:02:08 +0100

    test(ct): react vite slice by feature

---
+++ tests/components/ct-react-vite/tests/unmount.spec.tsx
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import Button from '@/components/Button';
+import MultiRoot from '@/components/MultiRoot';
+
+test('unmount', async ({ page, mount }) => {
+  const component = await mount(<Button title="Submit" />);
+  await expect(page.locator('#root')).toContainText('Submit');
+  await component.unmount();
+  await expect(page.locator('#root')).not.toContainText('Submit');
+});
+
+test('unmount a multi root component', async ({ mount, page }) => {
+  const component = await mount(<MultiRoot />);
+  await expect(page.locator('#root')).toContainText('root 1');
+  await expect(page.locator('#root')).toContainText('root 2');
+  await component.unmount();
+  await expect(page.locator('#root')).not.toContainText('root 1');
+  await expect(page.locator('#root')).not.toContainText('root 2');
+});
