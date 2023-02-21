f499d0a6a33f83e60019c3930858ffc9352c0008
Andrey Lushnikov <lushnikov@chromium.org>
2023-02-21 15:46:58 -0800

    test: add tests for input with open context menu
    These might be failing on certain platforms and browsers.
    
    References #20823

--- tests/page/page-mouse.spec.ts
+++ tests/page/page-mouse.spec.ts
@@ -259,3 +259,28 @@ it('should not crash on mouse drag with any button', async ({ page }) => {
     await page.mouse.move(100, 100);
   }
 });
+
+it('should dispatch mouse move after context menu was opened', async ({ page, browserName }) => {
+  it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/20823' });
+  it.fixme(browserName === 'firefox');
+  await page.evaluate(() => {
+    window['contextMenuPromise'] = new Promise(x => {
+      window.addEventListener('contextmenu', x, false);
+    });
+  });
+  const CX = 100, CY = 100;
+  await page.mouse.move(CX, CY);
+  await page.mouse.down({ button: 'right' });
+  await page.evaluate(() => window['contextMenuPromise']);
+  const N = 20;
+  for (const radius of [10, 30, 60, 90]) {
+    for (let i = 0; i < N; ++i) {
+      const angle = 2 * Math.PI * i / N;
+      const x = CX + Math.round(radius * Math.cos(angle));
+      const y = CY + Math.round(radius * Math.sin(angle));
+      console.log(x, y);
+      await page.mouse.move(x, y);
+    }
+  }
+});
+
