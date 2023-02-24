72b9da1fa24772583edd95af3b6fff2d5ca6a92e
sand4rt <info@mesander.com>
2023-02-24 18:21:16 +0100

    test: render empty component

--- tests/components/ct-angular/tests/render.spec.ts
+++ tests/components/ct-angular/tests/render.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/experimental-ct-angular';
 import { ButtonComponent } from '@/components/button.component';
+import { EmptyComponent } from '@/components/empty.component';
 
 test('render props', async ({ mount }) => {
   const component = await mount(ButtonComponent, {
@@ -8,4 +9,11 @@ test('render props', async ({ mount }) => {
     },
   });
   await expect(component).toContainText('Submit');
+});
+
+test('get textContent of the empty component', async ({ mount }) => {
+  const component = await mount(EmptyComponent);
+  expect(await component.allTextContents()).toEqual(['']);
+  expect(await component.textContent()).toBe('');
+  await expect(component).toHaveText('');
 });
