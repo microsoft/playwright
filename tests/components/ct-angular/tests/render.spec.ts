fcbe06b4475d6e4af2b5e1aa47d48185579c9ad1
sand4rt <info@mesander.com>
2023-02-25 14:05:34 +0100

    test: empty options

--- tests/components/ct-angular/tests/render.spec.ts
+++ tests/components/ct-angular/tests/render.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/experimental-ct-angular';
 import { ButtonComponent } from '@/components/button.component';
 import { EmptyComponent } from '@/components/empty.component';
+import { ComponentComponent } from '@/components/component.component';
 
 test('render props', async ({ mount }) => {
   const component = await mount(ButtonComponent, {
@@ -16,4 +17,9 @@ test('get textContent of the empty component', async ({ mount }) => {
   expect(await component.allTextContents()).toEqual(['']);
   expect(await component.textContent()).toBe('');
   await expect(component).toHaveText('');
+});
+
+test('render a component without options', async ({ mount }) => {
+  const component = await mount(ComponentComponent);
+  await expect(component).toContainText('test');
 });
