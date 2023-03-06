04b35089be4c094e6f8dc254702c51502d9233aa
Pavel Feldman <pavel.feldman@gmail.com>
2023-03-06 12:25:00 -0800

    chore: extract shared trace viewer components (#21433)

--- packages/trace-viewer/src/ui/timeline.tsx
+++ packages/trace-viewer/src/ui/timeline.tsx
@@ -38,24 +38,26 @@ type TimelineBar = {
 };
 
 export const Timeline: React.FunctionComponent<{
-  context: MultiTraceModel,
-  boundaries: Boundaries,
+  model: MultiTraceModel,
   selectedAction: ActionTraceEvent | undefined,
   onSelected: (action: ActionTraceEvent) => void,
-}> = ({ context, boundaries, selectedAction, onSelected }) => {
+}> = ({ model, selectedAction, onSelected }) => {
   const [measure, ref] = useMeasure<HTMLDivElement>();
   const barsRef = React.useRef<HTMLDivElement | null>(null);
 
   const [previewPoint, setPreviewPoint] = React.useState<{ x: number, clientY: number } | undefined>();
   const [hoveredBarIndex, setHoveredBarIndex] = React.useState<number | undefined>();
 
-  const offsets = React.useMemo(() => {
-    return calculateDividerOffsets(measure.width, boundaries);
-  }, [measure.width, boundaries]);
+  const { boundaries, offsets } = React.useMemo(() => {
+    const boundaries = { minimum: model.startTime, maximum: model.endTime };
+    // Leave some nice free space on the right hand side.
+    boundaries.maximum += (boundaries.maximum - boundaries.minimum) / 20;
+    return { boundaries, offsets: calculateDividerOffsets(measure.width, boundaries) };
+  }, [measure.width, model]);
 
   const bars = React.useMemo(() => {
     const bars: TimelineBar[] = [];
-    for (const entry of context.actions) {
+    for (const entry of model.actions) {
       let detail = trimRight(entry.params.selector || '', 50);
       if (entry.method === 'goto')
         detail = trimRight(entry.params.url || '', 50);
@@ -72,7 +74,7 @@ export const Timeline: React.FunctionComponent<{
       });
     }
 
-    for (const event of context.events) {
+    for (const event of model.events) {
       const startTime = event.time;
       bars.push({
         event,
@@ -87,7 +89,7 @@ export const Timeline: React.FunctionComponent<{
       });
     }
     return bars;
-  }, [context, boundaries, measure.width]);
+  }, [model, boundaries, measure.width]);
 
   const hoveredBar = hoveredBarIndex !== undefined ? bars[hoveredBarIndex] : undefined;
   let targetBar: TimelineBar | undefined = bars.find(bar => bar.action === selectedAction);
@@ -185,7 +187,7 @@ export const Timeline: React.FunctionComponent<{
         ></div>;
       })
     }</div>
-    <FilmStrip context={context} boundaries={boundaries} previewPoint={previewPoint} />
+    <FilmStrip model={model} boundaries={boundaries} previewPoint={previewPoint} />
     <div className='timeline-marker timeline-marker-hover' style={{
       display: (previewPoint !== undefined) ? 'block' : 'none',
       left: (previewPoint?.x || 0) + 'px',
