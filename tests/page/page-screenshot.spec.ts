8abe9fcf22d7089ec626a83583cc57b888e496e8
Andrey Lushnikov <aslushnikov@gmail.com>
2023-03-10 12:04:25 -0800

    tree gardening: disable & assign failing tests (#21575)
    References https://github.com/microsoft/playwright/issues/21574
    References https://github.com/microsoft/playwright/issues/21549

--- tests/page/page-screenshot.spec.ts
+++ tests/page/page-screenshot.spec.ts
@@ -310,7 +310,8 @@ it.describe('page screenshot', () => {
     }
   });
 
-  it('should work for webgl', async ({ page, server, browserName }) => {
+  it('should work for webgl', async ({ page, server, browserName, channel, browserMajorVersion }) => {
+    it.fixme(channel === 'msedge' && browserMajorVersion === 110, 'https://github.com/microsoft/playwright/issues/21549');
     it.fixme(browserName === 'firefox');
 
     await page.setViewportSize({ width: 640, height: 480 });
