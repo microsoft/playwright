06b52932363ddb29141a8ca69af6cada9760c6c4
Pavel <pavel.feldman@gmail.com>
2023-03-19 10:31:52 -0700

    chore: allow watching all tests

--- tests/playwright-test/ui-mode-test-watch.spec.ts
+++ tests/playwright-test/ui-mode-test-watch.spec.ts
@@ -57,3 +57,165 @@ test('should watch files', async ({ runUITest, writeFiles }) => {
         ✅ fails 👁 <=
   `);
 });
+
+test('should watch e2e deps', async ({ runUITest, writeFiles }) => {
+  const page = await runUITest({
+    'playwright.config.ts': `
+      import { defineConfig } from '@playwright/test';
+      export default defineConfig({ testDir: 'tests' });
+    `,
+    'src/helper.ts': `
+      export const answer = 41;
+    `,
+    'tests/a.test.ts': `
+      import { test, expect } from '@playwright/test';
+      import { answer } from '../src/helper';
+      test('answer', () => { expect(answer).toBe(42); });
+    `,
+  });
+
+  await page.getByText('answer').click();
+  await page.getByRole('listitem').filter({ hasText: 'answer' }).getByTitle('Watch').click();
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ◯ a.test.ts
+        ◯ answer 👁 <=
+  `);
+
+  await writeFiles({
+    'src/helper.ts': `
+      export const answer = 42;
+    `
+  });
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ✅ a.test.ts
+        ✅ answer 👁 <=
+  `);
+});
+
+test('should batch watch updates', async ({ runUITest, writeFiles }) => {
+  const page = await runUITest({
+    'a.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'b.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'c.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'd.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await page.getByText('a.test.ts').click();
+  await page.getByRole('listitem').filter({ hasText: 'a.test.ts' }).getByTitle('Watch').click();
+  await page.getByText('b.test.ts').click();
+  await page.getByRole('listitem').filter({ hasText: 'b.test.ts' }).getByTitle('Watch').click();
+  await page.getByText('c.test.ts').click();
+  await page.getByRole('listitem').filter({ hasText: 'c.test.ts' }).getByTitle('Watch').click();
+  await page.getByText('d.test.ts').click();
+  await page.getByRole('listitem').filter({ hasText: 'd.test.ts' }).getByTitle('Watch').click();
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ◯ a.test.ts 👁
+        ◯ test
+    ▼ ◯ b.test.ts 👁
+        ◯ test
+    ▼ ◯ c.test.ts 👁
+        ◯ test
+    ▼ ◯ d.test.ts 👁 <=
+        ◯ test
+  `);
+
+  await writeFiles({
+    'a.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'b.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'c.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'd.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await expect(page.getByTestId('status-line')).toHaveText('4/4 passed (100%)');
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ✅ a.test.ts 👁
+        ✅ test
+    ▼ ✅ b.test.ts 👁
+        ✅ test
+    ▼ ✅ c.test.ts 👁
+        ✅ test
+    ▼ ✅ d.test.ts 👁 <=
+        ✅ test
+  `);
+});
+
+test('should watch all', async ({ runUITest, writeFiles }) => {
+  const page = await runUITest({
+    'a.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'b.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'c.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'd.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ◯ a.test.ts
+        ◯ test
+    ▼ ◯ b.test.ts
+        ◯ test
+    ▼ ◯ c.test.ts
+        ◯ test
+    ▼ ◯ d.test.ts
+        ◯ test
+  `);
+  await page.getByTitle('Watch all').click();
+
+  await writeFiles({
+    'a.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+    'd.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await expect(page.getByTestId('status-line')).toHaveText('2/2 passed (100%)');
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ✅ a.test.ts
+        ✅ test
+    ▼ ◯ b.test.ts
+        ◯ test
+    ▼ ◯ c.test.ts
+        ◯ test
+    ▼ ✅ d.test.ts
+        ✅ test
+  `);
+});
+
+test('should watch new file', async ({ runUITest, writeFiles }) => {
+  const page = await runUITest({
+    'a.test.ts': `import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await page.getByTitle('Watch all').click();
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ◯ a.test.ts
+        ◯ test
+  `);
+
+  // First time add file.
+  await writeFiles({
+    'b.test.ts': ` import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ◯ a.test.ts
+        ◯ test
+    ▼ ◯ b.test.ts
+        ◯ test
+  `);
+
+  // Second time run file.
+  await writeFiles({
+    'b.test.ts': ` import { test } from '@playwright/test'; test('test', () => {});`,
+  });
+
+  await expect(page.getByTestId('status-line')).toHaveText('1/1 passed (100%)');
+
+  await expect.poll(dumpTestTree(page), { timeout: 15000 }).toBe(`
+    ▼ ◯ a.test.ts
+        ◯ test
+    ▼ ✅ b.test.ts
+        ✅ test
+  `);
+});
