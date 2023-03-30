bb20dd70a1d6db6a9b22cab7d95bbc56d10fb8ed
sand4rt <info@mesander.com>
2023-03-30 19:07:26 +0200

    fix(ct): solid type checking

--- tests/components/ct-solid/src/components/Counter.tsx
+++ tests/components/ct-solid/src/components/Counter.tsx
@@ -11,8 +11,8 @@ import { createSignal } from "solid-js";
  export default function Counter(props: CounterProps) {
   const [remountCount, setRemountCount] = createSignal(_remountCount++);
   return <div onClick={() => props.onClick?.('hello')}>
-     <div data-testid="props">{ props.count }</div>
-     <div data-testid="remount-count">{ remountCount }</div>
+     <div data-testid="props">{props.count}</div>
+     <div data-testid="remount-count">{remountCount()}</div>
      { props.children }
    </div>
  }
