75e1ad63156b6eb81c468178047ee3b597d7e1da
Pavel Feldman <pavel.feldman@gmail.com>
2023-04-17 09:21:38 -0700

    chore: introduce ct-core to hard-depend on vite

---
+++ packages/playwright-ct-core/README.md
@@ -0,0 +1,3 @@
+> **BEWARE** This package is EXPERIMENTAL and does not respect semver.
+
+Read more at https://playwright.dev/docs/test-components
--- packages/playwright-ct-solid/index.js
+++ packages/playwright-ct-solid/index.js
@@ -20,7 +20,7 @@ const path = require('path');
 
 const plugin = () => {
   // Only fetch upon request to avoid resolution in workers.
-  const { createPlugin } = require('@playwright/test/lib/plugins/vitePlugin');
+  const { createPlugin } = require('@playwright/experimental-ct-core/lib/vitePlugin');
   return createPlugin(
     path.join(__dirname, 'registerSource.mjs'),
     () => import('vite-plugin-solid').then(plugin => plugin.default()));
