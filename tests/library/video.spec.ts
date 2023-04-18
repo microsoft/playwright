dae79ab6039306b28fff1051070a62e0757b5e78
Yury Semikhatsky <yurys@chromium.org>
2023-04-18 09:02:04 -0700

    test: remove fixme for webkit video on linux
    Fixes https://github.com/microsoft/playwright/issues/22411

--- tests/library/video.spec.ts
+++ tests/library/video.spec.ts
@@ -674,10 +674,10 @@ it.describe('screencast', () => {
     expect(files.length).toBe(1);
   });
 
-  it('should capture full viewport', async ({ browserType, browserName, headless, isWindows, isLinux }, testInfo) => {
+  it('should capture full viewport', async ({ browserType, browserName, headless, isWindows }, testInfo) => {
+    it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/22411' });
     it.fixme(browserName === 'chromium' && !headless, 'The square is not on the video');
     it.fixme(browserName === 'firefox' && isWindows, 'https://github.com/microsoft/playwright/issues/14405');
-    it.fixme(browserName === 'webkit' && isLinux && !headless, 'https://github.com/microsoft/playwright/issues/22411');
     const size = { width: 600, height: 400 };
     const browser = await browserType.launch();
 
@@ -709,10 +709,10 @@ it.describe('screencast', () => {
     expectAll(pixels, almostRed);
   });
 
-  it('should capture full viewport on hidpi', async ({ browserType, browserName, headless, isWindows, isLinux }, testInfo) => {
+  it('should capture full viewport on hidpi', async ({ browserType, browserName, headless, isWindows }, testInfo) => {
+    it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/22411' });
     it.fixme(browserName === 'chromium' && !headless, 'The square is not on the video');
     it.fixme(browserName === 'firefox' && isWindows, 'https://github.com/microsoft/playwright/issues/14405');
-    it.fixme(browserName === 'webkit' && isLinux && !headless, 'https://github.com/microsoft/playwright/issues/22411');
     const size = { width: 600, height: 400 };
     const browser = await browserType.launch();
 
