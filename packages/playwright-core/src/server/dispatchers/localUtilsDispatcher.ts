c0c4178b60f7a3d26ba07ba2659d970d67d76a4f
Noam Gaash <noam.gaash@applitools.com>
2023-04-23 11:09:29 +0300

    feat(routeFromHar): prefer later responds

--- packages/playwright-core/src/server/dispatchers/localUtilsDispatcher.ts
+++ packages/playwright-core/src/server/dispatchers/localUtilsDispatcher.ts
@@ -306,9 +306,8 @@ class HarBackend {
           const matchingHeaders = countMatchingHeaders(candidate.request.headers, headers);
           list.push({ candidate, matchingHeaders });
         }
+        list.reverse();
         list.sort((a, b) => b.matchingHeaders - a.matchingHeaders);
-        while (list[0].candidate.time < 0 && list.some(entry => entry.candidate.time >= 0))
-          list.shift();
         entry = list[0].candidate;
       }
 
