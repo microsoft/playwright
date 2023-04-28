fa3a76f4720e42ba3e88e9ad23e49ed32337cfc0
sand4rt <info@mesander.com>
2023-04-28 23:51:37 +0200

    chore(ct): svelte resolve compile warnings

--- tests/components/ct-svelte/src/components/Counter.svelte
+++ tests/components/ct-svelte/src/components/Counter.svelte
@@ -6,9 +6,9 @@ const dispatch = createEventDispatcher();
 update();
 </script>
 
-<div on:click={() => dispatch('submit', 'hello')}>
+<button on:click={() => dispatch('submit', 'hello')}>
   <div data-testid="props">{count}</div>
   <div data-testid="remount-count">{remountCount}</div>
   <slot name="main" />
   <slot />
-</div>
+</button>
