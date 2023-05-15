eb0620cf204f63b5d15a8cca591b1adbcb6e514f
Pavel Feldman <pavel.feldman@gmail.com>
2023-05-15 09:23:27 -0700

    chore: do not load code for babeling twice

--- packages/playwright-test/src/common/babelBundle.ts
+++ packages/playwright-test/src/common/babelBundle.ts
@@ -21,7 +21,7 @@ export const types: typeof import('../../bundles/babel/node_modules/@types/babel
 export const parse: typeof import('../../bundles/babel/node_modules/@babel/parser/typings/babel-parser').parse = require('./babelBundleImpl').parse;
 export const traverse: typeof import('../../bundles/babel/node_modules/@types/babel__traverse').default = require('./babelBundleImpl').traverse;
 export type BabelPlugin = [string, any?];
-export type BabelTransformFunction = (filename: string, isTypeScript: boolean, isModule: boolean, pluginsPrefix: BabelPlugin[], pluginsSuffix: BabelPlugin[]) => BabelFileResult;
+export type BabelTransformFunction = (code: string, filename: string, isTypeScript: boolean, isModule: boolean, pluginsPrefix: BabelPlugin[], pluginsSuffix: BabelPlugin[]) => BabelFileResult;
 export const babelTransform: BabelTransformFunction = require('./babelBundleImpl').babelTransform;
 export type { NodePath, types as T } from '../../bundles/babel/node_modules/@types/babel__core';
 export type { BabelAPI } from '../../bundles/babel/node_modules/@types/babel__helper-plugin-utils';
