08cc0a46e4bb0a481ea877aa8bfd7659de720d7d
João Neves <joaomiguelneves@gmail.com>
2023-05-29 08:54:35 +0000

    Fix test

--- tests/page/selectors-react.spec.ts
+++ tests/page/selectors-react.spec.ts
@@ -126,7 +126,7 @@ for (const [name, url] of Object.entries(reacts)) {
 
     it('should work with react memo', async ({ page }) => {
       it.skip(name === 'react15' || name === 'react16', 'Class components dont support memo');
-      await expect(page.locator(`_react=ButtonGrid`)).toHaveCount(1);
+      await expect(page.locator(`_react=ButtonGrid`)).toHaveCount(9);
     });
 
     it('should work with multiroot react', async ({ page }) => {
