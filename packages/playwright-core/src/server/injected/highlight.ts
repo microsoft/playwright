0fbbb8df92a57ffa39852310aa71bceb3feb78d8
Max Schmitt <max@schmitt.mx>
2023-05-30 10:56:16 +0200

    fix: trace viewer with global applied background-color

--- packages/playwright-core/src/server/injected/highlight.ts
+++ packages/playwright-core/src/server/injected/highlight.ts
@@ -53,6 +53,7 @@ export class Highlight {
     this._glassPaneElement.style.zIndex = '2147483647';
     this._glassPaneElement.style.pointerEvents = 'none';
     this._glassPaneElement.style.display = 'flex';
+    this._glassPaneElement.style.backgroundColor = 'transparent';
 
     this._actionPointElement = document.createElement('x-pw-action-point');
     this._actionPointElement.setAttribute('hidden', 'true');
