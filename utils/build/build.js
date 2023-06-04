ffef575685b5bcc94f2774b8cf0e64712cf4fbc5
Rui Figueira <rui.figueira@gmail.com>
2023-06-04 14:41:28 +0100

    devops(crx): build crx module

--- utils/build/build.js
+++ utils/build/build.js
@@ -260,7 +260,7 @@ for (const bundle of bundles) {
 }
 
 // Build/watch web packages.
-for (const webPackage of ['html-reporter', 'recorder', 'trace-viewer']) {
+for (const webPackage of ['html-reporter', 'recorder', 'trace-viewer', 'crx']) {
   steps.push({
     command: 'npx',
     args: ['vite', 'build', ...(watchMode ? ['--watch', '--sourcemap', '--minify=false'] : [])],
@@ -278,6 +278,14 @@ steps.push({
   concurrent: true,
 });
 
+// Build/watch recorder for crx.
+steps.push({
+  command: 'npx',
+  args: ['vite', '--config', 'vite.recorder.config.ts', 'build', ...(watchMode ? ['--watch', '--sourcemap', '--minify=false'] : [])],
+  shell: true,
+  cwd: path.join(__dirname, '..', '..', 'packages', 'crx'),
+  concurrent: true,
+});
 
 // Generate injected.
 onChanges.push({
