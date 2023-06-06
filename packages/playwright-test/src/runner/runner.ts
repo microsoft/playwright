d4349e5d6f42014de887c066517521efbaacf4aa
Pavel Feldman <pavel.feldman@gmail.com>
2023-06-06 09:36:49 -0700

    chore: print listening on message in terminal for web trace modes (#23546)

--- packages/playwright-test/src/runner/runner.ts
+++ packages/playwright-test/src/runner/runner.ts
@@ -107,9 +107,9 @@ export class Runner {
     return await runWatchModeLoop(config);
   }
 
-  async uiAllTests(openInBrowser: boolean): Promise<FullResult['status']> {
+  async uiAllTests(options: { host?: string, port?: number }): Promise<FullResult['status']> {
     const config = this._config;
     webServerPluginsForConfig(config).forEach(p => config.plugins.push({ factory: p }));
-    return await runUIMode(config, openInBrowser);
+    return await runUIMode(config, options);
   }
 }
