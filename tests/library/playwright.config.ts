39a15b60ce775ff0692831cfa25b41b68ea24852
Yury Semikhatsky <yurys@chromium.org>
2023-06-12 13:40:50 -0700

    No screenshots either

--- tests/library/playwright.config.ts
+++ tests/library/playwright.config.ts
@@ -105,7 +105,7 @@ for (const browserName of browserNames) {
           executablePath,
           devtools
         },
-        trace: trace ? { mode: 'on', sources: false } : undefined,
+        trace: trace ? { mode: 'on', sources: false, screenshots: false } : undefined,
         coverageName: browserName,
       },
       metadata: {
