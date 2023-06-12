49ece365ab344adb5ea0b35746a83d2d938f9336
Andrey Lushnikov <aslushnikov@gmail.com>
2023-06-12 11:07:39 -0700

    cherry-pick(#23661): fix: drop `expect.configure({ poll })` from types
    `poll` option is not actually supported as of June 12, 2023.
    
    Fixes https://github.com/microsoft/playwright/issues/23622

--- packages/playwright-test/types/test.d.ts
+++ packages/playwright-test/types/test.d.ts
@@ -4786,7 +4786,6 @@ export type Expect = {
     message?: string,
     timeout?: number,
     soft?: boolean,
-    poll?: boolean | { timeout?: number, intervals?: number[] },
   }) => Expect;
   getState(): {
     expand?: boolean;
