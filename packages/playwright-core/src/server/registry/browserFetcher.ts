b44723708c50ee9692c55ceebc8ff988298234e7
Max Schmitt <max@schmitt.mx>
2023-06-16 20:40:15 +0200

    chore: move extracting of browser out-of-process (#23739)
    https://github.com/microsoft/playwright/issues/23729

--- packages/playwright-core/src/server/registry/browserFetcher.ts
+++ packages/playwright-core/src/server/registry/browserFetcher.ts
@@ -19,15 +19,14 @@ import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import childProcess from 'child_process';
-import { getUserAgent } from '../../utils/userAgent';
 import { existsAsync } from '../../utils/fileUtils';
 import { debugLogger } from '../../common/debugLogger';
-import { extract } from '../../zipBundle';
 import { ManualPromise } from '../../utils/manualPromise';
 import { colors } from '../../utilsBundle';
+import { browserDirectoryToMarkerFilePath } from '.';
 
 export async function downloadBrowserWithProgressBar(title: string, browserDirectory: string, executablePath: string | undefined, downloadURLs: string[], downloadFileName: string, downloadConnectionTimeout: number): Promise<boolean> {
-  if (await existsAsync(browserDirectory)) {
+  if (await existsAsync(browserDirectoryToMarkerFilePath(browserDirectory))) {
     // Already downloaded.
     debugLogger.log('install', `${title} is already downloaded.`);
     return false;
@@ -40,23 +39,19 @@ export async function downloadBrowserWithProgressBar(title: string, browserDirec
       debugLogger.log('install', `downloading ${title} - attempt #${attempt}`);
       const url = downloadURLs[(attempt - 1) % downloadURLs.length];
       logPolitely(`Downloading ${title}` + colors.dim(` from ${url}`));
-      const { error } = await downloadFileOutOfProcess(url, zipPath, getUserAgent(), downloadConnectionTimeout);
+      const { error } = await downloadBrowserWithProgressBarOutOfProcess(title, browserDirectory, url, zipPath, executablePath, downloadConnectionTimeout);
       if (!error) {
-        debugLogger.log('install', `SUCCESS downloading ${title}`);
+        debugLogger.log('install', `SUCCESS installing ${title}`);
         break;
       }
+      if (await existsAsync(zipPath))
+        await fs.promises.unlink(zipPath);
+      if (await existsAsync(browserDirectory))
+        await fs.promises.rmdir(browserDirectory, { recursive: true });
       const errorMessage = error?.message || '';
       debugLogger.log('install', `attempt #${attempt} - ERROR: ${errorMessage}`);
       if (attempt >= retryCount)
         throw error;
-    }
-    debugLogger.log('install', `extracting archive`);
-    debugLogger.log('install', `-- zip: ${zipPath}`);
-    debugLogger.log('install', `-- location: ${browserDirectory}`);
-    await extract(zipPath, { dir: browserDirectory });
-    if (executablePath) {
-      debugLogger.log('install', `fixing permissions at ${executablePath}`);
-      await fs.promises.chmod(executablePath, 0o755);
     }
   } catch (e) {
     debugLogger.log('install', `FAILED installation ${title} with error: ${e}`);
@@ -75,8 +70,8 @@ export async function downloadBrowserWithProgressBar(title: string, browserDirec
  * Thats why we execute it in a separate process and check manually if the destination file exists.
  * https://github.com/microsoft/playwright/issues/17394
  */
-function downloadFileOutOfProcess(url: string, destinationPath: string, userAgent: string, downloadConnectionTimeout: number): Promise<{ error: Error | null }> {
-  const cp = childProcess.fork(path.join(__dirname, 'oopDownloadMain.js'), [url, destinationPath, userAgent, String(downloadConnectionTimeout)]);
+function downloadBrowserWithProgressBarOutOfProcess(title: string, browserDirectory: string, url: string, zipPath: string, executablePath: string | undefined, downloadConnectionTimeout: number): Promise<{ error: Error | null }> {
+  const cp = childProcess.fork(path.join(__dirname, 'oopDownloadBrowserMain.js'), [title, browserDirectory, url, zipPath, executablePath || '', String(downloadConnectionTimeout)]);
   const promise = new ManualPromise<{ error: Error | null }>();
   cp.on('message', (message: any) => {
     if (message?.method === 'log')
@@ -87,8 +82,8 @@ function downloadFileOutOfProcess(url: string, destinationPath: string, userAgen
       promise.resolve({ error: new Error(`Download failure, code=${code}`) });
       return;
     }
-    if (!fs.existsSync(destinationPath))
-      promise.resolve({ error: new Error(`Download failure, ${destinationPath} does not exist`) });
+    if (!fs.existsSync(browserDirectoryToMarkerFilePath(browserDirectory)))
+      promise.resolve({ error: new Error(`Download failure, ${browserDirectoryToMarkerFilePath(browserDirectory)} does not exist`) });
     else
       promise.resolve({ error: null });
   });
