b032ec9f6229960d53cfb62acab74dec482c86fa
Max Schmitt <max@schmitt.mx>
2023-06-18 23:26:10 +0200

    fix(codegen): generate no Enter keyboard events for textareas

--- packages/playwright-core/src/server/injected/recorder.ts
+++ packages/playwright-core/src/server/injected/recorder.ts
@@ -315,6 +315,9 @@ export class Recorder {
   }
 
   private _shouldGenerateKeyPressFor(event: KeyboardEvent): boolean {
+    // Enter aka. new line is handled in input event.
+    if (event.key === 'Enter' && event.target && ((event.target as HTMLElement).nodeName === 'TEXTAREA') || (event.target as HTMLElement).isContentEditable)
+      return false;
     // Backspace, Delete, AltGraph are changing input, will handle it there.
     if (['Backspace', 'Delete', 'AltGraph'].includes(event.key))
       return false;
