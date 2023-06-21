6ad66881746678c09f56a55370d782ddc90b26e9
Ben Hofferber <hofferber.ben@gmail.com>
2023-06-21 00:38:39 +0000

    feat(goto): better navigation error message

--- tests/page/page-goto.spec.ts
+++ tests/page/page-goto.spec.ts
@@ -460,7 +460,7 @@ it('js redirect overrides url bar navigation ', async ({ page, server, browserNa
     expect(error).toBeFalsy();
     await expect(page).toHaveURL(server.PREFIX + '/b');
   } else if (browserName === 'webkit') {
-    expect(error.message).toContain('Navigation interrupted by another one');
+    expect(error.message).toContain(`Navigation to "${server.PREFIX + '/a'}" is interrupted by another navigation to "${server.PREFIX + '/b'}"`);
     await expect(page).toHaveURL(server.PREFIX + '/c');
   } else if (browserName === 'firefox') {
     expect(error.message).toContain('NS_BINDING_ABORTED');
