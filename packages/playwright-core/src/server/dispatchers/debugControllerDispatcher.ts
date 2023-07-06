0409bfca562102e5a0534461e214eac61ca22e9d
Pavel Feldman <pavel.feldman@gmail.com>
2023-07-06 12:01:45 -0700

    chore: ensure module deps in ct-core (#24056)
    Fixes https://github.com/microsoft/playwright/issues/23823

--- packages/playwright-core/src/server/dispatchers/debugControllerDispatcher.ts
+++ packages/playwright-core/src/server/dispatchers/debugControllerDispatcher.ts
@@ -15,8 +15,8 @@
  */
 
 import type * as channels from '@protocol/channels';
-import { eventsHelper } from 'playwright-core/lib/utils';
-import type { RegisteredListener } from 'playwright-core/lib/utils/eventsHelper';
+import { eventsHelper } from '../../utils';
+import type { RegisteredListener } from '../../utils/eventsHelper';
 import { DebugController } from '../debugController';
 import type { DispatcherConnection, RootDispatcher } from './dispatcher';
 import { Dispatcher } from './dispatcher';
