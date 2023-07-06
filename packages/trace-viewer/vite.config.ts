39a76657394084a3850647bb0bcee2708e3457a8
Fadi Zaqout <fadyzaqut@gmail.com>
2023-07-06 15:06:19 +0300

    trace viewer npm package

--- packages/trace-viewer/vite.config.ts
+++ packages/trace-viewer/vite.config.ts
@@ -19,13 +19,16 @@ import react from '@vitejs/plugin-react';
 // @ts-ignore
 import { bundle } from './bundle';
 import * as path from 'path';
+import dts from 'vite-plugin-dts';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   plugins: [
     react(),
-    bundle()
+    bundle(),
+    dts()
   ],
   resolve: {
     alias: {
@@ -38,20 +41,23 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(__dirname, '../playwright-core/lib/webpack/traceViewer'),
     // Output dir is shared with vite.sw.config.ts, clearing it here is racy.
+    sourcemap: true,
     emptyOutDir: false,
+    lib: {
+      entry: path.resolve(__dirname, './src/index.ts'),
+      name: 'traceViewer',
+      formats: ['es', 'cjs'],
+      fileName: format => `index.${format}.js`,
+    },
     rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, 'index.html'),
-        uiMode: path.resolve(__dirname, 'uiMode.html'),
-        snapshot: path.resolve(__dirname, 'snapshot.html'),
-      },
+      external: ['react', 'react-dom'],
       output: {
-        entryFileNames: () => '[name].[hash].js',
-        assetFileNames: () => '[name].[hash][extname]',
-        manualChunks: undefined,
-      },
+        globals: {
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+        }
+      }
     },
   }
 });
