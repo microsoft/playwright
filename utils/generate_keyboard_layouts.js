02e5620b963ca0de58e1bbc6decd49c12df8fcc3
Rui Figueira <rui.figueira@gmail.com>
2023-07-19 23:33:39 +0100

    chore: keyboard layout doc feeds the generator
    Keyboard layout generator uses the markdown table to get which layouts it needs to generate

--- utils/generate_keyboard_layouts.js
+++ utils/generate_keyboard_layouts.js
@@ -27,130 +27,10 @@ const fs = require('fs');
 const assert = require('assert');
 const path = require('path');
 const xml2js = require('xml2js');
+const md = require('./markdown');
 
 // US keyboard
 const defaultKlid = '00000409';
-
-/**
- * @type { Object.<string, { layoutName: string, locales: string[] }> }
- */
-const layoutUrls = {
-  '00000409': { layoutName: 'US keyboard', locales: ['af-ZA', 'en-US', 'en-AU', 'en-BZ', 'en-CA', 'en-029', 'en-HK', 'en-JM', 'en-MY', 'en-PH', 'en-SG', 'en-ZA', 'en-TT', 'en-ZW', 'fil-PH', 'id-ID', 'jv', 'rw-RW', 'sw-KE', 'ms-MY', 'ms-BN', 'moh-CA', 'om-ET', 'pap-029', 'st-ZA', 'so-SO', 'uz-Latn-UZ', 'ts-ZA', 'xh-ZA', 'zu-ZA'] },
-  '0000041C': { layoutName: 'Albanian keyboard', locales: ['sq-AL'] },
-  '0000040C': { layoutName: 'French keyboard', locales: ['gsw-FR', 'br-FR', 'fr-FR', 'co-FR', 'fr-CM', 'fr-CI', 'fr-HT', 'fr-ML', 'fr-MC', 'fr-MA', 'fr-RE', 'fr-SN', 'fr-CD', 'mg', 'oc-FR'] },
-  '00000401': { layoutName: 'Arabic (101) keyboard', locales: ['ar-SA', 'ar-BH', 'ar-EG', 'ar-IQ', 'ar-JO', 'ar-KW', 'ar-LB', 'ar-LY', 'ar-OM', 'ar-QA', 'ar-SY', 'ar-AE', 'ar-YE'] },
-  '00020401': { layoutName: 'Arabic (102) AZERTY keyboard', locales: ['ar-DZ', 'ar-MA', 'ar-TN'] },
-  '0002042B': { layoutName: 'Armenian Phonetic keyboard', locales: ['hy-AM'] },
-  '0000044D': { layoutName: 'Assamese - INSCRIPT keyboard', locales: ['as-IN'] },
-  '0000040A': { layoutName: 'Spanish keyboard', locales: ['es-ES_tradnl', 'eu-ES', 'ca-ES', 'gl-ES', 'es-ES', 'ca-ES-valencia'] },
-  '0000042C': { layoutName: 'Azerbaijani Latin keyboard', locales: ['az-Latn-AZ'] },
-  '0000082C': { layoutName: 'Azerbaijani Cyrillic keyboard', locales: ['az-Cyrl-AZ'] },
-  '00000445': { layoutName: 'Bangla keyboard', locales: ['bn-BD'] },
-  '0000046D': { layoutName: 'Bashkir keyboard', locales: ['ba-RU'] },
-  '00000423': { layoutName: 'Belarusian keyboard', locales: ['be-BY'] },
-  '00020445': { layoutName: 'Bangla - INSCRIPT keyboard', locales: ['bn-IN'] },
-  '00000439': { layoutName: 'Devanagari - INSCRIPT keyboard', locales: ['kok-IN', 'sa-IN'] },
-  '0000041A': { layoutName: 'Standard keyboard', locales: ['bs-Latn-BA', 'hr-HR', 'hr-BA'] },
-  '0000201A': { layoutName: 'Bosnian (Cyrillic) keyboard', locales: ['bs-Cyrl-BA'] },
-  '00030402': { layoutName: 'Bulgarian keyboard', locales: ['bg-BG'] },
-  '00130C00': { layoutName: 'Myanmar (Visual order) keyboard', locales: ['my-MM'] },
-  '00000410': { layoutName: 'Italian keyboard', locales: ['it-IT'] },
-  '0000085F': { layoutName: 'Central Atlas Tamazight keyboard', locales: ['tzm-Latn-DZ'] },
-  '0000105F': { layoutName: 'Tifinagh (Basic) keyboard', locales: ['tzm-Tfng-MA', 'zgh'] },
-  '00000492': { layoutName: 'Central Kurdish keyboard', locales: ['ku-Arab-IQ'] },
-  '00000419': { layoutName: 'Russian keyboard', locales: ['ru-RU'] },
-  '0000045C': { layoutName: 'Cherokee Nation keyboard', locales: ['chr-Cher-US'] },
-  '00000407': { layoutName: 'German keyboard', locales: ['de-DE', 'de-AT', 'de-LU'] },
-  '00000809': { layoutName: 'United Kingdom keyboard', locales: ['en-GB'] },
-  '00000405': { layoutName: 'Czech keyboard', locales: ['cs-CZ'] },
-  '00000406': { layoutName: 'Danish keyboard', locales: ['da-DK', 'fo-FO', 'kl-GL'] },
-  '00000465': { layoutName: 'Divehi Phonetic keyboard', locales: ['dv-MV'] },
-  '00020409': { layoutName: 'United States-International keyboard', locales: ['nl-NL', 'fy-NL'] },
-  '00000813': { layoutName: 'Belgian (Period) keyboard', locales: ['nl-BE'] },
-  '00000C51': { layoutName: 'Dzongkha keyboard', locales: ['dz-BT'] },
-  '0000080C': { layoutName: 'Belgian French keyboard', locales: ['fr-BE'] },
-  '0000040B': { layoutName: 'Finnish keyboard', locales: ['fi-FI'] },
-  '00004009': { layoutName: 'English (India) keyboard', locales: ['en-IN'] },
-  '00001809': { layoutName: 'Irish keyboard', locales: ['en-IE', 'ga-IE'] },
-  '00001409': { layoutName: 'NZ Aotearoa keyboard', locales: ['en-NZ'] },
-  '00000424': { layoutName: 'Slovenian keyboard', locales: ['sl-SI'] },
-  '0000041D': { layoutName: 'Swedish keyboard', locales: ['sv-SE', 'sv-FI'] },
-  '00000425': { layoutName: 'Estonian keyboard', locales: ['et-EE'] },
-  '00001009': { layoutName: 'Canadian French keyboard', locales: ['fr-CA'] },
-  '0000100C': { layoutName: 'Swiss French keyboard', locales: ['fr-LU', 'fr-CH', 'it-CH'] },
-  '00000488': { layoutName: 'Wolof keyboard', locales: ['ff-Latn-SN', 'wo-SN'] },
-  '00010437': { layoutName: 'Georgian (QWERTY) keyboard', locales: ['ka-GE'] },
-  '00000807': { layoutName: 'Swiss German keyboard', locales: ['de-LI', 'de-CH', 'rm-CH'] },
-  '00000408': { layoutName: 'Greek keyboard', locales: ['el-GR'] },
-  '00000474': { layoutName: 'Guarani keyboard', locales: ['gn-PY'] },
-  '00000447': { layoutName: 'Gujarati keyboard', locales: ['gu-IN'] },
-  '00000468': { layoutName: 'Hausa keyboard', locales: ['ha-Latn-NG'] },
-  '00000475': { layoutName: 'Hawaiian keyboard', locales: ['haw-US'] },
-  '0002040D': { layoutName: 'Hebrew (Standard) keyboard', locales: ['he-IL'] },
-  '00010439': { layoutName: 'Hindi Traditional keyboard', locales: ['hi-IN'] },
-  '0000040E': { layoutName: 'Hungarian keyboard', locales: ['hu-HU'] },
-  '0000040F': { layoutName: 'Icelandic keyboard', locales: ['is-IS'] },
-  '00000470': { layoutName: 'Igbo keyboard', locales: ['ig-NG'] },
-  '0000085D': { layoutName: 'Inuktitut - Latin keyboard', locales: ['iu-Latn-CA'] },
-  '0001045D': { layoutName: 'Inuktitut - Naqittaut keyboard', locales: ['iu-Cans-CA'] },
-  '00110C00': { layoutName: 'Javanese keyboard', locales: ['jv-Java'] },
-  '0000044B': { layoutName: 'Kannada keyboard', locales: ['kn-IN'] },
-  '00000420': { layoutName: 'Urdu keyboard', locales: ['ur-PK', 'pa-Arab-PK', 'sd-Arab-PK', 'ur-IN'] },
-  '0000043F': { layoutName: 'Kazakh keyboard', locales: ['kk-KZ'] },
-  '00000453': { layoutName: 'Khmer keyboard', locales: ['km-KH'] },
-  '00000440': { layoutName: 'Kyrgyz Cyrillic keyboard', locales: ['ky-KG'] },
-  '0000080A': { layoutName: 'Latin American keyboard', locales: ['quc-Latn-GT', 'arn-CL', 'quz-BO', 'quz-EC', 'quz-PE', 'es-AR', 'es-BO', 'es-CL', 'es-CO', 'es-CR', 'es-MX', 'es-DO', 'es-EC', 'es-SV', 'es-GT', 'es-HN', 'es-419', 'es-NI', 'es-PA', 'es-PY', 'es-PE', 'es-PR', 'es-US', 'es-UY', 'es-VE'] },
-  '00000454': { layoutName: 'Lao keyboard', locales: ['lo-LA'] },
-  '00020426': { layoutName: 'Latvian (Standard) keyboard', locales: ['lv-LV'] },
-  '00010427': { layoutName: 'Lithuanian keyboard', locales: ['lt-LT'] },
-  '0002042E': { layoutName: 'Sorbian Standard keyboard', locales: ['dsb-DE', 'hsb-DE'] },
-  '0000046E': { layoutName: 'Luxembourgish keyboard', locales: ['lb-LU'] },
-  '0001042F': { layoutName: 'Macedonian - Standard keyboard', locales: ['mk-MK'] },
-  '0000044C': { layoutName: 'Malayalam keyboard', locales: ['ml-IN'] },
-  '0000043A': { layoutName: 'Maltese 47-Key keyboard', locales: ['mt-MT'] },
-  '00000481': { layoutName: 'Maori keyboard', locales: ['mi-NZ'] },
-  '0000044E': { layoutName: 'Marathi keyboard', locales: ['mr-IN'] },
-  '00000429': { layoutName: 'Persian keyboard', locales: ['fa-IR'] },
-  '00000450': { layoutName: 'Mongolian Cyrillic keyboard', locales: ['mn-MN'] },
-  '00010850': { layoutName: 'Traditional Mongolian (Standard) keyboard', locales: ['mn-Mong-CN', 'mn-Mong-MN'] },
-  '00090C00': { layoutName: 'N’Ko keyboard', locales: ['nqo'] },
-  '00000461': { layoutName: 'Nepali keyboard', locales: ['ne-NP', 'ne-IN'] },
-  '0000043B': { layoutName: 'Norwegian with Sami keyboard', locales: ['se-NO', 'smj-NO', 'sma-NO'] },
-  '00000414': { layoutName: 'Norwegian keyboard', locales: ['nb-NO', 'nn-NO'] },
-  '00000448': { layoutName: 'Odia keyboard', locales: ['or-IN'] },
-  '00000463': { layoutName: 'Pashto (Afghanistan) keyboard', locales: ['ps-AF'] },
-  '00050429': { layoutName: 'Persian (Standard) keyboard', locales: ['fa-AF'] },
-  '00000415': { layoutName: 'Polish (Programmers) keyboard', locales: ['pl-PL'] },
-  '00000416': { layoutName: 'Portuguese (Brazil ABNT) keyboard', locales: ['pt-BR'] },
-  '00000816': { layoutName: 'Portuguese keyboard', locales: ['pt-PT'] },
-  '00000446': { layoutName: 'Punjabi keyboard', locales: ['pa-IN'] },
-  '00010418': { layoutName: 'Romanian (Standard) keyboard', locales: ['ro-RO', 'ro-MD'] },
-  '00000485': { layoutName: 'Sakha keyboard', locales: ['sah-RU'] },
-  '0001083B': { layoutName: 'Finnish with Sami keyboard', locales: ['smn-FI', 'sms-FI', 'se-FI'] },
-  '0000083B': { layoutName: 'Swedish with Sami keyboard', locales: ['smj-SE', 'sma-SE', 'se-SE'] },
-  '00011809': { layoutName: 'Scottish Gaelic keyboard', locales: ['gd-GB'] },
-  '0000081A': { layoutName: 'Serbian (Latin) keyboard', locales: ['sr-Latn-RS', 'sr-Latn-BA', 'sr-Latn-ME'] },
-  '00000C1A': { layoutName: 'Serbian (Cyrillic) keyboard', locales: ['sr-Cyrl-RS', 'sr-Cyrl-BA', 'sr-Cyrl-ME'] },
-  '0000046C': { layoutName: 'Sesotho sa Leboa keyboard', locales: ['nso-ZA'] },
-  '00000432': { layoutName: 'Setswana keyboard', locales: ['tn-ZA', 'tn-BW'] },
-  '0000045B': { layoutName: 'Sinhala keyboard', locales: ['si-LK'] },
-  '0000041B': { layoutName: 'Slovak keyboard', locales: ['sk-SK'] },
-  '0000045A': { layoutName: 'Syriac keyboard', locales: ['syr-SY'] },
-  '00000428': { layoutName: 'Tajik keyboard', locales: ['tg-Cyrl-TJ'] },
-  '00020449': { layoutName: 'Tamil 99 keyboard', locales: ['ta-IN', 'ta-LK'] },
-  '00010444': { layoutName: 'Tatar keyboard', locales: ['tt-RU'] },
-  '0000044A': { layoutName: 'Telugu keyboard', locales: ['te-IN'] },
-  '0000041E': { layoutName: 'Thai Kedmanee keyboard', locales: ['th-TH'] },
-  '00010451': { layoutName: 'Tibetan (PRC) - Updated keyboard', locales: ['bo-CN'] },
-  '00000451': { layoutName: 'Tibetan (PRC) keyboard', locales: [] },
-  '0000041F': { layoutName: 'Turkish Q keyboard', locales: ['tr-TR'] },
-  '00000442': { layoutName: 'Turkmen keyboard', locales: ['tk-TM'] },
-  '00020422': { layoutName: 'Ukrainian (Enhanced) keyboard', locales: ['uk-UA'] },
-  '00010480': { layoutName: 'Uyghur keyboard', locales: ['ug-CN'] },
-  '00000843': { layoutName: 'Uzbek Cyrillic keyboard', locales: ['uz-Cyrl-UZ'] },
-  '00000452': { layoutName: 'United Kingdom Extended keyboard', locales: ['cy-GB'] },
-  '0000046A': { layoutName: 'Yoruba keyboard', locales: ['yo-NG'] },
-};
 
 const copyrightHeader =
 `/**
@@ -437,10 +317,31 @@ function stringifyKeyDefinition(def) {
   return `{ ${propStrs.join(', ')} }`;
 }
 
+/**
+ * @return {{ klid: string, layoutName: string, codes: string[],  }[]}
+ */
+function getKeyboardLayoutsFromMarkdown() {
+  const params = md.parse(fs.readFileSync(paramsMarkdownFilename).toString());
+  const mdKls = params.find(m => m.text === 'template-keyboard-layouts');
+  assert(mdKls);
+  // markdown table row:
+  // | `<code-1>`, ..., `<code-n>` | [<name>](<url>) <!-- <klid> --> |
+  return mdKls.children
+      ?.map(m => m.text && /^\s*\|\s*`(.+)`\s*\|\s*\[(.+?)\]\(.*\)\s*<!--\s*([0-9A-F]+)\s*-->\s*\|\s*$/i.exec(m.text)?.slice(1))
+      .filter(Boolean)
+      // @ts-ignore
+      .map(([codesStr, layoutName, klid]) => ({ klid, layoutName, codes: codesStr.split(/`\s*,\s*`/) })) ?? [];
+}
+
 const keyboardsDir = path.resolve(__dirname, '../packages/playwright-core/src/server/keyboards');
+const paramsMarkdownFilename = path.resolve(__dirname, '../docs/src/api/params.md');
 
 (async () => {;
-  for (const [klid, { layoutName }] of Object.entries(layoutUrls)) {
+  const layouts = getKeyboardLayoutsFromMarkdown();
+
+  const klid2LayoutName = new Map(layouts.map(({ klid, layoutName }) => ([ klid, layoutName ])));
+
+  for (const [klid, layoutName] of klid2LayoutName.entries()) {
     console.log(`Generating keyboard layout for ${layoutName} (KLID ${klid})`);
     const layout = await generate(klid);
 
@@ -460,8 +361,8 @@ const keyboardsDir = path.resolve(__dirname, '../packages/playwright-core/src/se
     fs.writeFileSync(path.resolve(keyboardsDir, 'layouts', `${klid}.ts`), layoutData, 'utf-8');
   }
 
-  const mapEntries = Object.entries(layoutUrls)
-      .flatMap(([klid, { layoutName, locales }]) => locales.map(locale => `  ['${locale.replace(/-/g, '_').toLowerCase()}', '${klid}'], // ${layoutName}`));
+  const codesMapping = layouts
+      .flatMap(({ klid, layoutName, codes }) => codes.map(code => `  ['${code.replace(/-/g, '_').toLowerCase()}', '${klid}'], // ${layoutName}`));
 
   const index = [
     copyrightHeader,
@@ -473,7 +374,7 @@ const keyboardsDir = path.resolve(__dirname, '../packages/playwright-core/src/se
     `export const defaultKeyboardLayout: KeyboardLayout = defaultKeyboardLayoutObject;`,
     ``,
     `export const localeMapping = new Map<string, string>([`,
-    ...mapEntries,
+    ...codesMapping,
     `]);`,
     ``,
     `export const keypadLocation = 3;`,
