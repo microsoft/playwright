275e6ec1ab50b8f27ae43effcbc8cbcda78ec9c6
Pavel Feldman <pavel.feldman@gmail.com>
2023-08-02 18:07:30 -0700

    chore: allow web server w/o waiting

--- packages/playwright-test/types/test.d.ts
+++ packages/playwright-test/types/test.d.ts
@@ -6735,15 +6735,15 @@ interface TestConfigWebServer {
   command: string;
 
   /**
-   * The port that your http server is expected to appear on. It does wait until it accepts connections. Exactly one of
-   * `port` or `url` is required.
+   * The port that your http server is expected to appear on. It does wait until it accepts connections. Either `port`
+   * or `url` should be specified.
    */
   port?: number;
 
   /**
    * The url on your http server that is expected to return a 2xx, 3xx, 400, 401, 402, or 403 status code when the
    * server is ready to accept connections. Redirects (3xx status codes) are being followed and the new location is
-   * checked. Exactly one of `port` or `url` is required.
+   * checked. Either `port` or `url` should be specified.
    */
   url?: string;
 
