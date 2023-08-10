117f8cc088d1abda135fb982087ec9078cb2a621
Andrey Lushnikov <lushnikov@chromium.org>
2023-08-10 09:45:10 -0700

    chore: update browser patches as of July, 1 2023
    Internal commit reference: https://github.com/microsoft/playwright-browsers/commit/158dd8ada7783e4f0494b7d5b93ad39875eb9b7f

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="release"
-BASE_REVISION="7177c1fee2d0ae258241aa9f0f332aae8daf87e7"
+BASE_REVISION="2be4fd198990dc742793fd1ba611888fb640e7f7"
