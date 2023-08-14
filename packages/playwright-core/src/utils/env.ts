de65a8dd1caad5025745976e2343cde9f5a9772e
Colin McDonnell <colinmcd94@gmail.com>
2023-08-13 17:19:13 -0700

    Undo autoformatting

--- packages/playwright-core/src/utils/env.ts
+++ packages/playwright-core/src/utils/env.ts
@@ -16,26 +16,22 @@
 
 export function getFromENV(name: string): string | undefined {
   let value = process.env[name];
-  value =
-    value === undefined
-      ? process.env[`npm_config_${name.toLowerCase()}`]
-      : value;
-  value =
-    value === undefined
-      ? process.env[`npm_package_config_${name.toLowerCase()}`]
-      : value;
+  value = value === undefined ? process.env[`npm_config_${name.toLowerCase()}`] : value;
+  value = value === undefined ?  process.env[`npm_package_config_${name.toLowerCase()}`] : value;
   return value;
 }
 
 export function getAsBooleanFromENV(name: string): boolean {
   const value = getFromENV(name);
-  return !!value && value !== "false" && value !== "0";
+  return !!value && value !== 'false' && value !== '0';
 }
 
 export function getPackageManager() {
-  const env = process.env.npm_config_user_agent || "";
-  if (env.includes("yarn")) return "yarn";
-  if (env.includes("pnpm")) return "pnpm";
-  if (env.includes("bun")) return "bun";
-  return "npm";
+  const env = process.env.npm_config_user_agent || '';
+  if (env.includes('yarn'))
+    return 'yarn';
+  if (env.includes('pnpm'))
+    return 'pnpm';
+if (env.includes("bun")) return "bun";
+    return 'npm';
 }
