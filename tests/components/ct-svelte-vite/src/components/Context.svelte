e2a11bed190e723b10116238da5bd02cbae04f3e
Sander <info@mesander.com>
2023-08-20 01:26:06 +0200

    feat(ct): svelte vite context (#26554)

---
+++ tests/components/ct-svelte-vite/src/components/Context.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+import { getContext } from 'svelte';
+const contextValue = getContext('context-key');
+</script>
+
+<div>{contextValue}</div>
--- tests/components/ct-svelte-vite/tests/render.spec.ts
+++ tests/components/ct-svelte-vite/tests/render.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/experimental-ct-svelte';
+import type { HooksConfig } from 'playwright';
 import Button from '@/components/Button.svelte';
 import Empty from '@/components/Empty.svelte';
+import Context from '@/components/Context.svelte';
 
 test('render props', async ({ mount }) => {
   const component = await mount(Button, {
@@ -16,4 +18,13 @@ test('get textContent of the empty component', async ({ mount }) => {
   expect(await component.allTextContents()).toEqual(['']);
   expect(await component.textContent()).toBe('');
   await expect(component).toHaveText('');
+});
+
+test('render context', async ({ mount }) => {
+  const component = await mount<HooksConfig>(Context, {
+    hooksConfig: {
+      context: 'context-value',
+    }
+  });
+  await expect(component).toContainText('context-value');
 });
