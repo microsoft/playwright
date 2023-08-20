4f248e74203998e8fa0b6523cb9b11220c18a96d
Max Schmitt <max@schmitt.mx>
2023-08-20 10:17:50 +0200

    chore: use SVG icons for web apps

--- packages/trace-viewer/src/ui/uiModeView.tsx
+++ packages/trace-viewer/src/ui/uiModeView.tsx
@@ -188,7 +188,7 @@ export const UIModeView: React.FC<{}> = ({
       </div>
       <div className='vbox ui-mode-sidebar'>
         <Toolbar noShadow={true} noMinHeight={true}>
-          <img src='icon-32x32.png' />
+          <img src='playwright-logo.svg' />
           <div className='section-title'>Playwright</div>
           <ToolbarButton icon='color-mode' title='Toggle color mode' onClick={() => toggleTheme()} />
           <ToolbarButton icon='refresh' title='Reload' onClick={() => reloadTests()} disabled={isRunningTest || isLoading}></ToolbarButton>
