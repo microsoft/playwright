7892d2d3322a5ce7b4235f79c2b14e2e0e505114
Yury Semikhatsky <yurys@chromium.org>
2023-08-24 14:12:13 -0700

    Simplify html-reporter reports

--- packages/html-reporter/playwright.config.ts
+++ packages/html-reporter/playwright.config.ts
@@ -15,24 +15,13 @@
  */
 
 import { devices, defineConfig } from '@playwright/experimental-ct-react';
-import type { ReporterDescription } from '@playwright/test';
-
-const reporters = () => {
-  const result: ReporterDescription[] = process.env.CI ? [
-    ['html'],
-    ['blob'],
-  ] : [
-    ['html']
-  ];
-  return result;
-};
 
 export default defineConfig({
   testDir: 'src',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   snapshotPathTemplate: '{testDir}/__screenshots__/{projectName}/{testFilePath}/{arg}{ext}',
-  reporter: reporters(),
+  reporter: process.env.CI ? 'blob' : 'html',
   use: {
     ctPort: 3101,
     trace: 'on-first-retry',
