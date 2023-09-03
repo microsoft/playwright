f219f9fd41e7913de29d5c9f6201d785194acd1f
Ivo Berger <16694197+ivoberger@users.noreply.github.com>
2023-09-03 09:57:09 +0200

    devops: fix tags on jammy stable images (#26834)

--- utils/docker/publish_docker.sh
+++ utils/docker/publish_docker.sh
@@ -49,8 +49,8 @@ JAMMY_TAGS=(
 )
 
 if [[ "$RELEASE_CHANNEL" == "stable" ]]; then
-  FOCAL_TAGS+=("latest")
-  FOCAL_TAGS+=("jammy")
+  JAMMY_TAGS+=("latest")
+  JAMMY_TAGS+=("jammy")
 fi
 
 tag_and_push() {
