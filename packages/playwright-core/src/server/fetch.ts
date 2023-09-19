88038f1b00af1eae7a70890d1945a4fb41e52249
Yury Semikhatsky <yurys@chromium.org>
2023-09-19 16:18:16 -0700

    fix: do not throw from fetch when response has invalid cookie (#27192)
    Cookie value is limited by 4096 characters in the browsers. If
    setCookies failed we try setting each cookie individually just in case
    only some of them are bad.
    
    Fixes https://github.com/microsoft/playwright/issues/27165

--- packages/playwright-core/src/server/fetch.ts
+++ packages/playwright-core/src/server/fetch.ts
@@ -282,8 +282,15 @@ export abstract class APIRequestContext extends SdkObject {
           progress.log(`  ${name}: ${value}`);
 
         const cookies = this._parseSetCookieHeader(response.url || url.toString(), response.headers['set-cookie']) ;
-        if (cookies.length)
-          await this._addCookies(cookies);
+        if (cookies.length) {
+          try {
+            await this._addCookies(cookies);
+          } catch (e) {
+            // Cookie value is limited by 4096 characters in the browsers. If setCookies failed,
+            // we try setting each cookie individually just in case only some of them are bad.
+            await Promise.all(cookies.map(c => this._addCookies([c]).catch(() => {})));
+          }
+        }
 
         if (redirectStatus.includes(response.statusCode!) && options.maxRedirects >= 0) {
           if (!options.maxRedirects) {
