86303aec9a6e7427681197a61c1e20681fe9c250
Yury Semikhatsky <yurys@chromium.org>
2023-10-02 13:38:41 -0700

    Revert curl options

--- utils/linux-browser-dependencies/inside_docker/process.sh
+++ utils/linux-browser-dependencies/inside_docker/process.sh
@@ -9,7 +9,7 @@ export TZ=America/Los_Angeles
 apt-get update && \
     apt-get install -y --no-install-recommends ca-certificates curl gnupg && \
     mkdir -p /etc/apt/keyrings && \
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg && \
+    curl -sL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg && \
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" >> /etc/apt/sources.list.d/nodesource.list && \
     apt-get update && \
     apt-get install -y --no-install-recommends nodejs
