2aca7fdcc2ec07daaabda888c03c68982fc42b32
Dmitry Gozman <dgozman@gmail.com>
2023-10-02 17:21:06 -0700

    fix(chromium): scroll into view elements inside iframes before waiting (#27394)
    This forces iframes to be visible, so that `rAF`s always run.
    
    Fixes #27196.

--- tests/page/page-click-scroll.spec.ts
+++ tests/page/page-click-scroll.spec.ts
@@ -101,7 +101,6 @@ it('should scroll into view span element', async ({ page, isAndroid }) => {
 it('should scroll into view element in iframe', async ({ page, isAndroid, server, browserName }) => {
   it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/27196' });
   it.fixme(isAndroid);
-  it.fixme(browserName === 'chromium', 'rAFs are paused in cross-origin iframes outside viewport');
   await page.goto(server.EMPTY_PAGE);
   await page.setContent(`
     <div id=big style="height: 10000px;"></div>
