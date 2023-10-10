ebd0ff9422fb24bf7bdd651ae883b66792ecced9
Dmitry Gozman <dgozman@gmail.com>
2023-10-10 16:10:40 -0700

    chore: update browser patches to 7f74cfc1d (#27539)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="release"
-BASE_REVISION="fdbb85992450ee14d23efe08c0dd655eedab0e1d"
+BASE_REVISION="f5bc1abb4f0841558f7531e0c15a7577d23ed21c"
