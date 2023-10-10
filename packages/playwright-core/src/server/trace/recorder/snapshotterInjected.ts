2b21c1d38ffe1129e7fb3b3685cb4c37273f1ed8
Martin Schitter <ms+git@mur.at>
2023-10-10 13:07:12 +0200

    remove NoScript hide code (see: #27504)

--- packages/playwright-core/src/server/trace/recorder/snapshotterInjected.ts
+++ packages/playwright-core/src/server/trace/recorder/snapshotterInjected.ts
@@ -81,7 +81,6 @@ export function frameSnapshotStreamer(snapshotStreamer: string) {
   }
 
   class Streamer {
-    private _removeNoScript = true;
     private _lastSnapshotNumber = 0;
     private _staleStyleSheets = new Set<CSSStyleSheet>();
     private _readingStyleSheet = false;  // To avoid invalidating due to our own reads.
@@ -337,8 +336,6 @@ export function frameSnapshotStreamer(snapshotStreamer: string) {
           if (rel === 'preload' || rel === 'prefetch')
             return;
         }
-        if (this._removeNoScript && nodeName === 'NOSCRIPT')
-          return;
         if (nodeName === 'META' && (node as HTMLMetaElement).httpEquiv.toLowerCase() === 'content-security-policy')
           return;
         // Skip iframes which are inside document's head as they are not visible.
