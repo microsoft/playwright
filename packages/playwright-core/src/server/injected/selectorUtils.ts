cffcf356fa9e84676d9962b8da4cc9dddfcb15f5
Martin Schitter <ms+git@mur.at>
2023-10-10 17:43:30 +0200

    revert fd21852 / (text selector): ignore NOSCRIPT elements

--- packages/playwright-core/src/server/injected/selectorUtils.ts
+++ packages/playwright-core/src/server/injected/selectorUtils.ts
@@ -53,7 +53,7 @@ export function matchesAttributePart(value: any, attr: AttributeSelectorPart) {
 
 export function shouldSkipForTextMatching(element: Element | ShadowRoot) {
   const document = element.ownerDocument;
-  return element.nodeName === 'SCRIPT' || element.nodeName === 'NOSCRIPT' || element.nodeName === 'STYLE' || document.head && document.head.contains(element);
+  return element.nodeName === 'SCRIPT' || element.nodeName === 'STYLE' || document.head && document.head.contains(element);
 }
 
 export type ElementText = { full: string, immediate: string[] };
