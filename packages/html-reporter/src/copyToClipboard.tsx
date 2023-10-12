823b104a9e150414dbaca1e2b927cf342cac1e3a
Dmitry Gozman <dgozman@gmail.com>
2023-10-11 17:56:05 -0700

    feat(html): "copy to clipboard" for text attachments (#27556)
    Mostly copied from trace viewer. Not reused due to different
    colors/icons.
    
    Screenshot:
    
    <img width="999" alt="copy-to-clipboard"
    src="https://github.com/microsoft/playwright/assets/9881434/2bb38442-3b8d-42ba-a3ed-4b9052b22854">

---
+++ packages/html-reporter/src/copyToClipboard.tsx
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react';
+import * as icons from './icons';
+import './copyToClipboard.css';
+
+export const CopyToClipboard: React.FunctionComponent<{
+  value: string,
+}> = ({ value }) => {
+  type IconType = 'copy' | 'check' | 'cross';
+  const [icon, setIcon] = React.useState<IconType>('copy');
+  const handleCopy = React.useCallback(() => {
+    navigator.clipboard.writeText(value).then(() => {
+      setIcon('check');
+      setTimeout(() => {
+        setIcon('copy');
+      }, 3000);
+    }, () => {
+      setIcon('cross');
+    });
+  }, [value]);
+  const iconElement = icon === 'check' ? icons.check() : icon === 'cross' ? icons.cross() : icons.copy();
+  return <button className='copy-icon' onClick={handleCopy}>{iconElement}</button>;
+};
