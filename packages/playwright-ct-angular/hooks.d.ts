7a7347aa1f7baae0e32c9aa7da7b6311889fdc8c
sand4rt <info@mesander.com>
2023-10-24 22:29:29 +0200

    feat(ct): angular component testing

---
+++ packages/playwright-ct-angular/hooks.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { TestBedStatic } from '@angular/core/testing';
+import type { JsonObject } from '@playwright/experimental-ct-core/types/component';
+
+export declare function beforeMount<HooksConfig extends JsonObject>(
+  callback: (params: { hooksConfig?: HooksConfig, TestBed: TestBedStatic }) => Promise<void>
+): void;
+export declare function afterMount<HooksConfig extends JsonObject>(
+  callback: (params: { hooksConfig?: HooksConfig }) => Promise<void>
+): void;
---
+++ tests/components/ct-angular/playwright/index.ts
@@ -0,0 +1,31 @@
+import { beforeMount, afterMount } from '@playwright/experimental-ct-angular/hooks';
+import { provideRouter } from '@angular/router';
+import { ButtonComponent } from '@/components/button.component';
+import { TOKEN } from '@/components/inject.component';
+import { routes } from '@/router';
+import '@/assets/styles.css';
+
+export type HooksConfig = {
+  routing?: boolean;
+  injectToken?: boolean;
+};
+
+beforeMount<HooksConfig>(async ({ hooksConfig, TestBed }) => {
+  TestBed.configureTestingModule({
+    imports: [ButtonComponent],
+  });
+
+  if (hooksConfig?.routing)
+    TestBed.configureTestingModule({
+      providers: [provideRouter(routes)],
+    });
+
+  if (hooksConfig?.injectToken)
+    TestBed.configureTestingModule({
+      providers: [{ provide: TOKEN, useValue: { text: 'has been overwritten' }}]
+    })
+});
+
+afterMount<HooksConfig>(async () => {
+  console.log('After mount');
+});
