38a0921845f82443d3bf849da57a69b5b78a664d
Max Schmitt <max@schmitt.mx>
2023-10-31 16:49:13 +0100

    fix: Google Chrome Beta/Stable install on macOS (#27880)

--- packages/playwright-core/bin/reinstall_chrome_stable_mac.sh
+++ packages/playwright-core/bin/reinstall_chrome_stable_mac.sh
@@ -6,7 +6,7 @@ rm -rf "/Applications/Google Chrome.app"
 cd /tmp
 curl -o ./googlechrome.dmg -k https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg
 hdiutil attach -nobrowse -quiet -noautofsck -noautoopen -mountpoint /Volumes/googlechrome.dmg ./googlechrome.dmg
-cp -rf "/Volumes/googlechrome.dmg/Google Chrome.app" /Applications
+cp -pR "/Volumes/googlechrome.dmg/Google Chrome.app" /Applications
 hdiutil detach /Volumes/googlechrome.dmg
 rm -rf /tmp/googlechrome.dmg
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version
