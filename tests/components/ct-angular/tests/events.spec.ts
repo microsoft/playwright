614aed9194346651a6195cda7e6b28683df632d9
Younes Jaaidi <yjaaidi@gmail.com>
2023-11-13 22:42:16 +0100

    test(ct): test non-event-emitter outputs

--- tests/components/ct-angular/tests/events.spec.ts
+++ tests/components/ct-angular/tests/events.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/experimental-ct-angular';
 import { ButtonComponent } from '@/components/button.component';
+import { OutputComponent } from '@/components/output.component';
 
 test('emit an submit event when the button is clicked', async ({ mount }) => {
   const messages: string[] = [];
@@ -13,4 +14,23 @@ test('emit an submit event when the button is clicked', async ({ mount }) => {
   });
   await component.click();
   expect(messages).toEqual(['hello']);
+});
+
+test('unsubscribe from events when the component is unmounted', async ({
+  mount,
+  page,
+}) => {
+  test.skip(true, '🚧 work in progress');
+  const component = await mount(OutputComponent, {
+    on: {
+      answerChange: () => {},
+    },
+  });
+
+  await component.unmount();
+
+  /* Check that the output observable had been unsubscribed from
+   * as it sets a global variable `hasUnusbscribed` to true
+   * when it detects unsubscription. Cf. OutputComponent. */
+  expect(await page.evaluate(() => (window as any).hasUnsubscribed)).toBe(true);
 });
