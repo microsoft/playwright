5ca9d8c8a6bfe7879ff63845e6a451f50eb83f85
Yury Semikhatsky <yurys@chromium.org>
2023-11-13 11:36:04 -0800

    Add to details

--- packages/playwright/types/test.d.ts
+++ packages/playwright/types/test.d.ts
@@ -5632,6 +5632,8 @@ interface LocatorAssertions {
    * Ensures the {@link Locator} points to an element that contains the given text. You can use regular expressions for
    * the value as well.
    *
+   * **Details**
+   *
    * When `expected` parameter is a string, Playwright will normalize whitespaces and line breaks both in the actual
    * text and in the expected string before matching. When regular expression is used, the actual text is matched as is.
    *
@@ -6031,6 +6033,8 @@ interface LocatorAssertions {
   /**
    * Ensures the {@link Locator} points to an element with the given text. You can use regular expressions for the value
    * as well.
+   *
+   * **Details**
    *
    * When `expected` parameter is a string, Playwright will normalize whitespaces and line breaks both in the actual
    * text and in the expected string before matching. When regular expression is used, the actual text is matched as is.
