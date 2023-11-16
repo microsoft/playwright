b62987261973aeef5b187fcd7a09ad0b13586e1a
Max Schmitt <max@schmitt.mx>
2023-11-16 13:16:54 +0100

    test: skip Intl.ListFormat test on ubuntu20.04

--- tests/library/capabilities.spec.ts
+++ tests/library/capabilities.spec.ts
@@ -17,6 +17,7 @@
 import os from 'os';
 import url from 'url';
 import { contextTest as it, expect } from '../config/browserTest';
+const { hostPlatform } = require('../../packages/playwright-core/lib/utils') as typeof import('../../packages/playwright-core/src/utils');
 
 it('SharedArrayBuffer should work @smoke', async function({ contextFactory, httpsServer, browserName }) {
   it.fail(browserName === 'webkit', 'no shared array buffer on webkit');
@@ -284,8 +285,9 @@ it('should send no Content-Length header for GET requests with a Content-Type', 
   expect(request.headers['content-length']).toBe(undefined);
 });
 
-it('Intl.ListFormat should work', async ({ page, server }) => {
+it('Intl.ListFormat should work', async ({ page, server, browserName }) => {
   it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/23978' });
+  it.skip(browserName === 'webkit' && hostPlatform.startsWith('ubuntu20.04'), 'libicu is too low and WebKit disables it by default then');
   await page.goto(server.EMPTY_PAGE);
   const formatted = await page.evaluate(() => {
     const data = ['first', 'second', 'third'];
