94d03f1490c712dad7f59ae3070ce1eae2082e50
Max Schmitt <max@schmitt.mx>
2023-12-06 14:22:29 -0800

    chore: mark outofprocess connection not as remote

--- packages/playwright-core/src/inProcessFactory.ts
+++ packages/playwright-core/src/inProcessFactory.ts
@@ -25,6 +25,7 @@ export function createInProcessPlaywright(): PlaywrightAPI {
   const playwright = createPlaywright({ sdkLanguage: (process.env.PW_LANG_NAME as Language | undefined) || 'javascript' });
 
   const clientConnection = new Connection(undefined, undefined);
+  clientConnection.useBufferBinaryFormat();
   const dispatcherConnection = new DispatcherConnection(true /* local */);
 
   // Dispatch synchronously at first.
