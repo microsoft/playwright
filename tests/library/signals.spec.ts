12aa5ffdb2b12e01e84f4b3e93b28122fb005de3
Max Schmitt <max@schmitt.mx>
2023-12-06 13:38:30 -0800

    Update tests/library/signals.spec.ts
    Signed-off-by: Max Schmitt <max@schmitt.mx>

--- tests/library/signals.spec.ts
+++ tests/library/signals.spec.ts
@@ -64,7 +64,7 @@ test.describe('signals', () => {
     const pid = await remoteServer.out('pid');
     process.kill(-pid, 'SIGKILL');
     if (isMac && browserName === 'webkit' && parseInt(os.release(), 10) >= 22 && os.arch() === 'arm64') {
-      // WebKit on mac13 exits differently.
+      // WebKit on newer macOS exits differently.
       expect(await remoteServer.out('exitCode')).toBe('137');
       expect(await remoteServer.out('signal')).toBe('null');
     } else {
