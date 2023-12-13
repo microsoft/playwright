cd68b086d3897cc8dfa2a5bf554e68556af6d35a
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-12-13 08:41:08 -0800

    feat(ct): upgrade to vite-plugin-svelte 3

--- tests/components/ct-svelte-vite/svelte.config.js
+++ tests/components/ct-svelte-vite/svelte.config.js
@@ -1,7 +1,5 @@
-import sveltePreprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: vitePreprocess()
 }
