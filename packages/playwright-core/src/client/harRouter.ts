721d84f17a0e3b906fc1c5dd91d6932073342072
Yury Semikhatsky <yurys@chromium.org>
2024-02-06 10:17:29 -0800

    fix(har): do not throw on -1 status code (#29371)
    Fixes https://github.com/microsoft/playwright/issues/29311

--- packages/playwright-core/src/client/harRouter.ts
+++ packages/playwright-core/src/client/harRouter.ts
@@ -62,6 +62,13 @@ export class HarRouter {
     }
 
     if (response.action === 'fulfill') {
+      // If the response status is -1, the request was canceled or stalled, so we just stall it here.
+      // See https://github.com/microsoft/playwright/issues/29311.
+      // TODO: it'd be better to abort such requests, but then we likely need to respect the timing,
+      // because the request might have been stalled for a long time until the very end of the
+      // test when HAR was recorded but we'd abort it immediately.
+      if (response.status === -1)
+        return;
       await route.fulfill({
         status: response.status,
         headers: Object.fromEntries(response.headers!.map(h => [h.name, h.value])),
