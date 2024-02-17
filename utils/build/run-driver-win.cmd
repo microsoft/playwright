e0ab124e24bc92992dbf9e55a3edde024e0ce3eb
Yury Semikhatsky <yurys@chromium.org>
2024-02-16 19:31:47 -0800

    fix: update path to cli.js in run-driver-win.cmd
    This was missed in https://github.com/microsoft/playwright/pull/29131

--- utils/build/run-driver-win.cmd
+++ utils/build/run-driver-win.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 if not defined PLAYWRIGHT_NODEJS_PATH set PLAYWRIGHT_NODEJS_PATH=%~dp0node.exe
-"%PLAYWRIGHT_NODEJS_PATH%" "%~dp0package\lib\cli\cli.js" %*
+"%PLAYWRIGHT_NODEJS_PATH%" "%~dp0package\cli.js" %*
