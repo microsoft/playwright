3655a9842bf99920b9bb7b597f2ce6174bf0d153
Max Schmitt <max@schmitt.mx>
2024-03-14 18:03:19 +0100

    Update tests/page/page-set-input-files.spec.ts
    Signed-off-by: Max Schmitt <max@schmitt.mx>

--- tests/page/page-set-input-files.spec.ts
+++ tests/page/page-set-input-files.spec.ts
@@ -37,7 +37,7 @@ it('should upload the file', async ({ page, server, asset }) => {
   }, input)).toBe('contents of the file');
 });
 
-it('should upload the file after popup', async ({ page, server, asset, browserName }) => {
+it('should upload a file after popup', async ({ page, server, asset, browserName }) => {
   it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/29923' });
   it.fixme(browserName === 'firefox');
   await page.goto(server.PREFIX + '/input/fileupload.html');
