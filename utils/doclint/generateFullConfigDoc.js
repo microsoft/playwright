30c3115bfef31f392be5a55b02ae6a98176d6940
Yury Semikhatsky <yurys@chromium.org>
2024-03-29 18:39:34 -0700

    fullproject support

--- utils/doclint/generateFullConfigDoc.js
+++ utils/doclint/generateFullConfigDoc.js
@@ -19,34 +19,20 @@
 const path = require('path');
 const fs = require('fs');
 const PROJECT_DIR = path.join(__dirname, '..', '..');
-const md = require('../markdown');
 
-const allowList = [
-  'forbidOnly',
-  'fullyParallel',
-  'globalSetup',
-  'globalTeardown',
-  'globalTimeout',
-  'grep',
-  'grepInvert',
-  'maxFailures',
-  'metadata',
-  'version',
-  'preserveOutput',
-  'projects',
-  'reporter',
-  'reportSlowTests',
-  'rootDir',
-  'quiet',
-  'shard',
-  'updateSnapshots',
-  'workers',
-  'webServer',
-  'configFile',
-];
-const allowedNames = new Set(allowList);
+function generateFullConfigClass(fromClassName, toClassName, allowList) {
+  const allowedNames = new Set(allowList);
 
-const content = fs.readFileSync(path.join(PROJECT_DIR, 'docs/src/test-api/class-testconfig.md')).toString();
+  const content = fs.readFileSync(path.join(PROJECT_DIR, `docs/src/test-api/class-${fromClassName.toLowerCase()}.md`)).toString();
+  let sections = content.split('\n## ');
+  sections = filterAllowedSections(sections, allowedNames);
+  if (allowedNames.size)
+    console.log(`Undocumented properties for ${fromClassName}:\n  ${[...allowedNames].join('\n  ')}`);
+  sections = changeClassName(sections, fromClassName, toClassName);
+  sections = replacePropertyDescriptions(sections, fromClassName);
+  const fullconfig = sections.join('\n## ');
+  fs.writeFileSync(path.join(PROJECT_DIR, `docs/src/test-api/class-${toClassName.toLowerCase()}.md`), fullconfig);
+}
 
 function propertyNameFromSection(section) {
   section = section.split('\n')[0];
@@ -56,32 +42,80 @@ function propertyNameFromSection(section) {
   return match[1];
 }
 
-let sections = content.split('\n## ');
-sections = sections.filter(section => {
-  section = section.split('\n')[0];
-  const name = propertyNameFromSection(section);
-  if (!name)
-    return true;
-  return allowedNames.has(name);
-});
+function filterAllowedSections(sections, allowedNames) {
+  return sections.filter(section => {
+    section = section.split('\n')[0];
+    const name = propertyNameFromSection(section);
+    if (!name)
+      return true;
+    return allowedNames.delete(name);
+  });
+}
 
-// Change class name to FullConfig.
-sections = sections.map(section => {
-  const lines = section.split('\n');
-  lines[0] = lines[0].replace('TestConfig', 'FullConfig');
-  return lines.join('\n');
-});
+function changeClassName(sections, from, to) {
+  return sections.map(section => {
+    const lines = section.split('\n');
+    lines[0] = lines[0].replace(from, to);
+    return lines.join('\n');
+  });
+}
 
-// Replace description.
-sections = sections.map(section => {
-  const parts = section.split('\n\n');
-  section = parts[0];
-  const name = propertyNameFromSection(section);
-  console.log(name)
-  if (!name)
-    return `${section}\n`;
-  return `${section}\n\nSee [\`property: TestConfig.${name}\`].\n`;
-});
+function replacePropertyDescriptions(sections, configClassName) {
+  return sections.map(section => {
+    const parts = section.split('\n\n');
+    section = parts[0];
+    const name = propertyNameFromSection(section);
+    if (!name)
+      return `${section}\n`;
+    return `${section}\n\nSee [\`property: ${configClassName}.${name}\`].\n`;
+  });
+}
 
-const fullconfig = sections.join('\n## ');
-fs.writeFileSync(path.join(PROJECT_DIR, 'docs/src/test-api/class-fullconfig.md'), fullconfig);
+function generateFullConfig() {
+  generateFullConfigClass('TestConfig', 'FullConfig', [
+    'forbidOnly',
+    'fullyParallel',
+    'globalSetup',
+    'globalTeardown',
+    'globalTimeout',
+    'grep',
+    'grepInvert',
+    'maxFailures',
+    'metadata',
+    'version',
+    'preserveOutput',
+    'projects',
+    'reporter',
+    'reportSlowTests',
+    'rootDir',
+    'quiet',
+    'shard',
+    'updateSnapshots',
+    'workers',
+    'webServer',
+    'configFile',
+  ]);
+}
+
+function generateFullProject() {
+  generateFullConfigClass('TestProject', 'FullProject', [
+    'grep',
+    'grepInvert',
+    'metadata',
+    'name',
+    'dependencies',
+    'snapshotDir',
+    'outputDir',
+    'repeatEach',
+    'retries',
+    'teardown',
+    'testDir',
+    'testIgnore',
+    'testMatch',
+    'timeout',
+    'use',
+  ]);
+}
+
+generateFullConfig();
+generateFullProject();
