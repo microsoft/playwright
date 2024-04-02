eb18477c6ba42098a3f35359b4c9af6b309a1a89
Yury Semikhatsky <yurys@chromium.org>
2024-04-02 14:44:13 -0700

    globalsetup paramater

--- tests/playwright-test/global-setup.spec.ts
+++ tests/playwright-test/global-setup.spec.ts
@@ -280,8 +280,8 @@ const authFiles = {
     export default config;
   `,
   'auth.ts': `
-    import { chromium, FullConfig } from '@playwright/test';
-    async function globalSetup(config: FullConfig) {
+    import { chromium, ConfigInWorker } from '@playwright/test';
+    async function globalSetup(config: ConfigInWorker) {
       const { baseURL, storageState } = config.projects[0].use;
       const browser = await chromium.launch();
       const page = await browser.newPage();
