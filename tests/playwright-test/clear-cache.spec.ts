a6827772a5f210ca5b760586e0b685ef0054cfbb
Dmitry Gozman <dgozman@gmail.com>
2024-04-06 14:25:57 -0700

    fix(esmLoader): unref MessagePort in the main thread (#30271)
    This prevents the process from not exiting forever due to an open port.

---
+++ tests/playwright-test/clear-cache.spec.ts
@@ -0,0 +1,51 @@
+/**
+ * Copyright Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { test, expect } from './playwright-test-fixtures';
+import path from 'path';
+
+export const ctReactCliEntrypoint = path.join(__dirname, '../../packages/playwright-ct-react/cli.js');
+
+test('should clear cache with type:module', async ({ runCLICommand }) => {
+  const result = await runCLICommand({
+    'playwright.config.ts': `
+      import { defineConfig } from '@playwright/test';
+      export default defineConfig({});
+    `,
+    'pacakge.json': `
+      { "type": "module" }
+    `,
+    'a.spec.ts': `
+      import { test } from '@playwright/test';
+      test('example', () => {});
+    `,
+  }, 'clear-cache');
+  expect(result.exitCode).toBe(0);
+});
+
+test('should clear cache for ct', async ({ runCLICommand }) => {
+  const result = await runCLICommand({
+    'playwright.config.ts': `
+      import { defineConfig } from '@playwright/test';
+      export default defineConfig({});
+    `,
+    'a.spec.ts': `
+      import { test } from '@playwright/test';
+      test('example', () => {});
+    `,
+  }, 'clear-cache', [], ctReactCliEntrypoint);
+  expect(result.exitCode).toBe(0);
+});
