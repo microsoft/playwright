6523eb0dd611e07954d6aeda33549d455c29d3e0
Yury Semikhatsky <yurys@chromium.org>
2024-04-23 15:36:34 -0700

    Update expectation

--- tests/playwright-test/reporter-base.spec.ts
+++ tests/playwright-test/reporter-base.spec.ts
@@ -232,7 +232,7 @@ for (const useIntermediateMergeReport of [false, true] as const) {
       expect(result.passed).toBe(1);
       expect(result.interrupted).toBe(1);
       expect(result.didNotRun).toBe(1);
-      expect(result.output).toContain('Timed out waiting 1s for the test suite to run');
+      expect(result.output).toContain('Timed out waiting 3s for the test suite to run');
     });
 
     test('should not print slow parallel tests', async ({ runInlineTest }) => {
