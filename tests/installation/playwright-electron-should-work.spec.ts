bdc077c38f9f3bedcc598cf38530e04be5d60f71
Max Schmitt (from Dev Box) <maxschmitt@microsoft.com>
2024-05-15 12:05:50 +0200

    fix(electron): allow launching with spaces in path

--- tests/installation/playwright-electron-should-work.spec.ts
+++ tests/installation/playwright-electron-should-work.spec.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 import { test } from './npmTest';
+import fs from 'fs';
+import path from 'path';
 
 test('electron should work', async ({ exec, tsc, writeFiles }) => {
   await exec('npm i playwright electron@19.0.11');
@@ -23,4 +25,17 @@ test('electron should work', async ({ exec, tsc, writeFiles }) => {
       `import { Page, _electron, ElectronApplication, Electron } from 'playwright';`
   });
   await tsc('test.ts');
+});
+
+test('electron should work with special characters in path', async ({ exec, tmpWorkspace }) => {
+  test.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/30755' });
+  const folderName = path.join(tmpWorkspace, '!@#$% тест with spaces and 😊');
+
+  await exec('npm i playwright electron@19.0.11');
+  await fs.promises.mkdir(folderName);
+  for (const file of ['electron-app.js', 'sanity-electron.js'])
+    await fs.promises.copyFile(path.join(tmpWorkspace, file), path.join(folderName, file));
+  await exec('node sanity-electron.js', {
+    cwd: path.join(folderName)
+  });
 });
