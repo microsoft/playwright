2cbd7b78ea50ae6a223b92e16e18148277bc3526
Yury Semikhatsky <yurys@chromium.org>
2024-05-23 14:06:59 -0700

    chore: change expect.getState() return type to unknown (#30989)
    Eventually we would like to remove this method altogether.

--- packages/playwright/types/test.d.ts
+++ packages/playwright/types/test.d.ts
@@ -6556,7 +6556,7 @@ export type Expect<ExtendedMatchers = {}> = {
     timeout?: number,
     soft?: boolean,
   }) => Expect<ExtendedMatchers>;
-  getState(): ExpectMatcherState;
+  getState(): unknown;
   not: Omit<AsymmetricMatchers, 'any' | 'anything'>;
 } & AsymmetricMatchers;
 
