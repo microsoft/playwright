f8f648732a005d4bf7c44bb6582236358324a298
Max Schmitt <max@schmitt.mx>
2024-06-18 18:23:04 +0200

    upload to CDN

--- utils/build_android_driver.sh
+++ utils/build_android_driver.sh
@@ -11,5 +11,12 @@ if [ "$?" -ne "0" ]; then
 fi
 
 # These should be uploaded to the CDN
-# cp packages/playwright-core/src/server/android/driver/app/build/outputs/apk/debug/app-debug.apk ./bin/android-driver-target.apk
-# cp packages/playwright-core/src/server/android/driver/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk ./bin/android-driver.apk
+mkdir -p for-cdn
+cp packages/playwright-core/src/server/android/driver/app/build/outputs/apk/debug/app-debug.apk ./for-cdn/android-driver-target.apk
+cp packages/playwright-core/src/server/android/driver/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk ./for-cdn/android-driver.apk
+cd for-cdn
+zip /tmp/android.zip *.apk
+cd ..
+rm -r for-cdn
+echo "Android driver APKs are in /tmp/android.zip. Upload them to the CDN."
+
