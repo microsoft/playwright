c7f7928ce6ace0ac3fa1b979193deccd493211a8
alvaromartmart <alvaro.mart.mart+dev@gmail.com>
2024-06-23 22:53:41 +0200

    tweak: temporarily declare local isTextualMimeType until import is fixes.
    This commit will likely be reverted

--- packages/trace-viewer/src/ui/attachmentsTab.tsx
+++ packages/trace-viewer/src/ui/attachmentsTab.tsx
@@ -20,7 +20,14 @@ import { ImageDiffView } from '@web/shared/imageDiffView';
 import type { MultiTraceModel } from './modelUtil';
 import { PlaceholderPanel } from './placeholderPanel';
 import type { AfterActionTraceEventAttachment } from '@trace/trace';
-import { isTextualMimeType } from 'playwright-core/lib/utils';
+
+// TODO: remove local function and restore import
+// import { isTextualMimeType } from 'playwright-core/lib/utils';
+
+function isTextualMimeType(mimeType: string) {
+  return !!mimeType.match(/^(text\/.*?|application\/(json|(x-)?javascript|xml.*?|ecmascript|graphql|x-www-form-urlencoded)|image\/svg(\+xml)?|application\/.*?(\+json|\+xml))(;\s*charset=.*)?$/);
+}
+
 
 type Attachment = AfterActionTraceEventAttachment & { traceUrl: string };
 
