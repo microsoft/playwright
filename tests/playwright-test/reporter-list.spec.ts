2dacfec182d7cded382b34c4ac3d30044a8ee6d8
Simon Knott <info@simonknott.de>
2024-07-16 17:01:11 +0200

    fix(list reporter): remove last line

--- tests/playwright-test/reporter-list.spec.ts
+++ tests/playwright-test/reporter-list.spec.ts
@@ -144,6 +144,7 @@ for (const useIntermediateMergeReport of [false, true] as const) {
       }, { reporter: 'list' }, { PW_TEST_DEBUG_REPORTERS: '1', PLAYWRIGHT_LIST_PRINT_STEPS: '1' });
       const text = result.output;
       const lines = text.split('\n').filter(l => l.match(/^\d :/)).map(l => l.replace(/[.\d]+m?s/, 'Xms'));
+      lines.pop(); // Remove last item that contains [v] and time in ms.
       expect(lines).toEqual([
         '0 :      1.1 a.test.ts:5:26 › passes › outer 1.0 › inner 1.1 (Xms)',
         '1 :      1.2 a.test.ts:6:26 › passes › outer 1.0 › inner 1.2 (Xms)',
@@ -151,7 +152,6 @@ for (const useIntermediateMergeReport of [false, true] as const) {
         '3 :      1.4 a.test.ts:9:26 › passes › outer 2.0 › inner 2.1 (Xms)',
         '4 :      1.5 a.test.ts:10:26 › passes › outer 2.0 › inner 2.2 (Xms)',
         '5 :      1.6 a.test.ts:8:24 › passes › outer 2.0 (Xms)',
-        '6 :   ✓  1 a.test.ts:3:15 › passes (Xms)',
       ]);
     });
 
