cc78c53093831ee965f8c11f0e7b03272477b4ec
Simon Knott <info@simonknott.de>
2024-07-31 16:13:52 +0200

    use crash helper

--- tests/library/page-event-crash.spec.ts
+++ tests/library/page-event-crash.spec.ts
@@ -80,10 +80,10 @@ test('should be able to close context when page crashes', async ({ isAndroid, is
   await page.context().close();
 });
 
-test('should not hang when page crashed', async ({ page }) => {
+test('should not hang when page crashed', async ({ page, crash }) => {
   test.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/31907' });
 
   const expectPromise = expect(page.getByText('child')).toBeVisible();
-  await page.goto('chrome://crash').catch(e => {});
+  crash();
   await expect(expectPromise).rejects.toThrowError();
 });
