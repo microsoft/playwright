51f92a08ecd453b0e7df808c4afb18696cf5b78d
Yury Semikhatsky <yurys@chromium.org>
2024-08-01 14:14:10 -0700

    cherry-pick(#31970): fix(trace): do not place expect into unfinished api calls based on time
    Fixes https://github.com/microsoft/playwright/issues/31959

--- packages/playwright/src/worker/testInfo.ts
+++ packages/playwright/src/worker/testInfo.ts
@@ -33,7 +33,7 @@ export interface TestStepInternal {
   complete(result: { error?: Error, attachments?: Attachment[] }): void;
   stepId: string;
   title: string;
-  category: 'hook' | 'fixture' | 'test.step' | 'expect' | string;
+  category: 'hook' | 'fixture' | 'test.step' | 'expect' | 'attach' | string;
   location?: Location;
   boxedStack?: StackFrame[];
   steps: TestStepInternal[];
@@ -44,6 +44,9 @@ export interface TestStepInternal {
   infectParentStepsWithError?: boolean;
   box?: boolean;
   isStage?: boolean;
+  // TODO: this сould be decided based on the category, but pw:api
+  // is from a different abstraction layer.
+  canNestByTime?: boolean;
 }
 
 export type TestStage = {
@@ -252,7 +255,7 @@ export class TestInfoImpl implements TestInfo {
       parentStep = this._findLastStageStep();
     } else {
       parentStep = zones.zoneData<TestStepInternal>('stepZone');
-      if (!parentStep && data.category !== 'test.step') {
+      if (!parentStep && data.canNestByTime) {
         // API steps (but not test.step calls) can be nested by time, instead of by stack.
         // However, do not nest chains of route.continue by checking the title.
         parentStep = this._findLastNonFinishedStep(step => step.title !== data.title);
