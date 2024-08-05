09e0f33186980413979760aa738c2f8feb0f0a28
Dmitry Gozman <dgozman@gmail.com>
2024-08-05 14:34:52 +0100

    test: unflake various tests

--- tests/playwright-test/ui-mode-test-screencast.spec.ts
+++ tests/playwright-test/ui-mode-test-screencast.spec.ts
@@ -21,13 +21,15 @@ test.describe.configure({ mode: 'parallel', retries });
 test('should show screenshots', async ({ runUITest }) => {
   const { page } = await runUITest({
     'a.test.ts': `
-      import { test } from '@playwright/test';
+      import { test, expect } from '@playwright/test';
       test('test 1', async ({ page }) => {
         await page.setContent('<div style="background: red; width: 100%; height: 100%"></div>');
+        await expect(page.locator('body')).toBeVisible();
         await page.waitForTimeout(1000);
       });
       test('test 2', async ({ page }) => {
-        await page.setContent('<div style="background: blue; width: 100%; height: 100%"></div>');
+        await page.setContent('<div style="background: blue; width: 100%; height: 100%">hello</div>');
+        await expect(page.locator('body')).toHaveText('hello');
         await page.waitForTimeout(1000);
       });
     `,
@@ -36,14 +38,10 @@ test('should show screenshots', async ({ runUITest }) => {
   await expect(page.getByTestId('status-line')).toHaveText('2/2 passed (100%)');
 
   await page.getByText('test 1', { exact: true }).click();
-  await expect(
-      page.locator('.CodeMirror .source-line-running'),
-  ).toContainText(`test('test 1', async ({ page }) => {`);
+  await expect(page.getByTestId('actions-tree')).toContainText('expect.toBeVisible');
   await expect(page.locator('.film-strip-frame').first()).toBeVisible();
 
   await page.getByText('test 2', { exact: true }).click();
-  await expect(
-      page.locator('.CodeMirror .source-line-running'),
-  ).toContainText(`await page.waitForTimeout(1000);`);
+  await expect(page.getByTestId('actions-tree')).toContainText('expect.toHaveText');
   await expect(page.locator('.film-strip-frame').first()).toBeVisible();
 });
