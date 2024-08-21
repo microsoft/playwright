d5a74950410feb1a4fb4745a1e58573d3ff40819
Dmitry Gozman <dgozman@gmail.com>
2024-08-21 09:46:38 -0700

    feat(addInitScript): support cjs modules when passing both `path` and `arg` (#32240)
    This works with scripts bundled by:
    - `esbuild entrypoint.ts --bundle --format=cjs --outfile=injected.js`
    - webpack with a typical config
      ```js
      module.exports = {
        entry: { 'injected': './entrypoint.js', },
        output: {
          path: require('path').resolve(__dirname),
          filename: '[name].js',
          libraryTarget: 'commonjs2',
        },
      };
      ```

---
+++ tests/assets/injectedmodule.js
@@ -0,0 +1,33 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// one.ts
+var one_exports = {};
+__export(one_exports, {
+  default: () => one_default
+});
+module.exports = __toCommonJS(one_exports);
+
+// two.ts
+var value = 42;
+
+// one.ts
+function one_default(arg) {
+  window.injected = arg ?? value;
+}
