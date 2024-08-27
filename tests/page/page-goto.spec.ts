1077bfabd941754c7f5d9ba975b08ece855283cb
Yury Semikhatsky <yurys@chromium.org>
2024-08-27 13:54:21 -0700

    tests: delete flaky COOP test
    Fixes https://github.com/microsoft/playwright/issues/32107

--- tests/page/page-goto.spec.ts
+++ tests/page/page-goto.spec.ts
@@ -179,27 +179,6 @@ it('should work with Cross-Origin-Opener-Policy after redirect', async ({ page, 
   expect(firstRequest.url()).toBe(server.PREFIX + '/redirect');
 });
 
-it('should properly cancel Cross-Origin-Opener-Policy navigation', {
-  annotation: { type: 'issue', description: 'https://github.com/microsoft/playwright/issues/32107' },
-}, async ({ page, server, browserName, isLinux, headless }) => {
-  it.fixme(browserName === 'webkit' && isLinux, 'Started failing after https://commits.webkit.org/281488@main');
-  it.fixme(browserName === 'chromium' && headless, 'COOP navigation cancels the one that starts later');
-  server.setRoute('/empty.html', (req, res) => {
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.end();
-  });
-  const requestPromise = page.waitForRequest(server.EMPTY_PAGE);
-  page.goto(server.EMPTY_PAGE).catch(() => {});
-  await new Promise(f => setTimeout(f, 50));
-  // Non COOP response.
-  await page.goto(server.CROSS_PROCESS_PREFIX + '/error.html');
-  const req = await requestPromise;
-  const response = await Promise.race([req.response(), new Promise(f => setTimeout(() => f('timeout'), 5_000))]);
-  // First navigation request should either receive response or be canceled by the second
-  // navigation, but never hang unresolved.
-  expect(response).not.toBe('timeout');
-});
-
 it('should capture iframe navigation request', async ({ page, server }) => {
   await page.goto(server.EMPTY_PAGE);
   expect(page.url()).toBe(server.EMPTY_PAGE);
