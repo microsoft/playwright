a9c09bfcb20805c769805ae748dda52a45c0f28b
Simon Knott <info@simonknott.de>
2024-08-28 09:50:42 +0200

    fix(ui): respect --output param

--- tests/playwright-test/ui-mode-test-setup.spec.ts
+++ tests/playwright-test/ui-mode-test-setup.spec.ts
@@ -19,7 +19,7 @@ import path from 'path';
 
 test.describe.configure({ mode: 'parallel', retries });
 
-test('should run global setup and teardown', async ({ runUITest }) => {
+test('should run global setup and teardown', async ({ runUITest }, testInfo) => {
   const { page, testProcess } = await runUITest({
     'playwright.config.ts': `
       import { defineConfig } from '@playwright/test';
@@ -32,13 +32,16 @@ test('should run global setup and teardown', async ({ runUITest }) => {
       export default () => console.log('\\n%%from-global-setup');
     `,
     'globalTeardown.ts': `
-      export default () => console.log('\\n%%from-global-teardown');
+      export default (config) => {
+        console.log('\\n%%from-global-teardown');
+        console.log('%%' + JSON.stringify(config));
+      };
     `,
     'a.test.js': `
       import { test, expect } from '@playwright/test';
       test('should work', async ({}) => {});
     `
-  });
+  }, undefined, { additionalArgs: ['--output=foo'] });
   await page.getByTitle('Run all').click();
   await expect(page.getByTestId('status-line')).toHaveText('1/1 passed (100%)');
 
@@ -46,9 +49,10 @@ test('should run global setup and teardown', async ({ runUITest }) => {
   await expect(page.getByTestId('output')).toContainText('from-global-setup');
   await page.close();
 
-  await expect.poll(() => testProcess.outputLines()).toEqual([
-    'from-global-teardown',
-  ]);
+  await expect.poll(() => testProcess.outputLines()).toContain('from-global-teardown');
+
+  const config = JSON.parse(testProcess.outputLines()[1]);
+  expect(config.projects[0].outputDir).toEqual(testInfo.outputPath('foo'));
 });
 
 test('should teardown on sigint', async ({ runUITest, nodeVersion }) => {
