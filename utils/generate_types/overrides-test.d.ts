565aed6c393dac3ccba6ca98e7627e34842f4d25
Yury Semikhatsky <yurys@chromium.org>
2024-09-03 10:07:08 -0700

    Revert "chore: enforce tags format via typescript types (#32384)" (#32431)
    After API review we decided to revert it:
    * VSCode extension and UI mode users already get the (runtime) error if
    the tag is not prefixed
    * The typescript error message is not very nice
    * The type change would break those clients that generate tests with
    tags passed as string
    
    This reverts commit 90e7b9ebacbd597b7380522001eb6d17ee9c3d86.

--- utils/generate_types/overrides-test.d.ts
+++ utils/generate_types/overrides-test.d.ts
@@ -70,10 +70,8 @@ type TestDetailsAnnotation = {
   description?: string;
 };
 
-type TestDetailsTag = `@${string}`;
-
 export type TestDetails = {
-  tag?: TestDetailsTag | TestDetailsTag[];
+  tag?: string | string[];
   annotation?: TestDetailsAnnotation | TestDetailsAnnotation[];
 }
 
