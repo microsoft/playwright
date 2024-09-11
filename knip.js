1ac22bf67343c0997ec47eb035cc8849b8a9c595
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-11 18:10:51 +0100

    chore(root): ignore knip warning for now, refactoring should be in another PR

--- knip.js
+++ knip.js
@@ -1,10 +1,42 @@
+const packagesToBeIgnored = [
+  "@types/codemirror",
+  "@types/formidable",
+  "@types/immutable",
+  "@types/ws",
+  "@types/xml2js",
+  "@typescript-eslint/utils",
+  "@vitejs/plugin-basic-ssl",
+  "ansi-styles",
+  "colors",
+  "dotenv",
+  "esbuild",
+  "eslint-plugin-internal-playwright",
+  "formidable",
+  "immutable",
+  "license-checker",
+  "mime",
+  "ssim.js",
+  "ws",
+  "xml2js",
+  "@playwright/test",
+];
+
+const binariesToBeIgnored = ["playwright"];
+
 const config = {
   workspaces: {
     ".": {
       //   entry: [""],
-      ignore: [],
-      ignoreDependencies: [],
-      ignoreBinaries: [],
+      ignore: [
+        "packages/**/*",
+        "utils/**/*",
+        ".github/**/*",
+        "tests/**/*",
+        "examples/**/*",
+        "browser_patches/**/*",
+      ],
+      ignoreDependencies: packagesToBeIgnored,
+      ignoreBinaries: binariesToBeIgnored,
     },
   },
 };
