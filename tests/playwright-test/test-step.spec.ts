ef4be6afff499a1f6ca05a79495bcfdcd29d82f4
Simon Knott <info@simonknott.de>
2024-09-12 19:56:38 +0200

    feat(test runner): make `expect.extend` immutable (#32366)
    Changes `expect.extend` behaviour so that it doesn't mutate the global
    instance and behaves closer to what users expect. This is formally a
    breaking change, and I had to remove a test that asserts the breaking
    behaviour.
    
    TODO:
    - [x] decide wether this is a separate method or a flag for
    `expect.extend`
    - [x] figure out if we need to change docs

--- tests/playwright-test/test-step.spec.ts
+++ tests/playwright-test/test-step.spec.ts
@@ -311,7 +311,9 @@ test('should report custom expect steps', async ({ runInlineTest }) => {
       };
     `,
     'a.test.ts': `
-      expect.extend({
+      import { test, expect as baseExpect } from '@playwright/test';
+
+      const expect = baseExpect.extend({
         toBeWithinRange(received, floor, ceiling) {
           const pass = received >= floor && received <= ceiling;
           if (pass) {
@@ -338,7 +340,6 @@ test('should report custom expect steps', async ({ runInlineTest }) => {
         },
       });
 
-      import { test, expect } from '@playwright/test';
       test('fail', async ({}) => {
         expect(15).toBeWithinRange(10, 20);
         await expect(1).toBeFailingAsync(22);
@@ -349,8 +350,8 @@ test('should report custom expect steps', async ({ runInlineTest }) => {
   expect(result.exitCode).toBe(1);
   expect(result.output).toBe(`
 hook      |Before Hooks
-expect    |expect.toBeWithinRange @ a.test.ts:31
-expect    |expect.toBeFailingAsync @ a.test.ts:32
+expect    |expect.toBeWithinRange @ a.test.ts:32
+expect    |expect.toBeFailingAsync @ a.test.ts:33
 expect    |↪ error: Error: It fails!
 hook      |After Hooks
 hook      |Worker Cleanup
