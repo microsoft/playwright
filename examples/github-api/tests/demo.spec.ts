c0c036b4807663b3e63ab2026426e77553593ac9
Simon Knott <info@simonknott.de>
2024-09-14 12:18:09 +0200

    remove demo test

--- examples/github-api/tests/demo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test('should get posts', async ({ request }) => {
-    const posts = await request.get(`https://jsonplaceholder.typicode.com/posts/1`);
-    expect(posts.ok()).toBeTruthy();
-    expect(await posts.text()).toHaveLength(10)
-});
