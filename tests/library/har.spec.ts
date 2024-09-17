f1390cc269f1b6c8028680f79fcd3a197181ef55
Simon Knott <info@simonknott.de>
2024-09-17 19:59:44 +0200

    chore(har recorder): ensure we respect minimal mode (#32658)
    Closes https://github.com/microsoft/playwright/issues/32653.
    
    Adds some test coverage to ensure we respect minimal mode for API
    Requests in HAR tracing.
    
    | omit setting | result |
    | - | - |
    | `omitCookies` |  added test for it |
    | `omitTiming` | already covered |
    | `omitSecurityDetails` | not recorded yet |
    | `omitServerIP` | we don't record it yet, so no action here. gonna open
    a separate issue |
    | `omitPages` | not relevant to API requests |
    | `omitSizes` | added test for it |
    | `omitScripts` | not relevant to API requests |

--- tests/library/har.spec.ts
+++ tests/library/har.spec.ts
@@ -844,6 +844,8 @@ it('should respect minimal mode for API Requests', async ({ contextFactory, serv
   expect(entries).toHaveLength(1);
   const [entry] = entries;
   expect(entry.timings).toEqual({ receive: -1, send: -1, wait: -1 });
+  expect(entry.request.cookies).toEqual([]);
+  expect(entry.request.bodySize).toBe(-1);
 });
 
 it('should include redirects from API request', async ({ contextFactory, server }, testInfo) => {
