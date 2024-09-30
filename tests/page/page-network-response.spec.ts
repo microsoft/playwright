af04498272dc1e43099f6234b1e5bd346d876cd3
Dmitry Gozman <dgozman@gmail.com>
2024-09-30 14:23:06 +0100

    test: unflake two tests
    - the test that closes a context must not be a "page test";
    - account for stray browser requests in the proxy test.

--- tests/page/page-network-response.spec.ts
+++ tests/page/page-network-response.spec.ts
@@ -129,26 +129,6 @@ it('should reject response.finished if page closes', async ({ page, server }) =>
   expect(error.message).toContain('closed');
 });
 
-it('should reject response.finished if context closes', async ({ page, server }) => {
-  await page.goto(server.EMPTY_PAGE);
-  server.setRoute('/get', (req, res) => {
-    // In Firefox, |fetch| will be hanging until it receives |Content-Type| header
-    // from server.
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.write('hello ');
-  });
-  // send request and wait for server response
-  const [pageResponse] = await Promise.all([
-    page.waitForEvent('response'),
-    page.evaluate(() => fetch('./get', { method: 'GET' })),
-  ]);
-
-  const finishPromise = pageResponse.finished().catch(e => e);
-  await page.context().close();
-  const error = await finishPromise;
-  expect(error.message).toContain('closed');
-});
-
 it('should return json', async ({ page, server }) => {
   const response = await page.goto(server.PREFIX + '/simple.json');
   expect(await response.json()).toEqual({ foo: 'bar' });
