16edf2942388997d0cfe3f54a0a5ed17e683de35
Simon Knott <info@simonknott.de>
2024-10-02 14:54:21 +0200

    chore(docs): fix code highlighting

--- utils/markdown.js
+++ utils/markdown.js
@@ -46,6 +46,7 @@
  *    lines: string[],
  *    codeLang: string,
  *    title?: string,
+ *    highlight?: string,
  *  }} MarkdownCodeNode */
 
 /** @typedef {MarkdownBaseNode & {
@@ -165,13 +166,14 @@ function buildTree(lines) {
     // Remaining items respect indent-based nesting.
     const [, indent, content] = /** @type {string[]} */ (line.match('^([ ]*)(.*)'));
     if (content.startsWith('```')) {
-      const [codeLang, title] = parseCodeBlockMetadata(content);
+      const [codeLang, title, highlight] = parseCodeBlockMetadata(content);
       /** @type {MarkdownNode} */
       const node = {
         type: 'code',
         lines: [],
         codeLang,
         title,
+        highlight,
       };
       line = lines[++i];
       while (!line.trim().startsWith('```')) {
@@ -255,14 +257,18 @@ function buildTree(lines) {
 
 /**
  * @param {String} firstLine
- * @returns {[string, string|undefined]}
+ * @returns {[string, string|undefined, string|undefined]}
  */
 function parseCodeBlockMetadata(firstLine) {
   const withoutBackticks = firstLine.substring(3);
-  const match = withoutBackticks.match(/ title="(.+)"$/);
-  if (match)
-    return [withoutBackticks.substring(0, match.index), match[1]];
-  return [withoutBackticks, undefined];
+  const titleMatch = withoutBackticks.match(/ title="(.+)"/);
+  const highlightMatch = withoutBackticks.match(/\{.*\}/);
+
+  let codeLang = withoutBackticks;
+  if (titleMatch || highlightMatch)
+    codeLang = withoutBackticks.substring(0, titleMatch?.index ?? highlightMatch?.index);
+
+  return [codeLang, titleMatch?.[1], highlightMatch?.[0]];
 }
 
 /**
@@ -328,7 +334,7 @@ function innerRenderMdNode(indent, node, lastNode, result, options) {
 
   if (node.type === 'code') {
     newLine();
-    result.push(`${indent}\`\`\`${node.codeLang}${(options?.renderCodeBlockTitlesInHeader && node.title) ? ' title="' + node.title + '"' : ''}`);
+    result.push(`${indent}\`\`\`${node.codeLang}${(options?.renderCodeBlockTitlesInHeader && node.title) ? ' title="' + node.title + '"' : ''}${node.highlight ? ' ' + node.highlight : ''}`);
     if (!options?.renderCodeBlockTitlesInHeader && node.title)
       result.push(`${indent}// ${node.title}`);
     for (const line of node.lines)
