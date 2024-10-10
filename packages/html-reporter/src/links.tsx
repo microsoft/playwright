03082bdb0e5ca07c3e611958d0f15bc4b6a88a6e
Simon Knott <info@simonknott.de>
2024-10-10 12:12:33 +0200

    keep 0 margin as before

--- packages/html-reporter/src/links.tsx
+++ packages/html-reporter/src/links.tsx
@@ -87,7 +87,7 @@ export const AttachmentLink: React.FunctionComponent<{
         : <span>{linkifyText(attachment.name)}</span>
     )}
   </span>} loadChildren={attachment.body ? () => {
-    return [<div key={1} className='attachment-body' style={{ marginLeft: 24 + (depth ?? 0) * 22 + 4 }}><CopyToClipboard value={attachment.body!}/>{linkifyText(attachment.body!)}</div>];
+    return [<div key={1} className='attachment-body' style={{ marginLeft: 24 + (depth ?? 0) * 22 }}><CopyToClipboard value={attachment.body!}/>{linkifyText(attachment.body!)}</div>];
   } : undefined} depth={depth ?? 0} style={{ lineHeight: '32px' }}></TreeItem>;
 };
 
