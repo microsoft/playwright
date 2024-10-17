02d723699e51888af2eadab25ebc9a3f8c72b08a
Simon Knott <info@simonknott.de>
2024-10-17 14:29:14 +0200

    Update tests/playwright-test/global-setup.spec.ts
    Co-authored-by: Dmitry Gozman <dgozman@gmail.com>
    Signed-off-by: Simon Knott <info@simonknott.de>

--- tests/playwright-test/global-setup.spec.ts
+++ tests/playwright-test/global-setup.spec.ts
@@ -422,7 +422,7 @@ test('globalSetup should support multiple', async ({ runInlineTest }) => {
     'globalSetup3Function',
     'globalTeardown2',
     'globalSetup1Function',
-    // 'globalTeardown1' is missing, because globalTeardown1 errored out.
+    // 'globalTeardown1' is missing, because globalSetup1Function errored out.
   ]);
   expect(result.output).toContain('Error: kaboom');
 });
