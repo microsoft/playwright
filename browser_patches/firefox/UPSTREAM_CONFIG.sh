d1ae5f6ac4de84490310b83c0e071a7c13b0ede1
Max Schmitt <max@schmitt.mx>
2024-11-13 12:34:49 +0100

    chore: update browser patches to bb81b1c60 (#33573)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla/gecko-dev"
 BASE_BRANCH="release"
-BASE_REVISION="47bcb6d7d2013f9a3d864678675100e0b3d73c5e"
+BASE_REVISION="bc78b98043438d8ee2727a483b6e10dedfda883f"
