faef88bdc000d3694776a37a5809b9cb19c23670
Dmitry Gozman <dgozman@gmail.com>
2024-11-20 09:37:20 +0000

    docs: note that permissions list may change
    Also remove 'accessibility-events' from the list as it is
    not supported by any browser anymore.

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -8961,9 +8961,13 @@ export interface BrowserContext {
   /**
    * Grants specified permissions to the browser context. Only grants corresponding permissions to the given origin if
    * specified.
-   * @param permissions A permission or an array of permissions to grant. Permissions can be one of the following values:
+   * @param permissions A list of permissions to grant.
+   *
+   * **NOTE** Supported permissions differ between browsers, and even between different versions of the same browser.
+   * Any permission may stop working after an update.
+   *
+   * Here are some permissions that may be supported by some browsers:
    * - `'accelerometer'`
-   * - `'accessibility-events'`
    * - `'ambient-light-sensor'`
    * - `'background-sync'`
    * - `'camera'`
