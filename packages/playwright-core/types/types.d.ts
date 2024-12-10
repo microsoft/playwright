c2bc0008860c24d5c9d31a9c2df7ecee907548b4
Yury Semikhatsky <yurys@chromium.org>
2024-12-10 11:34:05 -0800

    docs: add examples for clock.install() followed by pauseAt()
    Fixes https://github.com/microsoft/playwright/issues/33926

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -18590,6 +18590,19 @@ export interface Clock {
    * await page.clock.pauseAt('2020-02-02');
    * ```
    *
+   * For best results, install the clock before navigating the page and set it to a time slightly before the intended
+   * test time. This ensures that all timers run normally during page loading, preventing the page from getting stuck.
+   * Once the page has fully loaded, you can safely use
+   * [clock.pauseAt(time)](https://playwright.dev/docs/api/class-clock#clock-pause-at) to pause the clock.
+   *
+   * ```js
+   * // Initialize clock with some time before the test time and let the page load
+   * // naturally. `Date.now` will progress as the timers fire.
+   * await page.clock.install({ time: new Date('2024-12-10T08:00:00') });
+   * await page.goto('http://localhost:3333');
+   * await page.clock.pauseAt(new Date('2024-12-10T10:00:00'));
+   * ```
+   *
    * @param time Time to pause at.
    */
   pauseAt(time: number|string|Date): Promise<void>;
