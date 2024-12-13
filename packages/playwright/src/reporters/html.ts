dd36de7809af2ff19e03d4b1c69fc82a283a23ee
Simon Knott <info@simonknott.de>
2024-12-13 12:01:20 +0100

    fix(html): encode all stdio attachments (#33950)

--- packages/playwright/src/reporters/html.ts
+++ packages/playwright/src/reporters/html.ts
@@ -602,17 +602,10 @@ type JsonAttachment = {
 };
 
 function stdioAttachment(chunk: Buffer | string, type: 'stdout' | 'stderr'): JsonAttachment {
-  if (typeof chunk === 'string') {
-    return {
-      name: type,
-      contentType: 'text/plain',
-      body: chunk
-    };
-  }
   return {
     name: type,
-    contentType: 'application/octet-stream',
-    body: chunk
+    contentType: 'text/plain',
+    body: typeof chunk === 'string' ? chunk : chunk.toString('utf-8')
   };
 }
 
