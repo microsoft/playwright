34cb35859aef71fc35f7b8a9dbfe4009c1e64bd5
Henrik Skupin <mail@hskupin.info>
2025-01-22 22:06:24 +0100

    chore(bidi): simplify launcher tests for Firefox (#34405)

--- tests/library/firefox/launcher.spec.ts
+++ tests/library/firefox/launcher.spec.ts
@@ -21,12 +21,12 @@ it('should pass firefox user preferences', async ({ browserType, mode }) => {
   const browser = await browserType.launch({
     firefoxUserPrefs: {
       'network.proxy.type': 1,
-      'network.proxy.http': '127.0.0.1',
-      'network.proxy.http_port': 3333,
+      'network.proxy.ssl': '127.0.0.1',
+      'network.proxy.ssl_port': 3333,
     }
   });
   const page = await browser.newPage();
-  const error = await page.goto('http://example.com').catch(e => e);
+  const error = await page.goto('https://example.com').catch(e => e);
   expect(error.message).toContain('NS_ERROR_PROXY_CONNECTION_REFUSED');
   await browser.close();
 });
@@ -36,10 +36,10 @@ it('should pass firefox user preferences in persistent', async ({ mode, launchPe
   const { page } = await launchPersistent({
     firefoxUserPrefs: {
       'network.proxy.type': 1,
-      'network.proxy.http': '127.0.0.1',
-      'network.proxy.http_port': 3333,
+      'network.proxy.ssl': '127.0.0.1',
+      'network.proxy.ssl_port': 3333,
     }
   });
-  const error = await page.goto('http://example.com').catch(e => e);
+  const error = await page.goto('https://example.com').catch(e => e);
   expect(error.message).toContain('NS_ERROR_PROXY_CONNECTION_REFUSED');
 });
