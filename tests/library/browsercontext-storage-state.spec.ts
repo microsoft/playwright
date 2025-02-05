33d8be42cb7725289ec2b8f5c60912c1e98d871a
Simon Knott <info@simonknott.de>
2025-02-05 10:57:37 +0100

    update other test

--- tests/library/browsercontext-storage-state.spec.ts
+++ tests/library/browsercontext-storage-state.spec.ts
@@ -40,12 +40,14 @@ it('should capture local storage', async ({ contextFactory }) => {
       name: 'name2',
       value: 'value2'
     }],
+    indexedDB: [],
   }, {
     origin: 'https://www.example.com',
     localStorage: [{
       name: 'name1',
       value: 'value1'
     }],
+    indexedDB: [],
   }]);
 });
 
