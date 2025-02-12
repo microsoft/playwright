89ff7fa804efb6073518d1013862a50f8b5fb06c
Simon Knott <info@simonknott.de>
2025-02-12 14:51:57 +0100

    fix lint

--- packages/web/src/components/prompts.ts
+++ packages/web/src/components/prompts.ts
@@ -20,8 +20,10 @@ function stripAnsiEscapes(str: string): string {
 }
 
 function enumerate(items: string[]) {
-  if (items.length === 0) return '';
-  if (items.length === 1) return items[0];
+  if (items.length === 0)
+    return '';
+  if (items.length === 1)
+    return items[0];
   return items.slice(0, -1).join(', ') + ' and ' + items[items.length - 1];
 }
 
