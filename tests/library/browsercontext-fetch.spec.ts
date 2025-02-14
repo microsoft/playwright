9139796fc25ecaa15fb2d8374181321840378a24
Yury Semikhatsky <yurys@chromium.org>
2025-02-14 10:15:22 -0800

    skip cookie order test on wk linux

--- tests/library/browsercontext-fetch.spec.ts
+++ tests/library/browsercontext-fetch.spec.ts
@@ -280,7 +280,8 @@ it('should add cookies from Set-Cookie header', async ({ context, page, server }
   expect((await page.evaluate(() => document.cookie)).split(';').map(s => s.trim()).sort()).toEqual(['foo=bar', 'session=value']);
 });
 
-it('should preserve cookie order from Set-Cookie header', async ({ context, page, server }) => {
+it('should preserve cookie order from Set-Cookie header', async ({ context, page, server, browserName, isLinux }) => {
+  it.fixme(browserName === 'webkit' && isLinux, 'https://github.com/microsoft/playwright-browsers/issues/1512');
   it.info().annotations.push({ type: 'issue', description: 'https://github.com/microsoft/playwright/issues/23390' });
   server.setRoute('/setcookie.html', (req, res) => {
     res.setHeader('Set-Cookie', ['cookie.0=foo', 'cookie.1=bar']);
