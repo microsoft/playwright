945e5a7848f69382c4dee714914acfdd61d52144
Christopher Tangonan <161169629+cTangonan123@users.noreply.github.com>
2025-02-20 22:53:36 +0000

    fix(reporter-json): added config specs for parallelIndex testing
    Co-authored-by: Neel Deshmukh <neel.deshmukh1@gmail.com>
    Co-authored-by: Marcelo Villalobos Diaz <mvillalobosdiaz@csumb.edu>

--- tests/playwright-test/reporter-json.spec.ts
+++ tests/playwright-test/reporter-json.spec.ts
@@ -333,23 +333,19 @@ test('should report parallelIndex', async ({ runInlineTest }, testInfo) => {
     'tests/a.spec.js': `
       import { test, expect } from '@playwright/test';
       const fs = require('fs');
-      test('test 1 passes!', async ({}) => {
-        expect(1 + 1).toBe(2);
-      });
+      test('test 1 passes!', async ({}) => {});
       test('test 2 fails!', async ({}) => {
         expect(1 + 1).toBe(3);
       });
-      test('test 3 passes!', async ({}) => {
-        expect(1 + 1).toBe(2);
-      });
+      test('test 3 passes!', async ({}) => {});
     `
-  });
+  }, { 'workers': '1', 'reporter': 'json' });
   expect(result.passed).toBe(2);
   expect(result.failed).toBe(1);
-  expect(result.results[0].workerIndex).toBe(0);
-  expect(result.results[0].parallelIndex).toBe(0);
-  expect(result.results[1].workerIndex).toBe(0);
-  expect(result.results[1].parallelIndex).toBe(0);
-  expect(result.results[2].workerIndex).toBe(1);
-  expect(result.results[2].parallelIndex).toBe(0);
+  expect(result.report.suites[0].specs[0].tests[0].results[0].workerIndex).toBe(0);
+  expect(result.report.suites[0].specs[1].tests[0].results[0].workerIndex).toBe(0);
+  expect(result.report.suites[0].specs[2].tests[0].results[0].workerIndex).toBe(1);
+  expect(result.report.suites[0].specs[0].tests[0].results[0].parallelIndex).toBe(0);
+  expect(result.report.suites[0].specs[1].tests[0].results[0].parallelIndex).toBe(0);
+  expect(result.report.suites[0].specs[2].tests[0].results[0].parallelIndex).toBe(0);
 });
