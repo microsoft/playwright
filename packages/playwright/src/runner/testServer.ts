afe44ffae0edbd149d80da7df810cae1e203ad3f
Simon Knott <info@simonknott.de>
2025-02-28 09:13:17 +0100

    process.env.ENV=undefined sets it to 'undefined'??

--- packages/playwright/src/runner/testServer.ts
+++ packages/playwright/src/runner/testServer.ts
@@ -323,7 +323,10 @@ export class TestServerDispatcher implements TestServerInterface {
       process.env.PW_LIVE_TRACE_STACKS = '1';
     else
       process.env.PW_LIVE_TRACE_STACKS = undefined;
-    process.env.PW_TEST_PAGE_SNAPSHOT = params.pageSnapshot;
+    if (params.pageSnapshot)
+      process.env.PW_TEST_PAGE_SNAPSHOT = params.pageSnapshot;
+    else
+      delete process.env.PW_TEST_PAGE_SNAPSHOT;
 
     const wireReporter = await this._wireReporter(e => this._dispatchEvent('report', e));
     const config = await this._loadConfigOrReportError(new InternalReporter([wireReporter]), overrides);
