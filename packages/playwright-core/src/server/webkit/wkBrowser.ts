4504353603a691373d409d18dc626788754c0426
Dmitry Gozman <dgozman@gmail.com>
2025-02-28 10:38:04 +0000

    chore: bump webkit version to 18.4
    Follows Safari Technology Preview.

--- packages/playwright-core/src/server/webkit/wkBrowser.ts
+++ packages/playwright-core/src/server/webkit/wkBrowser.ts
@@ -32,8 +32,8 @@ import type { Protocol } from './protocol';
 import type { PageProxyMessageReceivedPayload } from './wkConnection';
 import type * as channels from '@protocol/channels';
 
-const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15';
-const BROWSER_VERSION = '18.2';
+const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15';
+const BROWSER_VERSION = '18.4';
 
 export class WKBrowser extends Browser {
   private readonly _connection: WKConnection;
