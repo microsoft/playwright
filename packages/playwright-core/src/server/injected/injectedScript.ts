5c335ddc43bc4ec43aa5e412e0bc54c3c58df86f
Dmitry Gozman <dgozman@gmail.com>
2025-03-04 13:54:58 +0000

    chore: unblock dotnet roll

--- packages/playwright-core/src/server/injected/injectedScript.ts
+++ packages/playwright-core/src/server/injected/injectedScript.ts
@@ -1422,6 +1422,9 @@ export class InjectedScript {
         received = getAriaRole(element) || '';
       } else if (expression === 'to.have.title') {
         received = this.document.title;
+      } else if (expression === 'to.have.url') {
+        // Note: this is used by all language ports except for javascript.
+        received = this.document.location.href;
       } else if (expression === 'to.have.value') {
         element = this.retarget(element, 'follow-label')!;
         if (element.nodeName !== 'INPUT' && element.nodeName !== 'TEXTAREA' && element.nodeName !== 'SELECT')
