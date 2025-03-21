25ceaae3a84a8df2ae79b1c946b8e67d48e2e68e
Yury Semikhatsky <yurys@chromium.org>
2025-03-21 10:38:36 -0700

    skip on android

--- tests/page/page-request-continue.spec.ts
+++ tests/page/page-request-continue.spec.ts
@@ -767,7 +767,8 @@ it('propagate headers cross origin redirect after interception', {
     { type: 'issue', description: 'https://github.com/microsoft/playwright/issues/32045' },
     { type: 'issue', description: 'https://github.com/microsoft/playwright/issues/35154' },
   ]
-}, async ({ page, server, browserName }) => {
+}, async ({ page, server, browserName, isAndroid }) => {
+  it.skip(isAndroid, 'Context is reused, so the cookies are not isolated between tests.');
   await page.goto(server.PREFIX + '/empty.html');
   let resolve;
   const serverRequestPromise = new Promise<http.IncomingMessage>(f => resolve = f);
