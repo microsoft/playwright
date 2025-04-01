00bc31835548c1f3c713dc6d2f86980fd48131a0
Yury Semikhatsky <yurys@chromium.org>
2025-04-01 12:10:13 -0700

    test

--- tests/page/page-check.spec.ts
+++ tests/page/page-check.spec.ts
@@ -18,15 +18,9 @@
 import { test as it, expect } from './pageTest';
 
 it('should check the box @smoke', async ({ page }) => {
-  await it.step('first', async () => {
-    await page.setContent(`<input id='checkbox' type='checkbox'></input>`);
-  });
-  await it.step('middle', async () => {
-    await page.check('input');
-  });
-  await it.step('last', async () => {
-    expect(await page.evaluate(() => window['checkbox'].checked)).toBe(true);
-  });
+  await page.setContent(`<input id='checkbox' type='checkbox'></input>`);
+  await page.check('input');
+  expect(await page.evaluate(() => window['checkbox'].checked)).toBe(true);
 });
 
 it('should not check the checked box', async ({ page }) => {
