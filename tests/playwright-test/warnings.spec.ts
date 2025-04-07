8e1ca981658c6bbd2b74089ebbccfdb7cf26ed2e
Adam Gastineau <adam@agg.im>
2025-04-07 08:00:09 -0700

    Update tests to new logic

--- tests/playwright-test/warnings.spec.ts
+++ tests/playwright-test/warnings.spec.ts
@@ -16,7 +16,7 @@
 
 import { test, expect } from './playwright-test-fixtures';
 
-const description = 'Some async calls were not awaited by the end of the test. This can cause flakiness.';
+const description = 'This async call was not awaited by the end of the test. This can cause flakiness. It is recommended to run ESLint with "@typescript-eslint/no-floating-promises" to verify.';
 
 test.describe.configure({ mode: 'parallel' });
 
@@ -25,16 +25,17 @@ test.describe('await', () => {
     const { exitCode, results } = await runInlineTest({
       'a.test.ts': `
         import { test } from '@playwright/test';
-        test('test', () => {
+        test('test', async () => {
           new Promise(() => {});
+          await expect(page.locator('div')).toHaveText('A', { timeout: 100 });
         });
       `
     });
-    expect(exitCode).toBe(0);
+    expect(exitCode).toBe(1);
     expect(results[0].annotations).toEqual([]);
   });
 
-  test('should warn about missing await on expects when failing', async ({ runInlineTest }) => {
+  test('should warn on failure', async ({ runInlineTest }) => {
     const { exitCode, results } = await runInlineTest({
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
@@ -49,22 +50,36 @@ test.describe('await', () => {
     expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 39 }) }]);
   });
 
-  test('should warn about missing await on expects when passing', async ({ runInlineTest }) => {
+  test('should not warn on success', async ({ runInlineTest }) => {
     const { exitCode, results } = await runInlineTest({
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
-        test('test', async ({ page }) => {
+        test('custom test name', async ({ page }) => {
           await page.setContent('<div>A</div>');
-          expect(page.locator('div')).toHaveText('A');
+          expect(page.locator('div')).toHaveText('A', { timeout: 100 });
           await new Promise(f => setTimeout(f, 1000));
         });
       `
     });
     expect(exitCode).toBe(0);
-    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 5, column: 39 }) }]);
+    expect(results[0].annotations).toEqual([]);
   });
 
-  test('should not warn when not missing await on expects when failing', async ({ runInlineTest }) => {
+  test('should warn about missing await on expects', async ({ runInlineTest }) => {
+    const { exitCode, results } = await runInlineTest({
+      'a.test.ts': `
+        import { test, expect } from '@playwright/test';
+        test('custom test name', async ({ page }) => {
+          expect(page.locator('div')).toHaveText('A', { timeout: 100 });
+          await new Promise(f => setTimeout(f, 1000));
+        });
+      `
+    });
+    expect(exitCode).toBe(1);
+    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 39 }) }]);
+  });
+
+  test('should not warn when not missing await on expects', async ({ runInlineTest }) => {
     const { exitCode, results } = await runInlineTest({
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
@@ -77,47 +92,32 @@ test.describe('await', () => {
     expect(results[0].annotations).toEqual([]);
   });
 
-  test('should not warn when not missing await on expects when passing', async ({ runInlineTest }) => {
+  test('should not warn when using then() on expects', async ({ runInlineTest }) => {
     const { exitCode, results } = await runInlineTest({
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
         test('test', async ({ page }) => {
-          await page.setContent('<div>A</div>');
-          await expect(page.locator('div')).toHaveText('A');
-        });
-      `
-    });
-    expect(exitCode).toBe(0);
-    expect(results[0].annotations).toEqual([]);
-  });
-
-  test('should not warn when using then on expects when passing', async ({ runInlineTest }) => {
-    const { exitCode, results } = await runInlineTest({
-      'a.test.ts': `
-        import { test, expect } from '@playwright/test';
-        test('test', async ({ page }) => {
-          await page.setContent('<div>A</div>');
           expect(page.locator('div')).toHaveText('A').then(() => {});
           await new Promise(f => setTimeout(f, 1000));
         });
       `
     });
-    expect(exitCode).toBe(0);
+    expect(exitCode).toBe(1);
     expect(results[0].annotations).toEqual([]);
   });
 
-  test('should warn about missing await on reject', async ({ runInlineTest }) => {
+  test('should warn about missing await on resolve', async ({ runInlineTest }) => {
     const { exitCode, results } = await runInlineTest({
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
         test('test', async ({ page }) => {
-          expect(Promise.reject(new Error('foo'))).rejects.toThrow('foo');
+          expect(Promise.reject(new Error('foo'))).resolves.toBe('foo');
           await new Promise(f => setTimeout(f, 1000));
         });
       `
     });
-    expect(exitCode).toBe(0);
-    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 60 }) }]);
+    expect(exitCode).toBe(1);
+    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 61 }) }]);
   });
 
   test('should warn about missing await on reject.not', async ({ runInlineTest }) => {
@@ -139,14 +139,13 @@ test.describe('await', () => {
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
         test('test', async ({ page }) => {
-          await page.setContent('<div>A</div>');
           test.step('step', () => {});
-          await expect(page.locator('div')).toHaveText('A');
+          await expect(page.locator('div')).toHaveText('A', { timeout: 100 });
         });
       `
     });
-    expect(exitCode).toBe(0);
-    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 5, column: 16 }) }]);
+    expect(exitCode).toBe(1);
+    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 16 }) }]);
   });
 
   test('should not warn when not missing await on test.step', async ({ runInlineTest }) => {
@@ -154,13 +153,12 @@ test.describe('await', () => {
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
         test('test', async ({ page }) => {
-          await page.setContent('<div>A</div>');
           await test.step('step', () => {});
-          await expect(page.locator('div')).toHaveText('A');
+          await expect(page.locator('div')).toHaveText('A', { timeout: 100 });
         });
       `
     });
-    expect(exitCode).toBe(0);
+    expect(exitCode).toBe(1);
     expect(results[0].annotations).toEqual([]);
   });
 
@@ -169,14 +167,13 @@ test.describe('await', () => {
       'a.test.ts': `
         import { test, expect } from '@playwright/test';
         test('test', async ({ page }) => {
-          await page.setContent('<div>A</div>');
           test.step.skip('step', () => {});
           await expect(page.locator('div')).toHaveText('A');
         });
       `
     });
-    expect(exitCode).toBe(0);
-    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 5, column: 21 }) }]);
+    expect(exitCode).toBe(1);
+    expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 21 }) }]);
   });
 
   test('traced promise should be instanceof Promise', async ({ runInlineTest }) => {
@@ -209,12 +206,12 @@ test.describe('await', () => {
               await new Promise(f => setTimeout(f, 1000));
             });
             test('test ${hook}', async () => {
-              await expect(page.locator('div')).toBeVisible();
+              await expect(page.locator('button')).toBeVisible({ timeout: 100 });
             });
           `
         });
 
-        expect(exitCode).toBe(0);
+        expect(exitCode).toBe(1);
         expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 7, column: 43 }) }]);
       });
     }
@@ -229,7 +226,7 @@ test.describe('await', () => {
             import { test, expect } from '@playwright/test';
             let page;
             test('test ${hook}', async ({ browser }) => {
-              await expect(Promise.resolve()).resolves.toBe(undefined);
+              await expect(Promise.resolve()).resolves.toBe('A');
             });
             test.${hook}(async () => {
               expect(Promise.resolve()).resolves.toBe(undefined);
@@ -238,7 +235,7 @@ test.describe('await', () => {
           `
         });
 
-        expect(exitCode).toBe(0);
+        expect(exitCode).toBe(1);
         expect(results[0].annotations).toEqual([{ type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 8, column: 50 }) }]);
       });
     }
@@ -253,7 +250,7 @@ test.describe('await', () => {
           await new Promise(f => setTimeout(f, 1000));
         });
         test('test', async () => {
-          expect(Promise.resolve()).resolves.toBe(undefined);
+          expect(Promise.resolve()).resolves.toBe('A');
           await new Promise(f => setTimeout(f, 1000));
         });
         test.afterEach(async () => {
@@ -262,7 +259,7 @@ test.describe('await', () => {
         });
       `
     });
-    expect(exitCode).toBe(0);
+    expect(exitCode).toBe(1);
     expect(results[0].annotations).toEqual([
       { type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 4, column: 46 }) },
       { type: 'warning', description, location: expect.objectContaining({ file: expect.stringMatching(/a\.test\.ts$/), line: 8, column: 46 }) },
