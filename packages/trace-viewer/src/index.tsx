0535a2ee1112ae750af8e64a32440c2f81db3b10
Pavel Feldman <pavel.feldman@gmail.com>
2025-05-14 17:41:12 -0700

    chore: allow showing live trace (#35957)

--- packages/trace-viewer/src/index.tsx
+++ packages/trace-viewer/src/index.tsx
@@ -19,11 +19,14 @@ import { applyTheme } from '@web/theme';
 import '@web/third_party/vscode/codicon.css';
 import * as ReactDOM from 'react-dom/client';
 import { WorkbenchLoader } from './ui/workbenchLoader';
+import { LiveWorkbenchLoader } from './ui/liveWorkbenchLoader';
 
 (async () => {
+  const queryParams = new URLSearchParams(window.location.search);
+
   applyTheme();
   if (window.location.protocol !== 'file:') {
-    if (window.location.href.includes('isUnderTest=true'))
+    if (queryParams.get('isUnderTest') === 'true')
       await new Promise(f => setTimeout(f, 1000));
     if (!navigator.serviceWorker)
       throw new Error(`Service workers are not supported.\nMake sure to serve the Trace Viewer (${window.location}) via HTTPS or localhost.`);
@@ -38,5 +41,8 @@ import { WorkbenchLoader } from './ui/workbenchLoader';
     setInterval(function() { fetch('ping'); }, 10000);
   }
 
-  ReactDOM.createRoot(document.querySelector('#root')!).render(<WorkbenchLoader/>);
+  const trace = queryParams.get('trace');
+  const traceIsLive = trace?.endsWith('.json');
+  const workbench = traceIsLive ? <LiveWorkbenchLoader traceJson={trace!} /> : <WorkbenchLoader/>;
+  ReactDOM.createRoot(document.querySelector('#root')!).render(workbench);
 })();
