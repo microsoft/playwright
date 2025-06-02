a5d5cdc552137610ef7e0b5b934dee6bd3622e26
Simon Knott <info@simonknott.de>
2025-06-02 12:46:20 +0200

    globalTimeout 0

--- packages/playwright/src/common/config.ts
+++ packages/playwright/src/common/config.ts
@@ -94,7 +94,7 @@ export class FullConfigInternal {
       fullyParallel: takeFirst(configCLIOverrides.fullyParallel, userConfig.fullyParallel, false),
       globalSetup: this.globalSetups[0] ?? null,
       globalTeardown: this.globalTeardowns[0] ?? null,
-      globalTimeout: takeFirst(configCLIOverrides.debug === 'begin' ? 0 : undefined, configCLIOverrides.globalTimeout, userConfig.globalTimeout, 0),
+      globalTimeout: takeFirst(configCLIOverrides.debug ? 0 : undefined, configCLIOverrides.globalTimeout, userConfig.globalTimeout, 0),
       grep: takeFirst(userConfig.grep, defaultGrep),
       grepInvert: takeFirst(userConfig.grepInvert, null),
       maxFailures: takeFirst(configCLIOverrides.debug === 'begin' ? 1 : undefined, configCLIOverrides.maxFailures, userConfig.maxFailures, 0),
