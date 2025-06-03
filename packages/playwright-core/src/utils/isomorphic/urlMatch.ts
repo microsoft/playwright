fd109e30c523841d6ff13f35bdedac5adbf61d0b
Adam Gastineau <adam@agg.im>
2025-06-03 10:05:06 -0700

    fix: allow matching against `about:blank` and other custom URLs

--- packages/playwright-core/src/utils/isomorphic/urlMatch.ts
+++ packages/playwright-core/src/utils/isomorphic/urlMatch.ts
@@ -127,6 +127,11 @@ function resolveGlobBase(baseURL: string | undefined, match: string): string {
     }
     // Escaped `\\?` behaves the same as `?` in our glob patterns.
     match = match.replaceAll(/\\\\\?/g, '?');
+    // Node URL constructors automatically uses colon as an indicator of a base URL, such as with `about:blank`
+    const splitScheme = match.split('://');
+    const schemelessPath = splitScheme.length === 1 ? splitScheme[0] : splitScheme[1];
+    if (schemelessPath !== undefined && schemelessPath.indexOf(':') !== -1)
+      return match;
     // Glob symbols may be escaped in the URL and some of them such as ? affect resolution,
     // so we replace them with safe components first.
     const relativePath = match.split('/').map((token, index) => {
