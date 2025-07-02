a769b262906b44249c5776c971075c3b4fd314d2
Max Schmitt <max@schmitt.mx>
2025-07-02 10:17:06 +0200

    test: unflake toBeInViewport test

--- tests/page/expect-misc.spec.ts
+++ tests/page/expect-misc.spec.ts
@@ -464,7 +464,7 @@ test.describe('toBeInViewport', () => {
   test('should have good stack', async ({ page }) => {
     let error;
     try {
-      await expect(page.locator('body')).not.toBeInViewport({ timeout: 100 });
+      await expect(page.locator('body')).not.toBeInViewport({ timeout: 500 });
     } catch (e) {
       error = e;
     }
