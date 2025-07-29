bd62f66bd298440b10d97ccf21acd4b31afa3ceb
Max Schmitt <max@schmitt.mx>
2025-07-29 17:18:13 +0200

    adjust tests

--- tests/library/tracing.spec.ts
+++ tests/library/tracing.spec.ts
@@ -52,7 +52,7 @@ test('should collect trace with resources, but no js', async ({ context, page, s
     'Navigate to "/input/fileupload.html"',
     'Set input files',
     'Wait for timeout',
-    'Close',
+    'Close page',
   ]);
 
   expect(events.some(e => e.type === 'frame-snapshot')).toBeTruthy();
@@ -200,7 +200,7 @@ test('should collect two traces', async ({ context, page, server }, testInfo) =>
     expect(events[0].type).toBe('context-options');
     expect(actions).toEqual([
       'Double click',
-      'Close',
+      'Close page',
     ]);
   }
 });
