546918451764d40d40b28ae8c5c6e3402dff71b6
Dmitry Gozman <dgozman@gmail.com>
2025-08-07 08:37:26 +0100

    fix: do not prefix custom expect titles
    This changes `step.title` to include the formatting:
    - `toBeVisible` -> `Expect "toBeVisible"`
    - `foo.png` -> `Attach "foo.png"`
    
    All builtin clients like trace viewer or html reporter
    did that anyway.
    
    Such a change allows a custom expect title to not be
    forcefully prefixed like `Expect "verify sign in link"`.

--- packages/playwright/src/reporters/html.ts
+++ packages/playwright/src/reporters/html.ts
@@ -26,7 +26,7 @@ import { yazl } from 'playwright-core/lib/zipBundle';
 
 import { CommonReporterOptions, formatError, formatResultFailure, internalScreen } from './base';
 import { codeFrameColumns } from '../transform/babelBundle';
-import { resolveReporterOutputPath, stripAnsiEscapes, stepTitle } from '../util';
+import { resolveReporterOutputPath, stripAnsiEscapes } from '../util';
 
 import type { ReporterV2 } from './reporterV2';
 import type { HtmlReporterOptions as HtmlReporterConfigOptions, Metadata, TestAnnotation } from '../../types/test';
@@ -34,7 +34,6 @@ import type * as api from '../../types/testReporter';
 import type { HTMLReport, Location, Stats, TestAttachment, TestCase, TestCaseSummary, TestFile, TestFileSummary, TestResult, TestStep } from '@html-reporter/types';
 import type { ZipFile } from 'playwright-core/lib/zipBundle';
 import type { TransformCallback } from 'stream';
-import type { TestStepCategory } from '../util';
 
 type TestEntry = {
   testCase: TestCase;
@@ -545,7 +544,7 @@ class HtmlBuilder {
   private _createTestStep(dedupedStep: DedupedStep, result: api.TestResult): TestStep {
     const { step, duration, count } = dedupedStep;
     const skipped = dedupedStep.step.annotations?.find(a => a.type === 'skip');
-    let title = stepTitle(step.category as TestStepCategory, step.title);
+    let title = step.title;
     if (skipped)
       title = `${title} (skipped${skipped.description ? ': ' + skipped.description : ''})`;
     const testStep: TestStep = {
