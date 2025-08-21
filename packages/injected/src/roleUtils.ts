0aa4cd895606b21a4f2d92ea975a07f4bfda3dbe
Adam Gastineau <adam@agg.im>
2025-08-21 09:33:04 -0700

    Revert "fix(a11y): track inert elements as hidden (#36947)" (#37137)

--- packages/injected/src/roleUtils.ts
+++ packages/injected/src/roleUtils.ts
@@ -284,10 +284,10 @@ export function isElementHiddenForAria(element: Element): boolean {
   const isOptionInsideSelect = element.nodeName === 'OPTION' && !!element.closest('select');
   if (!isOptionInsideSelect && !isSlot && !isElementStyleVisibilityVisible(element, style))
     return true;
-  return belongsToDisplayNoneOrAriaHiddenOrNonSlottedOrInert(element);
+  return belongsToDisplayNoneOrAriaHiddenOrNonSlotted(element);
 }
 
-function belongsToDisplayNoneOrAriaHiddenOrNonSlottedOrInert(element: Element): boolean {
+function belongsToDisplayNoneOrAriaHiddenOrNonSlotted(element: Element): boolean {
   let hidden = cacheIsHidden?.get(element);
   if (hidden === undefined) {
     hidden = false;
@@ -298,17 +298,17 @@ function belongsToDisplayNoneOrAriaHiddenOrNonSlottedOrInert(element: Element): 
     if (element.parentElement && element.parentElement.shadowRoot && !element.assignedSlot)
       hidden = true;
 
-    // display:none and aria-hidden=true and inert are considered hidden for aria.
+    // display:none and aria-hidden=true are considered hidden for aria.
     if (!hidden) {
       const style = getElementComputedStyle(element);
-      hidden = !style || style.display === 'none' || getAriaBoolean(element.getAttribute('aria-hidden')) === true || element.getAttribute('inert') !== null;
+      hidden = !style || style.display === 'none' || getAriaBoolean(element.getAttribute('aria-hidden')) === true;
     }
 
     // Check recursively.
     if (!hidden) {
       const parent = parentElementOrShadowHost(element);
       if (parent)
-        hidden = belongsToDisplayNoneOrAriaHiddenOrNonSlottedOrInert(parent);
+        hidden = belongsToDisplayNoneOrAriaHiddenOrNonSlotted(parent);
     }
     cacheIsHidden?.set(element, hidden);
   }
