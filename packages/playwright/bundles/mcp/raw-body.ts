b11f6f93396e057142c52db0e365c9f7351a7a0e
Simon Knott <info@simonknott.de>
2025-08-27 14:06:14 +0200

    supress error

--- packages/playwright/bundles/mcp/raw-body.ts
+++ packages/playwright/bundles/mcp/raw-body.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// @ts-expect-error untyped module
 import bytes from 'bytes';
 import type { IncomingMessage } from 'node:http';
 
