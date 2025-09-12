5123acd6024a6ca54d249ae36432689ff1709e3c
Yury Semikhatsky <yurys@chromium.org>
2025-09-12 10:41:08 -0700

    fix trace-viewer build

--- packages/web/src/components/xtermModule.tsx
+++ packages/web/src/components/xtermModule.tsx
@@ -14,7 +14,7 @@
   limitations under the License.
 */
 
-import 'xterm/css/xterm.css';
+import '@xterm/xterm/css/xterm.css';
 
 import { Terminal } from '@xterm/xterm';
 import { FitAddon } from '@xterm/addon-fit';
