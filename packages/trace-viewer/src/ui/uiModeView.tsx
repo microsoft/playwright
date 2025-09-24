d9613621aee6127222ce73e0db50431ec322cd78
Adam Gastineau <adam@agg.im>
2025-09-24 10:39:04 -0700

    chore(ui-mode): switch to only showing Single worker button (#37538)

--- packages/trace-viewer/src/ui/uiModeView.tsx
+++ packages/trace-viewer/src/ui/uiModeView.tsx
@@ -100,7 +100,7 @@ export const UIModeView: React.FC<{}> = ({
   const [revealSource, setRevealSource] = React.useState(false);
   const onRevealSource = React.useCallback(() => setRevealSource(true), [setRevealSource]);
 
-  const [showBrowser, setShowBrowser] = useSetting<boolean>('show-browser', false);
+  const [singleWorker, setSingleWorker] = useSetting<boolean>('single-worker', false);
   const [updateSnapshots, setUpdateSnapshots] = useSetting<reporterTypes.FullConfig['updateSnapshots']>('updateSnapshots', 'missing');
   const [hideFiles] = useSetting('hideFiles', false);
 
@@ -289,8 +289,7 @@ export const UIModeView: React.FC<{}> = ({
         projects: [...projectFilters].filter(([_, v]) => v).map(([p]) => p),
         updateSnapshots,
         reporters: queryParams.reporters,
-        headed: showBrowser,
-        workers: showBrowser ? 1 : undefined,
+        workers: singleWorker ? 1 : undefined,
         trace: 'on',
       });
       // Clear pending tests in case of interrupt.
@@ -301,7 +300,7 @@ export const UIModeView: React.FC<{}> = ({
       setTestModel({ ...testModel });
       setRunningState(oldState => oldState ? ({ ...oldState, completed: true }) : undefined);
     });
-  }, [projectFilters, isRunningTest, testModel, testServerConnection, updateSnapshots, showBrowser]);
+  }, [projectFilters, isRunningTest, testModel, testServerConnection, updateSnapshots, singleWorker]);
 
   React.useEffect(() => {
     if (!testServerConnection || !teleSuiteUpdater)
@@ -505,7 +504,7 @@ export const UIModeView: React.FC<{}> = ({
           <div className='section-title'>Testing Options</div>
         </Toolbar>
         {testingOptionsVisible && <SettingsView settings={[
-          { type: 'check', value: showBrowser, set: setShowBrowser, name: 'Show browser — single worker' },
+          { type: 'check', value: singleWorker, set: setSingleWorker, name: 'Single worker' },
           { type: 'select', options: [
             { label: 'All', value: 'all' },
             { label: 'Changed', value: 'changed' },
