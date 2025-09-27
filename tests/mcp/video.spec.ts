bab11ee85de01d753f687be0ffb70d522ed0bffe
Pavel Feldman <pavel.feldman@gmail.com>
2025-09-26 17:31:36 -0700

    chore: produce longer videos in mcp test

--- tests/mcp/video.spec.ts
+++ tests/mcp/video.spec.ts
@@ -36,6 +36,20 @@ for (const mode of ['isolated', 'persistent']) {
       code: expect.stringContaining(`page.goto('http://localhost`),
     });
 
+    await client.callTool({
+      name: 'browser_evaluate',
+      arguments: {
+        function: `async () => {
+          document.body.style.backgroundColor = "red";
+          await new Promise(resolve => setTimeout(resolve, 100));
+          document.body.style.backgroundColor = "green";
+          await new Promise(resolve => setTimeout(resolve, 100));
+          document.body.style.backgroundColor = "blue";
+          await new Promise(resolve => setTimeout(resolve, 100));
+        }`,
+      },
+    });
+
     expect(await client.callTool({
       name: 'browser_close',
     })).toHaveResponse({
@@ -70,6 +84,20 @@ for (const mode of ['isolated', 'persistent']) {
       arguments: { url: server.HELLO_WORLD },
     })).toHaveResponse({
       code: expect.stringContaining(`page.goto('http://localhost`),
+    });
+
+    await client.callTool({
+      name: 'browser_evaluate',
+      arguments: {
+        function: `async () => {
+          document.body.style.backgroundColor = "red";
+          await new Promise(resolve => setTimeout(resolve, 100));
+          document.body.style.backgroundColor = "green";
+          await new Promise(resolve => setTimeout(resolve, 100));
+          document.body.style.backgroundColor = "blue";
+          await new Promise(resolve => setTimeout(resolve, 100));
+        }`,
+      },
     });
 
     expect(await client.callTool({
