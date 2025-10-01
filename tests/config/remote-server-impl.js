ffee07cec3e85f72dd8e6e969283a3f23d99c415
Simon Knott <info@simonknott.de>
2025-10-01 10:41:35 +0200

    Revert "test: move browser._launchServer in child process (#37114)"
    This reverts commit face8d44ea0b6691788c95e45dedab1738563925.

--- tests/config/remote-server-impl.js
+++ tests/config/remote-server-impl.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const cluster = require('cluster');
 
 async function start() {
-  /** @type {import("./remoteServer").RemoteServerOptions} */
-  const { browserTypeName, launchOptions, stallOnClose, disconnectOnSIGHUP, exitOnFile, exitOnWarning, startStopAndRunHttp, existingBrowser } = JSON.parse(process.argv[2]);
+  const { browserTypeName, launchOptions, stallOnClose, disconnectOnSIGHUP, exitOnFile, exitOnWarning, startStopAndRunHttp } = JSON.parse(process.argv[2]);
   if (stallOnClose) {
     launchOptions.__testHookGracefullyClose = () => {
       console.log(`(stalled=>true)`);
@@ -26,15 +25,7 @@ async function start() {
     return;
   }
 
-  let browserServer;
-  if (existingBrowser) {
-    const browser = await playwright[browserTypeName].launch(launchOptions);
-    const page = await browser.newPage();
-    await page.setContent(existingBrowser.content);
-    browserServer = await browser._launchServer();
-  } else {
-    browserServer = await playwright[browserTypeName].launchServer(launchOptions);
-  }
+  const browserServer = await playwright[browserTypeName].launchServer(launchOptions);
   if (disconnectOnSIGHUP)
     process.on('SIGHUP', () => browserServer._disconnectForTest());
 
