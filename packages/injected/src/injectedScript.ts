ccb908adac882d7c5f142e71693c875957bc6130
Simon Knott <info@simonknott.de>
2025-10-10 09:53:50 +0200

    fix(shadow dom): follow assigned slot when hit-testing (#37777)

--- packages/injected/src/injectedScript.ts
+++ packages/injected/src/injectedScript.ts
@@ -989,7 +989,9 @@ export class InjectedScript {
     const hitParents: Element[] = [];
     while (hitElement && hitElement !== targetElement) {
       hitParents.push(hitElement);
-      hitElement = parentElementOrShadowHost(hitElement);
+      // Prefer the composed tree over the light-dom tree, as browser performs hit testing on the composed tree.
+      // Note that we will still eventually climb to the light-dom parent, as any element distributed to a slot is a direct child of the shadow host that contains the slot.
+      hitElement = hitElement.assignedSlot ?? parentElementOrShadowHost(hitElement);
     }
     if (hitElement === targetElement)
       return 'done';
