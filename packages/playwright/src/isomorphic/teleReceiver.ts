459d4545596dd8ccbd1d303940f902c098396187
Simon Knott <info@simonknott.de>
2025-11-07 12:56:11 -0800

    chore: emit webServer on telereporter

--- packages/playwright/src/isomorphic/teleReceiver.ts
+++ packages/playwright/src/isomorphic/teleReceiver.ts
@@ -25,7 +25,7 @@ export type JsonStackFrame = { file: string, line: number, column: number };
 
 export type JsonStdIOType = 'stdout' | 'stderr';
 
-export type JsonConfig = Pick<reporterTypes.FullConfig, 'configFile' | 'globalTimeout' | 'maxFailures' | 'metadata' | 'rootDir' | 'version' | 'workers' | 'globalSetup' | 'globalTeardown'> & {
+export type JsonConfig = Pick<reporterTypes.FullConfig, 'configFile' | 'globalTimeout' | 'maxFailures' | 'metadata' | 'rootDir' | 'version' | 'workers' | 'globalSetup' | 'globalTeardown' | 'webServer'> & {
   // optional for backwards compatibility
   tags?: reporterTypes.FullConfig['tags'],
 };
