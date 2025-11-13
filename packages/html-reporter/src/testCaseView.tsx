ab695723c35a8de108fb5904191a2182a10bdb6e
Simon Knott <info@simonknott.de>
2025-11-13 16:54:00 +0100

    address

--- packages/html-reporter/src/testCaseView.tsx
+++ packages/html-reporter/src/testCaseView.tsx
@@ -44,21 +44,16 @@ export const TestCaseView: React.FC<{
   const [selectedResultIndex, setSelectedResultIndex] = React.useState(run);
   const searchParams = React.useContext(SearchParamsContext);
 
-  const filterParam = searchParams.has('q') ? '&q=' + searchParams.get('q') : '';
   const visibleTestAnnotations = test.annotations.filter(a => !a.type.startsWith('_')) ?? [];
-
-  searchParams.delete('testId');
-  const previousLink = testResultHref({ test: prev }, searchParams) + filterParam;
-  const nextLink = testResultHref({ test: next }, searchParams) + filterParam;
 
   return <>
     <HeaderView
       title={test.title}
       leftSuperHeader={<div className='test-case-path'>{test.path.join(' › ')}</div>}
       rightSuperHeader={<>
-        <div className={clsx(!prev && 'hidden')}><Link href={previousLink}>« previous</Link></div>
+        <div className={clsx(!prev && 'hidden')}><Link href={testResultHref({ test: prev }, searchParams)}>« previous</Link></div>
         <div style={{ width: 10 }}></div>
-        <div className={clsx(!next && 'hidden')}><Link href={nextLink}>next »</Link></div>
+        <div className={clsx(!next && 'hidden')}><Link href={testResultHref({ test: next }, searchParams)}>next »</Link></div>
       </>}
     />
     <div className='hbox' style={{ lineHeight: '24px' }}>
