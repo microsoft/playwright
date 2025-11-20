019900fd24d8a9cc9fce986a2c2fb0c7e03c9408
Simon Knott <info@simonknott.de>
2025-11-20 14:52:03 +0100

    chore: update browser_patches (#38286)

--- browser_patches/firefox/UPSTREAM_CONFIG.sh
+++ browser_patches/firefox/UPSTREAM_CONFIG.sh
@@ -1,3 +1,3 @@
 REMOTE_URL="https://github.com/mozilla-firefox/firefox"
 BASE_BRANCH="release"
-BASE_REVISION="c1ee0105d25a4c2b2887e916470bdf41a9fd47ef"
+BASE_REVISION="e0df3d598dba47de812bb5144cbc187557377095"
