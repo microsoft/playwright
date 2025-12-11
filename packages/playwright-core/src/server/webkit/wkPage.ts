360f72e29f953d2f77df0042cf383656ff0ba88e
Yury Semikhatsky <yurys@chromium.org>
2025-12-10 17:16:19 -0800

    chore(webkit): fix video on debian 11 (#38519)

--- packages/playwright-core/src/server/webkit/wkPage.ts
+++ packages/playwright-core/src/server/webkit/wkPage.ts
@@ -923,7 +923,13 @@ export class WKPage implements PageDelegate {
     const buffer = Buffer.from(event.data, 'base64');
     this._page.emit(Page.Events.ScreencastFrame, {
       buffer,
-      frameSwapWallTime: event.timestamp * 1000, // timestamp is in seconds, we need to convert to milliseconds.
+      frameSwapWallTime: event.timestamp
+        // timestamp is in seconds, we need to convert to milliseconds.
+        ? event.timestamp * 1000
+        // Fallback for Debian 11 and Ubuntu 20.04 where WebKit is frozen on an older
+        // version that did not send timestamp.
+        // TODO: remove this fallback when Debian 11 and Ubuntu 20.04 are EOL.
+        : Date.now(),
       width: event.deviceWidth,
       height: event.deviceHeight,
     });
