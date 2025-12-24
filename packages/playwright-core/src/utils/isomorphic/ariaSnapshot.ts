6a00a6aa94267401ab6a78f08571b42f2e6c3067
Pavel Feldman <pavel.feldman@gmail.com>
2025-12-23 17:22:25 -0800

    chore: make AriaNode isomorphic (#38639)

--- packages/playwright-core/src/utils/isomorphic/ariaSnapshot.ts
+++ packages/playwright-core/src/utils/isomorphic/ariaSnapshot.ts
@@ -35,7 +35,38 @@ export type AriaProps = {
   selected?: boolean;
 };
 
-export function ariaPropsEqual(a: AriaProps, b: AriaProps): boolean {
+export type AriaBox = {
+  visible: boolean;
+  inline: boolean;
+  cursor?: string;
+};
+
+// Note: please keep in sync with ariaNodesEqual() below.
+export type AriaNode = AriaProps & {
+  role: AriaRole | 'fragment' | 'iframe';
+  name: string;
+  ref?: string;
+  children: (AriaNode | string)[];
+  box: AriaBox;
+  receivesPointerEvents: boolean;
+  props: Record<string, string>;
+};
+
+export function ariaNodesEqual(a: AriaNode, b: AriaNode): boolean {
+  if (a.role !== b.role || a.name !== b.name)
+    return false;
+  if (!ariaPropsEqual(a, b) || hasPointerCursor(a) !== hasPointerCursor(b))
+    return false;
+  const aKeys = Object.keys(a.props);
+  const bKeys = Object.keys(b.props);
+  return aKeys.length === bKeys.length && aKeys.every(k => a.props[k] === b.props[k]);
+}
+
+export function hasPointerCursor(ariaNode: AriaNode): boolean {
+  return ariaNode.box.cursor === 'pointer';
+}
+
+function ariaPropsEqual(a: AriaProps, b: AriaProps): boolean {
   return a.active === b.active && a.checked === b.checked && a.disabled === b.disabled && a.expanded === b.expanded && a.selected === b.selected && a.level === b.level && a.pressed === b.pressed;
 }
 
@@ -496,4 +527,53 @@ export class ParserError extends Error {
     super(message);
     this.pos = pos;
   }
+}
+
+export function findNewNode(from: AriaNode | undefined, to: AriaNode): AriaNode | undefined {
+  type ByRoleAndName = Map<string, Map<string, { node: AriaNode, sizeAndPosition: number }>>;
+
+  function fillMap(root: AriaNode, map: ByRoleAndName, position: number) {
+    let size = 1;
+    let childPosition = position + size;
+    for (const child of root.children || []) {
+      if (typeof child === 'string') {
+        size++;
+        childPosition++;
+      } else {
+        size += fillMap(child, map, childPosition);
+        childPosition += size;
+      }
+    }
+    if (!['none', 'presentation', 'fragment', 'iframe', 'generic'].includes(root.role) && root.name) {
+      let byRole = map.get(root.role);
+      if (!byRole) {
+        byRole = new Map();
+        map.set(root.role, byRole);
+      }
+      const existing = byRole.get(root.name);
+      // This heuristic prioritizes elements at the top of the page, even if somewhat smaller.
+      const sizeAndPosition = size * 100 - position;
+      if (!existing || existing.sizeAndPosition < sizeAndPosition)
+        byRole.set(root.name, { node: root, sizeAndPosition });
+    }
+    return size;
+  }
+
+  const fromMap: ByRoleAndName = new Map();
+  if (from)
+    fillMap(from, fromMap, 0);
+
+  const toMap: ByRoleAndName = new Map();
+  fillMap(to, toMap, 0);
+
+  const result: { node: AriaNode, sizeAndPosition: number }[] = [];
+  for (const [role, byRole] of toMap) {
+    for (const [name, byName] of byRole) {
+      const inFrom = fromMap.get(role)?.get(name);
+      if (!inFrom)
+        result.push(byName);
+    }
+  }
+  result.sort((a, b) => b.sizeAndPosition - a.sizeAndPosition);
+  return result[0]?.node;
 }
