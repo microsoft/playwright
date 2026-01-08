79db07c5ec830155e8e9f61e572eea4fa3009832
Yury Semikhatsky <yurys@chromium.org>
2026-01-08 13:34:18 -0800

    devops: reduce workers 4->3 for tests without retries

--- tests/playwright-test/playwright.config.ts
+++ tests/playwright-test/playwright.config.ts
@@ -34,7 +34,7 @@ const reporters = () => {
 export default defineConfig({
   timeout: 30000,
   forbidOnly: !!process.env.CI,
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   snapshotPathTemplate: '__screenshots__/{testFilePath}/{arg}{ext}',
   projects: [
     {
