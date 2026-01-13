75c683771253397bf1ffdbf115d81d297518561d
Dmitry Gozman <dgozman@gmail.com>
2026-01-13 10:44:56 +0000

    feat(test runner): allow running test.spec.md files

---
+++ packages/playwright/src/transform/md.ts
@@ -0,0 +1,192 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fs from 'fs';
+import path from 'path';
+
+import { parseMarkdown } from '../utilsBundle';
+import type * as mdast from 'mdast';
+
+type Props = [string, string][];
+
+export function transformMDToTS(code: string, filename: string): string {
+  const parsed = parseSpec(code, filename);
+  const seed = parsed.props.find(prop => prop[0] === 'seed')?.[1];
+  if (seed) {
+    const seedFile = path.resolve(path.dirname(filename), seed);
+    const seedContents = fs.readFileSync(seedFile, 'utf-8');
+    const parsedSeed = parseSpec(seedContents, seedFile);
+    if (parsedSeed.tests.length !== 1)
+      throw new Error(`while parsing ${seedFile}: seed file must contain exactly one test`);
+    if (parsedSeed.tests[0].props.length)
+      throw new Error(`while parsing ${seedFile}: seed test must not have properties`);
+    for (const test of parsed.tests)
+      test.lines = parsedSeed.tests[0].lines.concat(test.lines);
+    const fixtures = parsedSeed.props.find(prop => prop[0] === 'fixtures');
+    if (fixtures && !parsed.props.find(prop => prop[0] === 'fixtures'))
+      parsed.props.push(fixtures);
+  }
+
+  const fixtures = parsed.props.find(prop => prop[0] === 'fixtures')?.[1] ?? '@playwright/test';
+  const importLine = `import { test, expect } from ${escapeString(fixtures)};`;
+  const renderedTests = parsed.tests.map(test => {
+    const tags: string[] = [];
+    const annotations: { type: string, description: string }[] = [];
+    for (const [key, value] of test.props) {
+      if (key === 'tag') {
+        tags.push(...value.split(' ').map(s => s.trim()).filter(s => !!s));
+      } else if (key === 'annotation') {
+        if (!value.includes('='))
+          throw new Error(`while parsing ${filename}: annotation must be in format "type=description", found "${value}"`);
+        const [type, description] = value.split('=').map(s => s.trim());
+        annotations.push({ type, description });
+      }
+    }
+    let props = '';
+    if (tags.length || annotations.length) {
+      props = '{\n';
+      if (tags.length)
+        props += `    tag: [${tags.map(tag => escapeString(tag)).join(', ')}],\n`;
+      if (annotations.length)
+        props += `    annotation: [${annotations.map(a => `{ type: ${escapeString(a.type)}, description: ${escapeString(a.description)} }`).join(', ')}],\n`;
+      props += '  }, ';
+    }
+    return `\n  test(${escapeString(test.title)}, ${props}async ({ page, agent }) => {\n` +
+      test.lines.map(line => '    ' + line).join('\n') + `\n  });\n`;
+  });
+
+  const result = `${importLine}\ntest.describe(${escapeString(parsed.describe)}, () => {${renderedTests.join('')}\n});\n`;
+  return result;
+}
+
+function escapeString(s: string): string {
+  return `'` + s.replace(/\n/g, ' ').replace(/'/g, `\\'`) + `'`;
+}
+
+function parsingError(filename: string, node: mdast.Node | undefined, message: string): Error {
+  const position = node?.position?.start ? ` at ${node.position.start.line}:${node.position.start.column}` : '';
+  return new Error(`while parsing ${filename}${position}: ${message}`);
+}
+
+function asText(filename: string, node: mdast.Parent, errorMessage: string, skipChild?: mdast.Node): string {
+  let children = node.children.filter(child => child !== skipChild);
+  while (children.length === 1 && children[0].type === 'paragraph')
+    children = children[0].children;
+  if (children.length !== 1 || children[0].type !== 'text')
+    throw parsingError(filename, node, errorMessage);
+  return children[0].value;
+}
+
+function parseSpec(content: string, filename: string): { describe: string, tests: { title: string, lines: string[], props: Props }[], props: Props } {
+  const root = parseMarkdown(content);
+  const props: Props = [];
+
+  const children = [...root.children];
+  const describeNode = children[0];
+  children.shift();
+  if (describeNode?.type !== 'heading' || describeNode.depth !== 2)
+    throw parsingError(filename, describeNode, `describe title must be ##`);
+  const describe = asText(filename, describeNode, `describe title must be ##`);
+
+  if (children[0]?.type === 'list') {
+    parseProps(filename, children[0], props);
+    children.shift();
+  }
+
+  const tests: { title: string, lines: string[], props: Props }[] = [];
+  while (children.length) {
+    let nextIndex = children.findIndex((n, i) => i > 0 && n.type === 'heading' && n.depth === 3);
+    if (nextIndex === -1)
+      nextIndex = children.length;
+    const testNodes = children.splice(0, nextIndex);
+    tests.push(parseTest(filename, testNodes));
+  }
+
+  return { describe, tests, props };
+}
+
+function parseProp(filename: string, node: mdast.ListItem, props: Props) {
+  const propText = asText(filename, node, `property must be a list item without children`);
+  const match = propText.match(/^([^:]+):(.*)$/);
+  if (!match)
+    throw parsingError(filename, node, `property must be in format "key: value"`);
+  props.push([match[1].trim(), match[2].trim()]);
+}
+
+function parseProps(filename: string, node: mdast.List, props: Props) {
+  for (const prop of node.children || []) {
+    if (prop.type !== 'listItem')
+      throw parsingError(filename, prop, `property must be a list item without children`);
+    parseProp(filename, prop, props);
+  }
+}
+
+function parseTest(filename: string, nodes: mdast.Node[]): { title: string, lines: string[], props: Props } {
+  const titleNode = nodes[0] as mdast.Heading;
+  nodes.shift();
+  if (titleNode.type !== 'heading' || titleNode.depth !== 3)
+    throw parsingError(filename, titleNode, `test title must be ###`);
+  const title = asText(filename, titleNode, `test title must be ###`);
+
+  const props: Props = [];
+  let handlingProps = true;
+
+  const lines: string[] = [];
+  const visit = (node: mdast.Node, indent: string) => {
+    if (node.type === 'list') {
+      for (const child of (node as mdast.List).children)
+        visit(child, indent);
+      return;
+    }
+    if (node.type === 'listItem') {
+      const listItem = node as mdast.ListItem;
+      const lastChild = listItem.children[listItem.children.length - 1];
+      if (lastChild?.type === 'code') {
+        handlingProps = false;
+        const text = asText(filename, listItem, `code step must be a list item with a single code block`, lastChild);
+        lines.push(`${indent}await test.step(${escapeString(text)}, async () => {`);
+        lines.push(lastChild.value.split('\n').map(line => indent + '  ' + line).join('\n'));
+        lines.push(`${indent}});`);
+      } else {
+        const text = asText(filename, listItem, `step must contain a single instruction`, lastChild?.type === 'list' ? lastChild : undefined);
+        let isGroup = false;
+        if (handlingProps && lastChild?.type !== 'list' && ['tag:', 'annotation:'].some(prefix => text.startsWith(prefix))) {
+          parseProp(filename, listItem, props);
+        } else if (text.startsWith('group:')) {
+          isGroup = true;
+          lines.push(`${indent}await test.step(${escapeString(text.substring('group:'.length).trim())}, async () => {`);
+        } else if (text.startsWith('expect:')) {
+          handlingProps = false;
+          const assertion = text.substring('expect:'.length).trim();
+          lines.push(`${indent}await agent.expect(${escapeString(assertion)});`);
+        } else if (!text.startsWith('//')) {
+          handlingProps = false;
+          lines.push(`${indent}await agent.perform(${escapeString(text)});`);
+        }
+        if (lastChild?.type === 'list')
+          visit(lastChild, indent + (isGroup ? '  ' : ''));
+        if (isGroup)
+          lines.push(`${indent}});`);
+      }
+    } else {
+      throw parsingError(filename, node, `test step must be a markdown list item`);
+    }
+  };
+
+  for (const node of nodes)
+    visit(node, '');
+  return { title, lines, props };
+}
