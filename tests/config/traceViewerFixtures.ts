cb1cb445a47cf8337a57cf69674ac51826d25da3
Holger Benl <hbenl@evandor.de>
2026-02-11 15:40:01 +0100

    Update tests/config/traceViewerFixtures.ts
    Co-authored-by: Simon Knott <info@simonknott.de>
    Signed-off-by: Holger Benl <hbenl@evandor.de>

--- tests/config/traceViewerFixtures.ts
+++ tests/config/traceViewerFixtures.ts
@@ -171,7 +171,7 @@ export const traceViewerFixtures: Fixtures<TraceViewerFixtures, {}, BaseTestFixt
       const cp = childProcess({ command });
       await cp.waitForOutput('Listening on');
       const browser = await playwright.chromium.launch({
-        channel: 'chrome',
+        channel: 'chromium',
         executablePath: process.env.CRPATH, // without this, setting FFPATH makes us launch Firefox with Chromium args
       });
       browsers.push(browser);
