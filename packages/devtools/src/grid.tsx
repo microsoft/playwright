036f0073406e84df02fadb72f81ba859a40dd3ef
Pavel Feldman <pavel.feldman@gmail.com>
2026-02-25 16:09:19 -0800

    Revert "chore(devtools): show tests (#39388)"
    This reverts commit 5cbd81fc8cc5b3031e4b43f1927e47a9f7ab566e.

--- packages/devtools/src/grid.tsx
+++ packages/devtools/src/grid.tsx
@@ -130,10 +130,10 @@ const SessionChip: React.FC<{ sessionFile: SessionFile; canConnect: boolean; vis
   }, [channel]);
 
   const chipTitle = selectedTab ? `[${config.name}] ${selectedTab.url} \u2014 ${selectedTab.title}` : config.name;
-  const clickable = canConnect && !!wsUrl;
+  const clickable = canConnect && wsUrl !== null;
 
   return (
-    <a className={'session-chip' + (canConnect ? '' : ' disconnected') + (wsUrl === null ? ' not-supported' : '')} href={clickable ? href : undefined} aria-disabled={!clickable} title={chipTitle} onClick={e => {
+    <a className={'session-chip' + (canConnect ? '' : ' disconnected') + (wsUrl === null ? ' not-supported' : '')} href={clickable ? href : undefined} title={chipTitle} onClick={e => {
       e.preventDefault();
       if (clickable)
         navigate(href);
