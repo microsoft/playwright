cb5a1da7f36d87c3b21e5788991f332ea837dce6
Clayton Blythe <19982015+claytonblythe@users.noreply.github.com>
2026-02-27 02:02:01 -0600

    chore(docker): add Finch build helper script

---
+++ utils/docker/build_finch.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+set +x
+
+if [[ ($1 == '--help') || ($1 == '-h') || ($1 == '') || ($2 == '') ]]; then
+  echo "usage: $(basename $0) {--arm64,--amd64} {jammy,noble} playwright:localbuild-noble"
+  echo
+  echo "Build Playwright Finch image and tag it as 'playwright:localbuild-noble'."
+  echo "Once image is built, you can run it with"
+  echo ""
+  echo "  finch run --rm -it playwright:localbuild-noble /bin/bash"
+  echo ""
+  echo "NOTE: this requires Playwright dependencies to be installed with 'npm install'"
+  echo "      and Playwright itself being built with 'npm run build'"
+  echo ""
+  exit 0
+fi
+
+function cleanup() {
+  rm -f "playwright-core.tar.gz"
+}
+
+trap "cleanup; cd $(pwd -P)" EXIT
+cd "$(dirname "$0")"
+
+# We rely on `./playwright-core.tar.gz` to download browsers into the container
+# image.
+node ../../utils/pack_package.js playwright-core ./playwright-core.tar.gz
+
+PLATFORM=""
+if [[ "$1" == "--arm64" ]]; then
+  PLATFORM="linux/arm64";
+elif [[ "$1" == "--amd64" ]]; then
+  PLATFORM="linux/amd64"
+else
+  echo "ERROR: unknown platform specifier - $1. Only --arm64 or --amd64 is supported"
+  exit 1
+fi
+
+finch build --platform "${PLATFORM}" -t "$3" -f "Dockerfile.$2" .
