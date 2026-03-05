2bcea00f1bc776f3026cadfe30d0abbcffcf08f2
Yury Semikhatsky <yurys@chromium.org>
2026-03-04 17:02:35 -0800

    different crop

--- tests/library/tracing.spec.ts
+++ tests/library/tracing.spec.ts
@@ -436,7 +436,7 @@ for (const params of [
     browserTest.fixme(params.id === 'fit' && browserName === 'webkit' && platform === 'linux', 'Image size is flaky');
     browserTest.fixme(browserName === 'firefox' && !headless, 'Image size is different');
 
-    const scale = Math.min(800 / params.width, 600 / params.height, 1);
+    const scale = Math.min(800 / params.width, 800 / params.height, 1);
     const previewWidth = params.width * scale;
     const previewHeight = params.height * scale;
 
