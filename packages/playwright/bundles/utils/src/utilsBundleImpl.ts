f392a2be49824f924483d65135c6aa48da9dbe27
Dmitry Gozman <dgozman@gmail.com>
2026-03-24 11:38:50 +0000

    Revert "feat(test runner): allow running test.spec.md files (#38758)"
    This reverts commit 7e1913f89fdeef8c8603a9b93f114134f013c0a2.

--- packages/playwright/bundles/utils/src/utilsBundleImpl.ts
+++ packages/playwright/bundles/utils/src/utilsBundleImpl.ts
@@ -31,9 +31,3 @@ export const chokidar = chokidarLibrary;
 
 import * as getEastAsianWidthLibrary from 'get-east-asian-width';
 export const getEastAsianWidth = getEastAsianWidthLibrary;
-
-import * as unifiedLibrary from 'unified';
-export const unified = unifiedLibrary;
-
-import remarkParseLibrary from 'remark-parse';
-export const remarkParse = remarkParseLibrary;
