fe35ae048e7e623f7ed8de3e0b7e99b087ab686d
Yury Semikhatsky <yurys@chromium.org>
2026-05-11 11:50:57 -0700

    test(test-runner): use unix-style relative path in spaces filter test

--- tests/playwright-test/command-line-filter.spec.ts
+++ tests/playwright-test/command-line-filter.spec.ts
@@ -174,8 +174,7 @@ test('should focus a single nested test spec', async ({ runInlineTest }) => {
   expect(result.report.suites[1].suites[0].suites[0].specs[0].title).toEqual('pass2');
 });
 
-test('should accept absolute file path containing a space', async ({ runInlineTest }, testInfo) => {
-  const absolutePath = path.join(testInfo.outputPath(), 'dir with space', 'a.spec.ts').split(path.sep).join('/');
+test('should accept unix file path containing a space', async ({ runInlineTest }) => {
   const result = await runInlineTest({
     'dir with space/a.spec.ts': `
       import { test, expect } from '@playwright/test';
@@ -185,7 +184,7 @@ test('should accept absolute file path containing a space', async ({ runInlineTe
       import { test, expect } from '@playwright/test';
       test('not picked', () => { expect(1).toBe(2); });
     `,
-  }, undefined, undefined, { additionalArgs: [absolutePath] });
+  }, undefined, undefined, { additionalArgs: ['dir with space/a.spec.ts'] });
   expect(result.exitCode).toBe(0);
   expect(result.passed).toBe(1);
   expect(result.report.suites).toHaveLength(1);
