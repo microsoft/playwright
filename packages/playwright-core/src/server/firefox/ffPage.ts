85b91075312ad04d6e86ac5a87902238981b98a8
Andrey Lushnikov <aslushnikov@gmail.com>
2026-06-08 23:50:51 +0100

    fix flint

--- packages/playwright-core/src/server/firefox/ffPage.ts
+++ packages/playwright-core/src/server/firefox/ffPage.ts
@@ -381,10 +381,11 @@ export class FFPage implements PageDelegate {
 
   async updateEmulatedViewportSize(): Promise<void> {
     const emulatedSize = this._page.emulatedSize();
-    const viewportSize = emulatedSize?.viewport ?? null;
-    const screenSize = emulatedSize?.screen ?? null;
-    const isMobile = !!this._browserContext._options.isMobile;
-    await this._session.send('Page.setViewportSize', { viewportSize, screenSize, isMobile });
+    await this._session.send('Page.setViewportSize', {
+      viewportSize: emulatedSize?.viewport ?? null,
+      screenSize: emulatedSize?.screen,
+      isMobile: !!this._browserContext._options.isMobile,
+    });
   }
 
   async bringToFront(): Promise<void> {
