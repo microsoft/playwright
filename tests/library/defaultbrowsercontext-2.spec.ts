e5cd8b395a1a99d436d833996747d7d3fd85b7a9
Devin Rousso <devinrousso@microsoft.com>
2026-06-17 15:27:53 -0600

    Revert "fix(firefox): treat `navigationCommitted` events without `navigationId` as same-document URL updates (#41224)" (#41349)
    this is a partial revert for commits 81881e2c1ba7c9cb96db3eab97c0a043b32e0725 <https://github.com/microsoft/playwright/pull/41224> and c2ac911d09675c7dc9124bcad0a1cf04748ffa8a <https://github.com/microsoft/playwright/pull/41309>
    
    leep the test added as that's still a valuable scenario to check, though it's skipped for Firefox due to the known issue
    
    fixes <https://github.com/microsoft/playwright/issues/41347>

--- tests/library/defaultbrowsercontext-2.spec.ts
+++ tests/library/defaultbrowsercontext-2.spec.ts
@@ -145,7 +145,8 @@ it('should create userDataDir if it does not exist', async ({ createUserDataDir,
 
 it('should goto about:blank on relaunched persistent context', {
   annotation: { type: 'issue', description: 'https://github.com/microsoft/playwright/issues/41216' },
-}, async ({ browserType, createUserDataDir }) => {
+}, async ({ browserType, createUserDataDir, browserName }) => {
+  it.fixme(browserName === 'firefox');
   it.slow();
 
   const userDataDir = await createUserDataDir();
