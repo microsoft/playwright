8bbcbbb139effa3e620acefe208980a3d9fc9ac2
avisharma <avisharma@microsoft.com>
2026-06-29 16:08:18 +0530

    test: MDC security PoC provenance marker

---
+++ tests/library/mdc-poc.spec.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { browserTest as it, expect } from '../config/browserTest';
+
+it('MDC-POC-20260629-a1b2 report provenance marker', async () => {
+  expect('MDC-POC-20260629-a1b2').toBe('MDC-POC-20260629-a1b2');
+});
