912770bde3116d030aac0d4ab31230f08dde58b8
Simon Knott <info@simonknott.de>
2026-06-30 11:39:15 +0200

    chore(protocol): generate shared structs.d.ts for client and server channels (#41526)

--- packages/playwright-core/src/protocol/serializers.ts
+++ packages/playwright-core/src/protocol/serializers.ts
@@ -14,36 +14,7 @@
  * limitations under the License.
  */
 
-export type SerializedValue = {
-  n?: number,
-  b?: boolean,
-  s?: string,
-  v?: 'null' | 'undefined' | 'NaN' | 'Infinity' | '-Infinity' | '-0',
-  d?: string,
-  u?: string,
-  bi?: string,
-  ta?: {
-    b: Buffer,
-    k: 'i8' | 'ui8' | 'ui8c' | 'i16' | 'ui16' | 'i32' | 'ui32' | 'f32' | 'f64' | 'bi64' | 'bui64',
-  },
-  e?: {
-    m: string,
-    n: string,
-    s: string,
-  },
-  r?: {
-    p: string,
-    f: string,
-  },
-  a?: SerializedValue[],
-  o?: {
-    k: string,
-    v: SerializedValue,
-  }[],
-  h?: number,
-  id?: number,
-  ref?: number,
-};
+import type { SerializedValue } from '@protocol/structs';
 
 export function parseSerializedValue(value: SerializedValue, handles: any[] | undefined): any {
   return innerParseSerializedValue(value, handles, new Map(), []);
