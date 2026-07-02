1731de0f42a7339a44350568fee6abbd0528dbc4
Dmitry Gozman <dgozman@gmail.com>
2026-07-02 10:06:12 +0100

    chore: consistently use TestAnnotation type

--- utils/generate_types/overrides-testReporter.d.ts
+++ utils/generate_types/overrides-testReporter.d.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import type { TestStatus, Metadata, PlaywrightTestOptions, PlaywrightWorkerOptions, ReporterDescription, FullConfig, FullProject, Location, WorkerInfo } from './test';
+import type { TestStatus, Metadata, PlaywrightTestOptions, PlaywrightWorkerOptions, ReporterDescription, FullConfig, FullProject, Location, WorkerInfo, TestAnnotation } from './test';
 export type { FullConfig, FullProject, TestStatus, Location, WorkerInfo } from './test';
 
 /**
@@ -95,7 +95,7 @@ export interface JSONReportSpec {
 
 export interface JSONReportTest {
   timeout: number;
-  annotations: { type: string, description?: string }[],
+  annotations: TestAnnotation[],
   expectedStatus: TestStatus;
   projectName: string;
   projectId: string;
@@ -127,7 +127,7 @@ export interface JSONReportTestResult {
     body?: string;
     contentType: string;
   }[];
-  annotations: { type: string, description?: string }[];
+  annotations: TestAnnotation[];
   errorLocation?: Location;
 }
 
