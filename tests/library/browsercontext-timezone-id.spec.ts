a51f7da21f60297e8faedf65120bc0f15f1324b9
Dmitry Gozman <dgozman@gmail.com>
2026-07-22 16:24:36 +0100

    chore(firefox): update version to 153.0, unskip worker locale/timezone tests
    Fixes https://github.com/microsoft/playwright/issues/38919

--- tests/library/browsercontext-timezone-id.spec.ts
+++ tests/library/browsercontext-timezone-id.spec.ts
@@ -118,8 +118,7 @@ it('should affect Intl.DateTimeFormat().resolvedOptions().timeZone', async ({ br
   await context.close();
 });
 
-it('should propagate timezone to workers', async ({ browser, browserName, isBidi, server }) => {
-  it.fail(browserName === 'firefox' && !isBidi, 'https://github.com/microsoft/playwright/issues/38919');
+it('should propagate timezone to workers', async ({ browser, server }) => {
   const context = await browser.newContext({ timezoneId: 'America/Jamaica' });
   const page = await context.newPage();
   await page.goto(server.EMPTY_PAGE);
