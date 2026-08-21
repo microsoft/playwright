c65878646b7eb504c1ae3015eae04569a69a5883
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-21 21:59:50 +0500

    test(android): cover swipe starting from the given point

---
+++ tests/android/input.spec.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2020 Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { androidTest as test, expect } from './androidTest';
+
+test('androidInput.swipe should start from the given point', async ({ androidDevice }) => {
+  const context = await androidDevice.launchBrowser();
+  const [page] = context.pages();
+  await page.setContent(`<div style="height: 3000px">${'line<br>'.repeat(200)}</div>`);
+  await androidDevice.input.swipe({ x: 250, y: 1500 }, [{ x: 250, y: 500 }], 30);
+  await expect.poll(() => page.evaluate(() => window.scrollY)).toBeGreaterThan(0);
+  await context.close();
+});
