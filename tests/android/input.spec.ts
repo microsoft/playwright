6de3e9018a638a16529bc6f7eef5cef26a68a8f8
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-24 22:34:32 +0500

    test(android): use rafraf helper and current license header

--- tests/android/input.spec.ts
+++ tests/android/input.spec.ts
@@ -1,11 +1,11 @@
 /**
- * Copyright 2020 Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +15,7 @@
  */
 
 import { androidTest as test, expect } from './androidTest';
+import { rafraf } from '../config/utils';
 
 test('androidInput.swipe should start from the given point', async ({ androidDevice }) => {
   const context = await androidDevice.launchBrowser();
@@ -24,7 +25,7 @@ test('androidInput.swipe should start from the given point', async ({ androidDev
   // pipeline, independent of the CDP connection used by setContent() above. Wait for
   // the new content to actually be composited and presented on the device screen,
   // otherwise the swipe can land on the previous (blank) frame and never scroll anything.
-  await page.evaluate(() => new Promise(resolve => requestAnimationFrame(() => requestAnimationFrame(resolve))));
+  await rafraf(page);
   await androidDevice.input.swipe({ x: 250, y: 1500 }, [{ x: 250, y: 500 }], 30);
   await expect.poll(() => page.evaluate(() => window.scrollY)).toBeGreaterThan(0);
   await context.close();
