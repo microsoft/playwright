da022741298e015a4014c3f8ee6ccc750a2f09e3
Yury Semikhatsky <yurys@chromium.org>
2026-09-08 15:00:46 -0700

    test: fix referer check on msedge, drop persistent OPFS test (#42570)

--- tests/page/page-set-extra-http-headers.spec.ts
+++ tests/page/page-set-extra-http-headers.spec.ts
@@ -15,7 +15,6 @@
  * limitations under the License.
  */
 
-import { chromiumVersionLessThan } from '../config/utils';
 import { test as it, expect } from './pageTest';
 
 it('should work @smoke', async ({ page, server }) => {
@@ -62,8 +61,8 @@ it('should throw for non-string header values', async ({ page }) => {
   expect(error2.message).toContain('Expected value of header "foo" to be String, but "boolean" is found.');
 });
 
-it('should not duplicate referer header', async ({ page, server, browserName, browserVersion }) => {
-  it.fail(browserName === 'chromium' && chromiumVersionLessThan(browserVersion, '154.0.8014.0'), 'Request has referer and Referer');
+it('should not duplicate referer header', async ({ page, server, browserName, browserMajorVersion }) => {
+  it.fail(browserName === 'chromium' && browserMajorVersion < 154, 'Request has referer and Referer');
   await page.setExtraHTTPHeaders({ 'referer': server.EMPTY_PAGE });
   const response = await page.goto(server.EMPTY_PAGE);
   expect(response.ok()).toBe(true);
